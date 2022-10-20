b4a8f9e7839b6725a07c15f80b82702e2a8451d2
Łukasz Nowogórski <74199705+luxus-0@users.noreply.github.com>
2022-10-21 00:15:21 +0200

    Update NumberReceiverMessageProvider.java
    Repair FAILED message and SUCCESS message

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverMessageProvider.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverMessageProvider.java
@@ -4,6 +4,6 @@ public class NumberReceiverMessageProvider {
     public static final int SIZE_NUMBER = 6;
     public static final int RANGE_FROM = 1;
     public static final int RANGE_TO = 99;
-    public static final String FAILED_MESSAGE = "SUCCESS";
-    public static final String SUCCESS_MESSAGE = "FAILED";
+    public static final String FAILED_MESSAGE = "FAILED";
+    public static final String SUCCESS_MESSAGE = "SUCCESS";
 }
