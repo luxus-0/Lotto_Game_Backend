7815e249da85198e79a4e03e3d29bd0bc323c40e
= <luxus0@gmail.com>
2022-10-20 12:43:55 +0200

    Add message provider used in NumberReceiverFacade and NumberValidator

---
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverMessageProvider.java
@@ -0,0 +1,9 @@
+package pl.lotto.numberreceiver;
+
+public class NumberReceiverMessageProvider {
+    public static final int SIZE_NUMBER = 6;
+    public static final int RANGE_FROM = 1;
+    public static final int RANGE_TO = 99;
+    public static final String FAILED_MESSAGE = "SUCCESS";
+    public static final String SUCCESS_MESSAGE = "FAILED";
+}
