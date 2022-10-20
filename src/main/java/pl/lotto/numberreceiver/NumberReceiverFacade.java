c06d486c3cc1603ac7e38bcc5e15df84e7b08763
= <luxus0@gmail.com>
2022-10-20 12:42:58 +0200

    Add method inputNumbers(Set<Integer> inputNumber) with validators

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -1,22 +1,25 @@
 package pl.lotto.numberreceiver;
 
+import pl.lotto.numberreceiver.dto.NumberReceiverDto;
+
 import java.util.Collection;
+import java.util.Set;
+
+import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.FAILED_MESSAGE;
+import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.SUCCESS_MESSAGE;
 
 public class NumberReceiverFacade {
-
-    public static final String FAILED_MESSAGE = "failed";
-    public static final String SUCCESS_MESSAGE = "success";
     private final NumberValidator numberValidator;
 
     public NumberReceiverFacade(NumberValidator numberValidator) {
         this.numberValidator = numberValidator;
     }
 
-    public String inputNumbers(Collection<Integer> numbersFromUser) {
+    public NumberReceiverDto inputNumbers(Set<Integer> numbersFromUser) {
         if (numberValidator.isLessThanSixNumbers(numbersFromUser)) {
-            return FAILED_MESSAGE;
+            return new NumberReceiverDto(numbersFromUser, SUCCESS_MESSAGE);
         }
-        return SUCCESS_MESSAGE;
+        return new NumberReceiverDto(numbersFromUser, FAILED_MESSAGE);
     }
 
 }
