67840facf7b53cffca1e15e6c666233e46219a18
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-21 20:22:16 +0200

    Add methods validation:  less than six number,  equal six numbers,  more than six number

--- src/main/java/pl/lotto/numberreceiver/NumberValidator.java
+++ src/main/java/pl/lotto/numberreceiver/NumberValidator.java
@@ -1,12 +1,37 @@
 package pl.lotto.numberreceiver;
 
-import java.util.Collection;
+import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
+import pl.lotto.numberreceiver.dto.ValidationDto;
 
-import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.SIZE_NUMBER;
+import java.util.Set;
+
+import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.SIZE_NUMBERS;
+import static pl.lotto.numberreceiver.enums.ValidateMessageInfo.NOT_CORRECT_SIZE_NUMBERS;
 
 class NumberValidator {
+    boolean isLessThanSixNumbers(Set<Integer> numbersFromUser) {
+        return numbersFromUser.stream()
+                .map(valid -> new ValidationDto(numbersFromUser, NOT_CORRECT_SIZE_NUMBERS))
+                .anyMatch(NumberValidator::checkSizeLessThanSixNumbers);
+    }
 
-    boolean isLessThanSixNumbers(Collection<Integer> numbersFromUser) {
-        return numbersFromUser.size() < SIZE_NUMBER;
+    private static boolean checkSizeLessThanSixNumbers(ValidationDto validation) {
+        return validation.numbersFromUser().size() < SIZE_NUMBERS;
+    }
+
+    public boolean isEqualsSixNumbers(Set<Integer> inputNumbers) {
+        return inputNumbers.stream().allMatch(numbers -> checkEqualSixNumbers(inputNumbers));
+    }
+
+    public static boolean isSizeEqualSix(NumbersResultMessageDto correctResult) {
+        return correctResult.inputNumber().size() == SIZE_NUMBERS;
+    }
+
+    private static boolean checkEqualSixNumbers(Set<Integer> inputNumbers) {
+        return inputNumbers.size() == SIZE_NUMBERS;
+    }
+
+    static boolean checkMoreThanSixNumbers(Set<Integer> inputNumbers) {
+        return inputNumbers.size() > SIZE_NUMBERS;
     }
 }
