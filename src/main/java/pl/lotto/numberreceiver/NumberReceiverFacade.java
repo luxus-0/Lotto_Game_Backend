53732c5a34c011ea64150642b7cc402fe25c3d3c
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-22 00:30:30 +0200

    Add method isDuplicatedNumbers method check the same numbers

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -1,10 +1,15 @@
 package pl.lotto.numberreceiver;
 
 import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
+import pl.lotto.numberreceiver.enums.ValidateMessageInfo;
+import pl.lotto.numberreceiver.exception.DuplicateNumbersNotFoundException;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import static pl.lotto.numberreceiver.DuplicateNumbersMessage.countDuplicateNumbersInfo;
+import static pl.lotto.numberreceiver.DuplicateNumbersMessage.duplicateNumbersInfo;
 import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.*;
 import static pl.lotto.numberreceiver.NumberValidator.checkMoreThanSixNumbers;
 import static pl.lotto.numberreceiver.enums.ValidateMessageInfo.*;
@@ -33,11 +38,32 @@ public class NumberReceiverFacade {
     }
 
     public NumbersResultMessageDto isMoreThanSixNumbers(Set<Integer> inputNumbers) {
-        boolean greaterThanSixNumbers = inputNumbers.stream().anyMatch(numbers -> checkMoreThanSixNumbers(inputNumbers));
+        boolean greaterThanSixNumbers = inputNumbers.stream()
+                .anyMatch(numbers -> checkMoreThanSixNumbers(inputNumbers));
+
         if (greaterThanSixNumbers) {
             return Optional.of(new NumbersResultMessageDto(inputNumbers, NOT_CORRECT_SIZE_NUMBERS.name()))
                     .orElseGet(() -> new NumbersResultMessageDto(inputNumbers, CORRECT_SIZE_NUMBERS.name()));
         }
-        return new NumbersResultMessageDto(Set.of(0), UNKNOWN_SIZE_NUMBERS.name());
+        return new NumbersResultMessageDto(null, UNKNOWN_SIZE_NUMBERS.name());
+    }
+
+    public ValidateMessageInfo isDuplicateNumbers(List<Integer> numbersCheck){
+        DuplicateNumbersChecker numbersFinder = new DuplicateNumbersChecker();
+        if(numbersFinder.checkIdenticalNumbers(numbersCheck)){
+            duplicateNumbersInfo();
+            return DUPLICATE_NUMBERS;
+        }
+        throw new DuplicateNumbersNotFoundException();
+    }
+
+    public ValidateMessageInfo quantityDuplicateNumbers(List<Integer> numbersFromUser){
+        DuplicateNumbersCounter numbers = new DuplicateNumbersCounter();
+        long counterNumbers = numbers.countIdenticalNumbers(numbersFromUser);
+        if(counterNumbers > 0){
+            countDuplicateNumbersInfo(counterNumbers);
+            return DUPLICATE_NUMBERS;
+        }
+        return DUPLICATE_NUMBERS_NOT_FOUND;
     }
 }
