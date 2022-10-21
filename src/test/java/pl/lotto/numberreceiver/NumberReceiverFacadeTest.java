cc3424a0b04cb63ead69a74cdd2e8e9295c3cff5
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-21 20:13:25 +0200

    Add test methods validation:  less than six number,  equal six numbers,  more than six number

--- src/test/java/pl/lotto/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/pl/lotto/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,8 +1,14 @@
 package pl.lotto.numberreceiver;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
+import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.FAILED_MESSAGE;
+import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.SUCCESS_MESSAGE;
+import static pl.lotto.numberreceiver.enums.ValidateMessageInfo.NOT_CORRECT_SIZE_NUMBERS;
 
 public class NumberReceiverFacadeTest {
 
@@ -11,11 +17,12 @@ public class NumberReceiverFacadeTest {
         // given
         NumberValidator numberValidator = new NumberValidator();
         NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade(numberValidator);
-        List<Integer> numbersFromUser = List.of(1, 2, 3, 4, 5, 6);
+        Set<Integer> numbersFromUser = Set.of(1, 2, 3, 4, 5, 6);
         // when
-        String result = numberReceiverFacade.inputNumbers(numbersFromUser);
+        NumbersResultMessageDto readNumbers = numberReceiverFacade.inputNumbers(numbersFromUser);
         // then
-        assertThat(result).isEqualTo("success");
+        NumbersResultMessageDto numbersResult = new NumbersResultMessageDto(numbersFromUser, SUCCESS_MESSAGE);
+        assertThat(readNumbers).isEqualTo(numbersResult);
     }
 
     @Test
@@ -23,15 +30,25 @@ public class NumberReceiverFacadeTest {
         // given
         NumberValidator numberValidator = new NumberValidator();
         NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade(numberValidator);
-        List<Integer> numbersFromUser = List.of(1, 2, 3, 4);
+        Set<Integer> numbersFromUser = Set.of(1, 2, 3, 4);
         // when
-        String result = numberReceiverFacade.inputNumbers(numbersFromUser);
+        NumbersResultMessageDto readNumbers = numberReceiverFacade.inputNumbers(numbersFromUser);
         // then
-        assertThat(result).isEqualTo("failed");
+        NumbersResultMessageDto numbersResult = new NumbersResultMessageDto(numbersFromUser, FAILED_MESSAGE);
+        assertThat(readNumbers).isEqualTo(numbersResult);
     }
 
     @Test
     public void should_return_failed_when_user_gave_more_than_six_numbers() {
+        // given
+        NumberValidator numberValidator = new NumberValidator();
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade(numberValidator);
+        Set<Integer> numbersFromUser = Set.of(1, 2, 3, 4, 5, 6, 12, 14);
+        // when
+        NumbersResultMessageDto readNumbers = numberReceiverFacade.isMoreThanSixNumbers(numbersFromUser);
+        // then
+        NumbersResultMessageDto numbersResult = new NumbersResultMessageDto(numbersFromUser, NOT_CORRECT_SIZE_NUMBERS.name());
+        assertThat(readNumbers).isEqualTo(numbersResult);
     }
 
     @Test
