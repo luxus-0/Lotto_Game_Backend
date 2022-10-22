019f3abb4a640b8bc0cc13339686a6ae374158a0
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-23 00:55:47 +0200

    Changing name methods with reorganize code.

--- src/main/java/pl/lotto/numberreceiver/NumbersRangeChecker.java
+++ src/main/java/pl/lotto/numberreceiver/NumbersRangeChecker.java
@@ -5,11 +5,11 @@ import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
 import java.util.Set;
 import java.util.stream.IntStream;
 
-import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.RANGE_FROM_NUMBER;
-import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.RANGE_TO_NUMBER;
-import static pl.lotto.numberreceiver.enums.ValidateMessageInfo.IN_RANGE_NUMBERS;
+import static pl.lotto.numberreceiver.NumbersMessageProvider.RANGE_FROM_NUMBER;
+import static pl.lotto.numberreceiver.NumbersMessageProvider.RANGE_TO_NUMBER;
+import static pl.lotto.numberreceiver.enums.ValidateMessage.IN_RANGE_NUMBERS;
 
-class InRangeNumbersChecker {
+class NumbersRangeChecker {
     boolean checkNumbersInRange(Set<Integer> numbersInRange){
         return IntStream.rangeClosed(RANGE_FROM_NUMBER, RANGE_TO_NUMBER)
                 .mapToObj(rangeNumbers -> new NumbersResultMessageDto(numbersInRange, IN_RANGE_NUMBERS.name()))
