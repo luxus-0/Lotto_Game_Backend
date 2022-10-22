e86dadf57eb847290c6604ad7382b85ad9a97048
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-23 00:55:24 +0200

    Changing name methods with reorganize code.

--- src/main/java/pl/lotto/numberreceiver/NumbersDuplicationChecker.java
+++ src/main/java/pl/lotto/numberreceiver/NumbersDuplicationChecker.java
@@ -3,7 +3,7 @@ package pl.lotto.numberreceiver;
 import java.util.Collections;
 import java.util.List;
 
-class DuplicateNumbersChecker {
+class NumbersDuplicationChecker {
    boolean checkIdenticalNumbers(List<Integer> numbersInput){
       return numbersInput.stream()
               .anyMatch(duplicateNumbers -> Collections.frequency(numbersInput, duplicateNumbers) > 1);
