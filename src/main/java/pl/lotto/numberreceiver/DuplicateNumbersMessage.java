4bb1db1ee8105a919a269d81341fb72a715f8154
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-22 03:01:06 +0200

    Change in return method FROM void TO ValidateMessageInfo

--- src/main/java/pl/lotto/numberreceiver/DuplicateNumbersMessage.java
+++ src/main/java/pl/lotto/numberreceiver/DuplicateNumbersMessage.java
@@ -3,8 +3,8 @@ package pl.lotto.numberreceiver;
 import pl.lotto.numberreceiver.enums.ValidateMessageInfo;
 
 class DuplicateNumbersMessage {
-    static void duplicateNumbersInfo(){
-        System.out.println(ValidateMessageInfo.DUPLICATE_NUMBERS);
+    static ValidateMessageInfo duplicateNumbersInfo(){
+        return ValidateMessageInfo.DUPLICATE_NUMBERS;
     }
 
     static void countDuplicateNumbersInfo(long countNumbers){
