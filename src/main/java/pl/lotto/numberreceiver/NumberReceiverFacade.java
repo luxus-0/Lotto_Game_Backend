c6856d62b9b60b5e5ebfb4773bcd1d43f8d39107
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-23 00:48:45 +0200

    Reformat code with change in all methods

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -4,71 +4,60 @@ import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
 import pl.lotto.numberreceiver.enums.ValidateMessageInfo;
 import pl.lotto.numberreceiver.exception.DuplicateNumbersNotFoundException;
 import pl.lotto.numberreceiver.exception.NotInRangeNumbersException;
+import pl.lotto.numberreceiver.exception.NumbersNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import static pl.lotto.numberreceiver.DuplicateNumbersMessage.countDuplicateNumbersInfo;
-import static pl.lotto.numberreceiver.DuplicateNumbersMessage.duplicateNumbersInfo;
 import static pl.lotto.numberreceiver.NumberReceiverMessageProvider.*;
-import static pl.lotto.numberreceiver.NumberValidator.checkMoreThanSixNumbers;
+import static pl.lotto.numberreceiver.NumbersDuplicationInfo.duplicateNumbersInfo;
 import static pl.lotto.numberreceiver.enums.ValidateMessageInfo.*;
 
 public class NumberReceiverFacade {
 
-    private final NumberValidator numberValidator;
+    private final NumberReceiverValidator numberValidator;
 
-    public NumberReceiverFacade(NumberValidator numberValidator) {
+    public NumberReceiverFacade(NumberReceiverValidator numberValidator) {
         this.numberValidator = numberValidator;
     }
 
-    public NumbersResultMessageDto inputNumbers(Set<Integer> inputNumbers) {
-        if (numberValidator.isLessThanSixNumbers(inputNumbers)) {
+    public NumbersResultMessageDto isLessThanSixNumbers(Set<Integer> inputNumbers) {
+        if (numberValidator.checkLessThanSixNumbers(inputNumbers)) {
             NumbersResultMessageDto notCorrectResults = new NumbersResultMessageDto(inputNumbers, FAILED_MESSAGE);
-            return Optional.of(notCorrectResults)
-                    .orElse(new NumbersResultMessageDto(inputNumbers, SUCCESS_MESSAGE));
+            return Optional.of(notCorrectResults).orElse(new NumbersResultMessageDto(inputNumbers, SUCCESS_MESSAGE));
         }
+        throw new NumbersNotFoundException();
+    }
 
-        if (numberValidator.isEqualsSixNumbers(inputNumbers)) {
+    public NumbersResultMessageDto isEqualsSixNumbers(Set<Integer> inputNumbers) {
+        if (numberValidator.checkEqualsSixNumbers(inputNumbers)) {
             NumbersResultMessageDto result = new NumbersResultMessageDto(inputNumbers, CORRECT_SIZE_NUMBERS.name());
             return new NumbersResultMessageDto(inputNumbers, result.message());
         }
-
         return new NumbersResultMessageDto(inputNumbers, INVALID_MESSAGE);
     }
 
     public NumbersResultMessageDto isMoreThanSixNumbers(Set<Integer> inputNumbers) {
-        boolean greaterThanSixNumbers = inputNumbers.stream()
-                .anyMatch(numbers -> checkMoreThanSixNumbers(inputNumbers));
-        if (greaterThanSixNumbers) {
+        if (numberValidator.checkMoreThanSixNumbers(inputNumbers)) {
             return Optional.of(new NumbersResultMessageDto(inputNumbers, NOT_CORRECT_SIZE_NUMBERS.name()))
                     .orElseGet(() -> new NumbersResultMessageDto(inputNumbers, CORRECT_SIZE_NUMBERS.name()));
         }
         return new NumbersResultMessageDto(null, UNKNOWN_SIZE_NUMBERS.name());
+
     }
 
-    public ValidateMessageInfo isDuplicateNumbers(List<Integer> numbersCheck){
-        DuplicateNumbersChecker numbersFinder = new DuplicateNumbersChecker();
-        if(numbersFinder.checkIdenticalNumbers(numbersCheck)){
+    public ValidateMessageInfo isDuplicateNumbers(List<Integer> numbersCheck) {
+        NumbersDuplicationChecker numbersFinder = new NumbersDuplicationChecker();
+        if (numbersFinder.checkIdenticalNumbers(numbersCheck)) {
             return duplicateNumbersInfo();
         }
         throw new DuplicateNumbersNotFoundException();
     }
 
-    public ValidateMessageInfo quantityDuplicateNumbers(List<Integer> numbersFromUser){
-        DuplicateNumbersCounter numbers = new DuplicateNumbersCounter();
-        long counterNumbers = numbers.countIdenticalNumbers(numbersFromUser);
-        if(counterNumbers > 0){
-            countDuplicateNumbersInfo(counterNumbers);
-            return DUPLICATE_NUMBERS;
-        }
-        return DUPLICATE_NUMBERS_NOT_FOUND;
-    }
-
-    public ValidateMessageInfo isNumbersNotInRange(Set<Integer> inputNumbers){
-        InRangeNumbersChecker numbers = new InRangeNumbersChecker();
-        if(numbers.checkNumbersInRange(inputNumbers)){
+    public ValidateMessageInfo isNumbersNotInRange(Set<Integer> inputNumbers) {
+        NumberReceiverRangeChecker numbers = new NumberReceiverRangeChecker();
+        if (numbers.checkNumbersInRange(inputNumbers)) {
             return IN_RANGE_NUMBERS;
         }
         throw new NotInRangeNumbersException();
