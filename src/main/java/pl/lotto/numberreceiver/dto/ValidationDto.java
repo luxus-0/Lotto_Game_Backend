8340343eb6414dd1c83f90ad8c622d9c9d73c6e6
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-23 00:56:09 +0200

    Changing name methods with reorganize code.

--- src/main/java/pl/lotto/numberreceiver/dto/ValidationDto.java
+++ src/main/java/pl/lotto/numberreceiver/dto/ValidationDto.java
@@ -1,7 +1,7 @@
 package pl.lotto.numberreceiver.dto;
 
-import pl.lotto.numberreceiver.enums.ValidateMessageInfo;
+import pl.lotto.numberreceiver.enums.ValidateMessage;
 
 import java.util.Set;
 
-public record ValidationDto(Set<Integer> numbersFromUser, ValidateMessageInfo validateMessageInfo) {}
+public record ValidationDto(Set<Integer> numbersFromUser, ValidateMessage validateMessageInfo) {}
