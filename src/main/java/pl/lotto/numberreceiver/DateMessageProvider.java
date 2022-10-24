12b42b41f928cd23610b5fee9333092e7895b1ff
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-24 15:38:28 +0200

    Add message provider with fields: MESSAGE DATE and DRAW_DATE

--- src/main/java/pl/lotto/numberreceiver/DateMessageProvider.java
+++ src/main/java/pl/lotto/numberreceiver/DateMessageProvider.java
@@ -2,6 +2,7 @@ package pl.lotto.numberreceiver;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 class DateMessageProvider {
     public static final int YEAR = LocalDate.now().getYear();
@@ -9,5 +10,6 @@ class DateMessageProvider {
     public static final int DAY = DayOfWeek.SATURDAY.plus(7).getValue();
     public static final int HOUR = 12;
     public static final int MINUTES = 0;
-    public static final String MESSAGE = "DRAW DATE FOR "+ DAY + "AT TIME : "+HOUR + " : " +MINUTES;
+    public static final String MESSAGE_DATE = "DRAW DATE FOR "+ DAY + "AT TIME : "+HOUR + " : " +MINUTES;
+    public static final LocalDateTime DRAW_DATE = LocalDateTime.of(YEAR, MONTH, DAY, HOUR, MINUTES);
 }
