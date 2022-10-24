6e3c608477c91879635400ad57a08af45f3048cb
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-24 19:37:21 +0200

    Change method name: FROM ValidateMessage printInfo(List<Integer> numbersFromUser); TO static ValidateMessage printDuplicatedNumbersInfo(List<Integer> numbersFromUser)
    REFACTOR NAME METHOD

--- src/main/java/pl/lotto/numberreceiver/NumbersDuplicationCounter.java
+++ src/main/java/pl/lotto/numberreceiver/NumbersDuplicationCounter.java
@@ -10,18 +10,18 @@ import static pl.lotto.numberreceiver.enums.ValidateMessage.DUPLICATE_NUMBERS;
 import static pl.lotto.numberreceiver.enums.ValidateMessage.DUPLICATE_NUMBERS_NOT_FOUND;
 
 class NumbersDuplicationCounter {
-    long countDuplicatedNumbers(List<Integer> numbersInput){
-        return numbersInput.stream()
-                .filter(duplicateNumbers -> Collections.frequency(numbersInput, duplicateNumbers) > 1)
-                .count();
-    }
-
-    ValidateMessage printInfo(List<Integer> numbersFromUser) {
+    static ValidateMessage printDuplicatedNumbersInfo(List<Integer> numbersFromUser) {
         long counterNumbers = countDuplicatedNumbers(numbersFromUser);
         if (counterNumbers > 0) {
             countDuplicateNumbersInfo(counterNumbers);
             return DUPLICATE_NUMBERS;
         }
         return DUPLICATE_NUMBERS_NOT_FOUND;
+    }
+
+    static long countDuplicatedNumbers(List<Integer> numbersInput){
+        return numbersInput.stream()
+                .filter(duplicateNumbers -> Collections.frequency(numbersInput, duplicateNumbers) > 1)
+                .count();
     }
 }
