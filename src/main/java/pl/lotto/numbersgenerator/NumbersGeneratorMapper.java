fc92bd5c8026c0df05acd9956468723785106841
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-24 23:12:28 +0200

    Add constant field RANDOM_NUMBERS Generate random numbers: FROM min number TO max number

---
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorMapper.java
@@ -0,0 +1,10 @@
+package pl.lotto.numbersgenerator;
+
+import java.util.Random;
+
+import static pl.lotto.numbersgenerator.NumbersGeneratorMessageProvider.MAX_NUMBERS;
+import static pl.lotto.numbersgenerator.NumbersGeneratorMessageProvider.MIN_NUMBERS;
+
+public class NumbersGeneratorMapper {
+    public final static int RANDOM_NUMBERS = new Random().nextInt(MIN_NUMBERS, MAX_NUMBERS);
+}
