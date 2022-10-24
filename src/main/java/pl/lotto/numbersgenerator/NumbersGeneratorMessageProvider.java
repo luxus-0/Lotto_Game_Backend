5161ee27a8b31cda64757715423c2c7a37718e9c
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-24 22:53:54 +0200

    Add static message Integer SIZE_MIN = 1; Integer SIZE_MAX = 6; Integer MIN_NUMBERS = 1; Integer MAX_NUMBERS = 99;

---
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorMessageProvider.java
@@ -0,0 +1,8 @@
+package pl.lotto.numbersgenerator;
+
+class NumbersGeneratorMessageProvider {
+    public static final Integer SIZE_MIN = 1;
+    public static final Integer SIZE_MAX = 6;
+    public static final Integer MIN_NUMBERS = 1;
+    public static final Integer MAX_NUMBERS = 99;
+}
