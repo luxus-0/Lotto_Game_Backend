f912299f0807f34bc85c9477be6fb293b6a62ebe
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-24 19:41:02 +0200

    Add dto() method with Builder contains: Set<Integer> numbers; LocalDateTime drawDate;

---
+++ src/main/java/pl/lotto/numberreceiver/Ticket.java
@@ -0,0 +1,22 @@
+package pl.lotto.numberreceiver;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Builder
+@Getter
+public class Ticket{
+   private String hash;
+   private Set<Integer> numbers;
+   private LocalDateTime drawDate;
+
+   Ticket dto(){
+      return Ticket.builder()
+              .numbers(numbers)
+              .drawDate(drawDate)
+              .build();
+   }
+}
