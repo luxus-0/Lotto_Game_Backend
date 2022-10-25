9bba45764cc3cf99616d1d271efd6832cd982ea2
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-26 01:38:37 +0200

    REFORMAT CODE

--- src/main/java/pl/lotto/numberreceiver/dto/NumbersResultMessageDto.java
+++ src/main/java/pl/lotto/numberreceiver/dto/NumbersResultMessageDto.java
@@ -1,5 +1,6 @@
 package pl.lotto.numberreceiver.dto;
 
+import java.util.List;
 import java.util.Set;
 
-public record NumbersResultMessageDto(Set<Integer> inputNumber, String message) {}
+public record NumbersResultMessageDto(Set<Integer> inputNumber, List<String> errorMessage) {}
