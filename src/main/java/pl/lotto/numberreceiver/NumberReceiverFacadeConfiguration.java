01087982fc5e6f445999bb051436f2e4e55091f5
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-26 17:20:29 +0200

    REFACTOR CODE

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
@@ -1,9 +1,15 @@
 package pl.lotto.numberreceiver;
 
+import pl.lotto.date_time_generator.DateTimeGenerator;
+
+import java.time.Clock;
+
 public class NumberReceiverFacadeConfiguration {
-    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository){
+    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, Clock clock){
         NumbersValidator numbersValidator = new NumbersValidator();
-        TicketGenerator ticketGenerator = new TicketGenerator();
+        UuidGenerator uuidGenerator = new UuidGenerator();
+        DateTimeGenerator dateTimeGenerator = new DateTimeGenerator(clock);
+        TicketGenerator ticketGenerator = new TicketGenerator(uuidGenerator, dateTimeGenerator);
         return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketGenerator);
     }
 }
