d7eeb6506290681f9a14dc74434b9b6d31549367
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-26 15:16:37 +0200

    import static pl.lotto.numberreceiver.NumbersMessageProvider.*;

--- src/test/java/pl/lotto/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/pl/lotto/numberreceiver/NumberReceiverFacadeTest.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static pl.lotto.numberreceiver.TicketMessageProvider.*;
+import static pl.lotto.numberreceiver.NumbersMessageProvider.*;
 
 public class NumberReceiverFacadeTest {
 
