a95d49be90a6f8a8d275b9d4b3a52a793f41c4cd
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-26 15:15:59 +0200

    add static method from class NumbersMessageProvider

--- src/main/java/pl/lotto/numberreceiver/NumbersValidator.java
+++ src/main/java/pl/lotto/numberreceiver/NumbersValidator.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
 
-import static pl.lotto.numberreceiver.TicketMessageProvider.*;
+import static pl.lotto.numberreceiver.NumbersMessageProvider.*;
 
 class NumbersValidator {
 
