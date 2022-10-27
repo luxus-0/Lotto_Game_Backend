0052453c1f57de40357c90578a955fa6f4e5f46e
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-27 02:25:26 +0200

    REFACTOR CODE

--- src/main/java/pl/lotto/numberreceiver/TicketRepository.java
+++ src/main/java/pl/lotto/numberreceiver/TicketRepository.java
@@ -1,11 +1,5 @@
 package pl.lotto.numberreceiver;
 
-import java.util.Set;
-
 interface TicketRepository {
     Ticket save(Ticket ticket);
-
-    Set<Integer> findByHash(String hash);
-
-    Set<Integer> findByDate(String dateDraw);
 }
