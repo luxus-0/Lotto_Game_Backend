32c594175c8edb1abd6788ade73529498e4ffcbb
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-28 19:15:29 +0200

    Not finished Facade

--- src/main/java/pl/lotto/datetimegenerator/DateTimeGeneratorFacade.java
+++ src/main/java/pl/lotto/datetimegenerator/DateTimeGeneratorFacade.java
@@ -3,23 +3,10 @@ package pl.lotto.datetimegenerator;
 import java.time.LocalDateTime;
 
 public class DateTimeGeneratorFacade {
-    private final DateTimeActualGenerator dateTimeActualGenerator;
-    private final DateTimeDrawGenerator dateTimeDrawGenerator;
-    private final DateTimeExpiryGenerator dateTimeExpiryGenerator;
 
-    public DateTimeGeneratorFacade(DateTimeActualGenerator dateTimeActualGenerator, DateTimeDrawGenerator dateTimeDrawGenerator, DateTimeExpiryGenerator dateTimeExpiryGenerator) {
-        this.dateTimeActualGenerator = dateTimeActualGenerator;
-        this.dateTimeDrawGenerator = dateTimeDrawGenerator;
-        this.dateTimeExpiryGenerator = dateTimeExpiryGenerator;
-    }
+    private final LocalDateTime drawDate;
 
-
-    public LocalDateTime readActualDateTime() {
-    }
-
-    public LocalDateTime readDrawDateTime() {
-    }
-
-    public LocalDateTime readExpirationDateTime() {
+    public DateTimeGeneratorFacade(LocalDateTime drawDate) {
+        this.drawDate = drawDate;
     }
 }
