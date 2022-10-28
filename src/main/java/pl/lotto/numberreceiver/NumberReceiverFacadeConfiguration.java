3c32243083e979e9c52d713080ea4f6d558f6262
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-28 20:53:36 +0200

    Change method parameter FROM NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository) TO NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime currentDateTime)

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NumberReceiverFacadeConfiguration {
 
-    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository){
+    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime currentDateTime){
         NumbersValidator numbersValidator = new NumbersValidator();
         TicketRandomUUID uuid = new TicketRandomUUID();
-        TicketDrawDate drawDate = new TicketDrawDate();
+        TicketDrawDate drawDate = new TicketDrawDate(currentDateTime);
         TicketGenerator ticketCreated = new TicketGenerator(uuid, drawDate);
         return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketCreated);
     }
