47105ad2e6d566352bf69116bd00d31348336b6d
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-28 19:29:27 +0200

    Changing to name class TicketRandomUUID and TicketDrawDate Add in TicketGenerator as parameters

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
@@ -1,19 +1,15 @@
 package pl.lotto.numberreceiver;
 
 import org.springframework.context.annotation.Configuration;
-import pl.lotto.datetimegenerator.*;
 
 @Configuration
 public class NumberReceiverFacadeConfiguration {
 
-    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, DateTimeGenerator timeGenerator){
+    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository){
         NumbersValidator numbersValidator = new NumbersValidator();
-        UuidGenerator uuidGenerator = new UuidGenerator();
-        DateTimeActualGenerator dateTimeActualGenerator = new DateTimeActualGenerator(timeGenerator.getClock());
-        DateTimeDrawGenerator dateTimeDrawGenerator = new DateTimeDrawGenerator(timeGenerator.getDrawHour(), timeGenerator.getDrawDayOfWeek());
-        DateTimeExpiryGenerator dateTimeExpiryGenerator = new DateTimeExpiryGenerator(timeGenerator.getExpirationInDays());
-        DateTimeGeneratorFacade dateTimeGeneratorFacade = new DateTimeGeneratorFacade(dateTimeActualGenerator, dateTimeDrawGenerator, dateTimeExpiryGenerator);
-        TicketGenerator ticketCreated = new TicketGenerator(uuidGenerator, dateTimeGeneratorFacade);
+        TicketRandomUUID uuid = new TicketRandomUUID();
+        TicketDrawDate drawDate = new TicketDrawDate();
+        TicketGenerator ticketCreated = new TicketGenerator(uuid, drawDate);
         return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketCreated);
     }
 }
