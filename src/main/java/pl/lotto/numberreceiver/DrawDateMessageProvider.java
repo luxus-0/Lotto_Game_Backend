9896b0c6c38f1cc2de3150589f45f12af46a2e7c
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-28 20:47:40 +0200

    Add static final message date draw: success or failed

---
+++ src/main/java/pl/lotto/numberreceiver/DrawDateMessageProvider.java
@@ -0,0 +1,6 @@
+package pl.lotto.numberreceiver;
+
+class DrawDateMessageProvider {
+    public static final String SUCCESS_GENERATED_DATE_WITH_TIME = "success";
+    public static final String FAILED_GENERATED_DATE_WITH_TIME = "failed";
+}
