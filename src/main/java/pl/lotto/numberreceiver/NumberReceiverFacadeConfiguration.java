4e0ac6436673b314b316eb2e1100918ea69b2e2e
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-28 21:19:12 +0200

    Reformate code

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NumberReceiverFacadeConfiguration {
 
-    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime currentDateTime){
+    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime ticketCurrentDateTime){
         NumbersValidator numbersValidator = new NumbersValidator();
         TicketRandomUUID uuid = new TicketRandomUUID();
-        TicketDrawDate drawDate = new TicketDrawDate(currentDateTime);
+        TicketDrawDate drawDate = new TicketDrawDate(ticketCurrentDateTime);
         TicketGenerator ticketCreated = new TicketGenerator(uuid, drawDate);
         return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketCreated);
     }
