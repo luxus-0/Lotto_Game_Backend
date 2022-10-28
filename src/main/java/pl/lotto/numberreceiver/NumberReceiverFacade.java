21d2d2e5bb4c4b86e022a7317b7c70b3ed9fdcec
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-28 19:29:11 +0200

    Changing to name class TicketRandomUUID and TicketDrawDate Add in TicketGenerator as parameters

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -2,10 +2,13 @@ package pl.lotto.numberreceiver;
 
 import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public class NumberReceiverFacade {
 
+    private final static String NUMBERS_NOT_FOUND = "Numbers not found";
     private final NumbersValidator numberValidator;
     private final TicketRepository ticketRepository;
     private final TicketGenerator ticketGenerator;
@@ -21,8 +24,12 @@ public class NumberReceiverFacade {
        if(validate){
            Ticket ticketCreated = ticketGenerator.generateTicket(inputNumbers);
            ticketRepository.save(ticketCreated);
-           return new NumbersResultMessageDto(inputNumbers, numberValidator.errors);
+           return new NumbersResultMessageDto(inputNumbers, numberValidator.messageValidations);
        }
-        return new NumbersResultMessageDto(inputNumbers, numberValidator.errors);
+       return new NumbersResultMessageDto(inputNumbers, numbersNotFound());
+    }
+
+    private List<String> numbersNotFound() {
+        return List.of(NUMBERS_NOT_FOUND);
     }
 }
