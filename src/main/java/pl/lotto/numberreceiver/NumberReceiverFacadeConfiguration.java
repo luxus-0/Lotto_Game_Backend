52494876185f659e83e267a2e2528a5623318838
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-29 03:53:53 +0200

    Reformat code

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
@@ -7,11 +7,11 @@ import java.time.LocalDateTime;
 @Configuration
 public class NumberReceiverFacadeConfiguration {
 
-    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime ticketCurrentDateTime){
+    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime ticketCurrentDateTime, LocalDateTime drawDate){
         NumbersValidator numbersValidator = new NumbersValidator();
         TicketRandomUUID uuid = new TicketRandomUUID();
         TicketDrawDate ticketDrawDate = new TicketDrawDate(ticketCurrentDateTime);
         TicketGenerator ticketCreated = new TicketGenerator(uuid, ticketDrawDate);
-        return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketCreated);
+        return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketCreated, drawDate);
     }
 }
