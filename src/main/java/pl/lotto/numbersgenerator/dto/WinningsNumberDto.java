116db49ecb37a7a4de7cffccd6aa4fe2858d7f7a
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-30 01:39:16 +0200

    Remove

--- src/main/java/pl/lotto/numbersgenerator/dto/WinningsNumberDto.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto.numbersgenerator.dto;
-
-import java.time.LocalDateTime;
-import java.util.Set;
-
-public record WinningsNumberDto(Set<Integer> userNumbers, Set<Integer> randomNumbers, LocalDateTime date) {
-}
