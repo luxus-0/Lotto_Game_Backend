502340f1fe7b37a67dd0c7e484875dddef35828c
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-29 03:53:47 +0200

    Reformat code

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -2,6 +2,8 @@ package pl.lotto.numberreceiver;
 
 import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -11,17 +13,22 @@ public class NumberReceiverFacade {
     private final NumbersValidator numberValidator;
     private final TicketRepository ticketRepository;
     private final TicketGenerator ticketGenerator;
+    private final LocalDateTime drawDateTime;
 
-    public NumberReceiverFacade(NumbersValidator numberValidator, TicketRepository ticketRepository, TicketGenerator ticketGenerator) {
+    public NumberReceiverFacade(NumbersValidator numberValidator, TicketRepository ticketRepository, TicketGenerator ticketGenerator, LocalDateTime drawDateTime) {
         this.numberValidator = numberValidator;
         this.ticketRepository = ticketRepository;
         this.ticketGenerator = ticketGenerator;
+        this.drawDateTime = drawDateTime;
     }
 
     public NumbersResultMessageDto inputNumbers(Set<Integer> inputNumbers) {
        boolean validate = numberValidator.validate(inputNumbers);
        if(validate){
-           Ticket ticketCreated = ticketGenerator.generateTicket(inputNumbers);
+           TicketCurrentDateTime currentDateTime = new TicketCurrentDateTime(Clock.systemUTC());
+           TicketDrawDate ticketDrawDate = new TicketDrawDate(currentDateTime);
+           LocalDateTime drawDate = ticketDrawDate.generateDrawDate(drawDateTime);
+           Ticket ticketCreated = ticketGenerator.generateTicket(inputNumbers, drawDate);
            ticketRepository.save(ticketCreated);
            return new NumbersResultMessageDto(inputNumbers, numberValidator.messagesValidation);
        }
