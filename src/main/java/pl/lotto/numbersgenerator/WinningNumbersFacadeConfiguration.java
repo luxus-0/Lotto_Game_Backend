e146e04a7318851f6d2fa80ba9d9aeb8d0e68cff
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-31 14:48:30 +0100

    REFACTOR CODE

--- src/main/java/pl/lotto/numbersgenerator/WinningNumbersFacadeConfiguration.java
+++ src/main/java/pl/lotto/numbersgenerator/WinningNumbersFacadeConfiguration.java
@@ -1,7 +1,16 @@
 package pl.lotto.numbersgenerator;
 
 import org.springframework.context.annotation.Configuration;
+import pl.lotto.numberreceiver.NumbersValidator;
+import pl.lotto.numberreceiver.Ticket;
 
 @Configuration
-class WinningNumbersFacadeConfiguration {
+public class WinningNumbersFacadeConfiguration {
+
+    public WinningNumbersFacade createModuleForTests(WinningNumbersRepository winningNumbersRepository, Ticket ticket){
+        NumbersGenerator numbersGenerator = new NumbersGenerator();
+        NumbersValidator validator = new NumbersValidator();
+        WinningNumbersValidator winningNumbersValidator = new WinningNumbersValidator(numbersGenerator, validator, ticket);
+        return new WinningNumbersFacade(winningNumbersRepository, winningNumbersValidator);
+    }
 }
