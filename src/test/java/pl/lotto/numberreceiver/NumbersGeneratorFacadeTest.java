10205ebf8ad3ec5e264bb3e0904bf7f2a118253c
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-31 17:10:12 +0100

    REFACTOR CODE

--- src/test/java/pl/lotto/numberreceiver/NumbersGeneratorFacadeTest.java
+++ src/test/java/pl/lotto/numberreceiver/NumbersGeneratorFacadeTest.java
@@ -2,6 +2,7 @@ package pl.lotto.numberreceiver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
 import pl.lotto.numbersgenerator.InMemoryWinningNumbersRepository;
 import pl.lotto.numbersgenerator.WinningNumbersFacade;
 import pl.lotto.numbersgenerator.WinningNumbersFacadeConfiguration;
@@ -13,12 +14,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Set;
+import java.util.stream.Stream;
 
+import static java.time.LocalTime.MIDNIGHT;
 import static java.time.LocalTime.NOON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static pl.lotto.numberreceiver.NumbersMessageProvider.SIZE_NUMBERS;
 import static pl.lotto.numbersgenerator.WinningNumbersMessageProvider.FAILED;
+import static pl.lotto.numbersgenerator.WinningNumbersMessageProvider.SUCCESS;
 
 public class NumbersGeneratorFacadeTest {
 
@@ -96,6 +100,30 @@ public class NumbersGeneratorFacadeTest {
         //then
         assertFalse(isWinnerNumber, result.messageInfo());
         assertNotEquals(SIZE_NUMBERS, inputNumbers.size());
+    }
+
+    @Test
+    @DisplayName("return failed when user get six numbers and not correct date with time draw")
+    public void should_return_failed_when_user_get_6_numbers_and_not_correct_date_with_time_draw() {
+        //given
+        Set<Integer> inputNumbers = Set.of(10, 20, 34, 40, 75, 60);
+        Set<Integer> randomNumbers = Set.of(10, 20, 30, 40, 50, 60);
+        LocalDate notCorrectDate = LocalDate.of(2022, Month.OCTOBER, 31);
+        LocalDateTime notCorrectDateTime = LocalDateTime.of(notCorrectDate, MIDNIGHT);
+        LocalDate correctDate = LocalDate.of(2022, Month.OCTOBER, 29);
+        LocalDateTime correctDateTime = LocalDateTime.of(correctDate, NOON);
+        Integer randomNumber = randomNumbers.stream().findAny().orElse(0);
+        Ticket ticket = getTicket(correctDateTime, inputNumbers);
+
+        WinningNumbersFacade winningNumbersFacade = new WinningNumbersFacadeConfiguration()
+                .createModuleForTests(winningNumbersRepository, ticket);
+        //when
+        boolean isWinnerNumbers = inputNumbers.contains(randomNumber);
+        //then
+        assertThat(isWinnerNumbers).isEqualTo(true);
+        assertNotEquals(correctDateTime, notCorrectDateTime);
+        assertEquals(SIZE_NUMBERS, randomNumbers.size());
+        assertEquals(SIZE_NUMBERS, inputNumbers.size());
     }
 
     @Test
