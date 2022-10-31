e1cb600ef0cd6e0197a4c3f6db56b7b32ceb5efb
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-31 14:47:17 +0100

    REFACTOR CODE

--- src/main/java/pl/lotto/numbersgenerator/NumbersGenerator.java
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGenerator.java
@@ -1,15 +1,15 @@
 package pl.lotto.numbersgenerator;
 
-import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static pl.lotto.numbersgenerator.NumbersGeneratorMapper.RANDOM_NUMBERS;
-import static pl.lotto.numbersgenerator.NumbersGeneratorMessageProvider.*;
+import static pl.lotto.numbersgenerator.NumbersGeneratorMessageProvider.SIZE_MAX;
+import static pl.lotto.numbersgenerator.NumbersGeneratorMessageProvider.SIZE_MIN;
 
-public class NumbersGenerator {
-    Set<Integer> generateNumbers() {
+class NumbersGenerator {
+    public Set<Integer> generateNumbers() {
         return IntStream.rangeClosed(SIZE_MIN, SIZE_MAX)
                 .map(mapper -> RANDOM_NUMBERS)
                 .boxed()
