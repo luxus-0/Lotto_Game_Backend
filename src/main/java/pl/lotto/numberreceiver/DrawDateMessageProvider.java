ad1691e14cb3355365d005564e7c2315fbf22456
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-31 17:16:25 +0100

    Remove unused class

--- src/main/java/pl/lotto/numberreceiver/DrawDateMessageProvider.java
+++
@@ -1,6 +0,0 @@
-package pl.lotto.numberreceiver;
-
-class DrawDateMessageProvider {
-    public static final String SUCCESS_GENERATED_DATE_WITH_TIME = "success";
-    public static final String FAILED_GENERATED_DATE_WITH_TIME = "failed";
-}
