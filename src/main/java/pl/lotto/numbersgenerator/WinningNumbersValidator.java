8f34c5e22f4752731e42d98b05d6c0f5e8f22f96
Łukasz Nowogórski <luxus0@gmail.com>
2022-10-31 14:48:53 +0100

    REFACTOR CODE

--- src/main/java/pl/lotto/numbersgenerator/WinningNumbersValidator.java
+++ src/main/java/pl/lotto/numbersgenerator/WinningNumbersValidator.java
@@ -1,5 +1,7 @@
 package pl.lotto.numbersgenerator;
 
+import pl.lotto.numberreceiver.NumbersValidator;
+import pl.lotto.numberreceiver.Ticket;
 import pl.lotto.numbersgenerator.dto.WinningNumbersResultDto;
 
 import java.util.Optional;
@@ -11,18 +13,23 @@ import static pl.lotto.numbersgenerator.WinningNumbersMessageProvider.SUCCESS;
 class WinningNumbersValidator {
 
     private final NumbersGenerator numbersGenerator;
+    private final NumbersValidator numbersValidator;
+    private final Ticket ticket;
 
-    WinningNumbersValidator(NumbersGenerator numbersGenerator) {
+    WinningNumbersValidator(NumbersGenerator numbersGenerator, NumbersValidator numbersValidator, Ticket ticket) {
         this.numbersGenerator = numbersGenerator;
+        this.numbersValidator = numbersValidator;
+        this.ticket = ticket;
     }
 
-    public WinningNumbersResultDto isWinnerNumbers(Set<Integer> numbersInputFromUser) {
-        Integer numberUser = numbersInputFromUser.stream().findAny().orElse(0);
+    public WinningNumbersResultDto isWinnerNumbers() {
+        Set<Integer> inputNumbers = ticket.numbersUser();
+        Integer numberFromUser = inputNumbers.stream().findAny().orElse(0);
         Set<Integer> randomNumbers = numbersGenerator.generateNumbers();
-        if(randomNumbers.contains(numberUser)){
-            return new WinningNumbersResultDto(numbersInputFromUser, SUCCESS);
+        if(randomNumbers.contains(numberFromUser) && numbersValidator.validate(inputNumbers)){
+            return new WinningNumbersResultDto(ticket, SUCCESS);
         }
-       return Optional.of(new WinningNumbersResultDto(numbersInputFromUser, FAILED))
+       return Optional.of(new WinningNumbersResultDto(ticket, FAILED))
                .orElseThrow();
     }
 }
