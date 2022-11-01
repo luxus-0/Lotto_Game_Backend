07261ffc7fd10e182f1c89f31597f5369a15da73
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-01 20:11:45 +0100

    REFACTOR CODE

--- src/main/java/pl/lotto/numberreceiver/TicketRandomUUID.java
+++ src/main/java/pl/lotto/numberreceiver/TicketRandomUUID.java
@@ -3,6 +3,7 @@ package pl.lotto.numberreceiver;
 import java.util.UUID;
 
 class TicketRandomUUID {
+
     public String generateUUID() {
         return UUID.randomUUID().toString();
     }
--- src/test/java/pl/lotto/numberreceiver/TicketDrawDateTest.java
+++ src/test/java/pl/lotto/numberreceiver/TicketDrawDateTest.java
@@ -2,32 +2,54 @@ package pl.lotto.numberreceiver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.cglib.core.Local;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TicketDrawDateTest {
 
     private final TicketDrawDate drawDate;
-    private final TicketCurrentDateTime currentDateTime;
 
     TicketDrawDateTest() {
         Clock clock = Clock.systemUTC();
-        this.currentDateTime = new TicketCurrentDateTime(clock);
+        TicketCurrentDateTime currentDateTime = new TicketCurrentDateTime(clock);
         this.drawDate = new TicketDrawDate(currentDateTime);
+
     }
 
     @Test
-    @DisplayName("return failed date time draw given date time with clock UTC")
-    public void should_return_date_time_draw_when_user_get_date_time_now_with_UTC_clock() {
+    @DisplayName("return success day of week with clock UTC when today is saturday draw date")
+    public void should_return_success_day_of_week_with_clock_UTC_when_saturday() {
+
         //given
-        LocalDate date = LocalDate.of(2000, Month.APRIL, DayOfWeek.SATURDAY.getValue());
-        LocalTime time = LocalTime.parse("11:00");
-        LocalDateTime actualDateTime = LocalDateTime.of(date, time);
+        Clock clock = Clock.systemUTC();
+        TicketCurrentDateTime currentDateTime = new TicketCurrentDateTime(clock);
+        DayOfWeek dayOfWeek = DayOfWeek.SATURDAY;
+
         //when
-        LocalDateTime drawDateCreated = drawDate.generateDrawDate(currentDateTime.generateToday());
+        DayOfWeek resultDay = currentDateTime.generateToday().getDayOfWeek();
+
         //then
-        assertNotEquals(actualDateTime, drawDateCreated);
+        assertNotEquals(dayOfWeek, resultDay);
+    }
+
+    @Test
+    @DisplayName("return correct draw day time when user give saturday 11 am with clock UTC when draw date today")
+    public void should_return_correct_draw_day_time_when_user_get_saturday_11_am_with_clock_UTC_and_not_today_is_draw_date() {
+
+        //given
+        LocalDate actualDate = LocalDate.of(2022, Month.NOVEMBER, DayOfWeek.SATURDAY.getValue());
+        LocalTime actualTime = LocalTime.NOON;
+        LocalDateTime actualDateTime = LocalDateTime.of(actualDate, actualTime);
+        actualDateTime.format(DateTimeFormatter.ISO_DATE_TIME);
+        //when
+        LocalDateTime resultDrawDate = drawDate.generateDrawDate(LocalDateTime.now());
+
+        //then
+        assertNotEquals(resultDrawDate, actualDateTime);
     }
 }
