96d3c844b2dd024ec545de97da495da4f5aa16b3
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-01 20:33:53 +0100

    REFACTOR CODE

--- src/test/java/pl/lotto/numberreceiver/TicketDrawDateTest.java
+++ src/test/java/pl/lotto/numberreceiver/TicketDrawDateTest.java
@@ -2,13 +2,11 @@ package pl.lotto.numberreceiver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.cglib.core.Local;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class TicketDrawDateTest {
 
@@ -38,12 +36,28 @@ class TicketDrawDateTest {
     }
 
     @Test
-    @DisplayName("return correct draw day time when user give saturday 11 am with clock UTC when draw date today")
+    @DisplayName("return correct draw day time when user give saturday 11 am with clock UTC")
     public void should_return_correct_draw_day_time_when_user_get_saturday_12_am_with_clock_UTC_and_not_today_is_draw_date() {
 
         //given
         LocalDate actualDate = LocalDate.of(2022, Month.NOVEMBER, DayOfWeek.SATURDAY.getValue());
         LocalTime actualTime = LocalTime.NOON;
+        LocalDateTime actualDateTime = LocalDateTime.of(actualDate, actualTime);
+        actualDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+        //when
+        LocalDateTime resultDrawDate = drawDate.generateDrawDate(LocalDateTime.now());
+
+        //then
+        assertNotEquals(resultDrawDate, actualDateTime);
+    }
+
+    @Test
+    @DisplayName("return correct draw day time when user give sunday 11 am with clock UTC")
+    public void should_return_failed_draw_day_time_when_user_get_not_saturday_12_am_with_clock_UTC_and_not_today_is_draw_date() {
+
+        //given
+        LocalDate actualDate = LocalDate.of(2022, Month.NOVEMBER, DayOfWeek.SUNDAY.getValue());
+        LocalTime actualTime = LocalTime.of(11,0);
         LocalDateTime actualDateTime = LocalDateTime.of(actualDate, actualTime);
         actualDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
         //when
