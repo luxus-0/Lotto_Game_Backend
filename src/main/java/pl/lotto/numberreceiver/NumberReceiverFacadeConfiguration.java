093e33da37adc8f091bdd2af9919e9d0b334dcb6
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-01 23:10:37 +0100

    REFACTOR CODE

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacadeConfiguration.java
@@ -2,15 +2,17 @@ package pl.lotto.numberreceiver;
 
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 @Configuration
 public class NumberReceiverFacadeConfiguration {
 
-    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, TicketCurrentDateTime ticketCurrentDateTime, LocalDateTime drawDate){
+    NumberReceiverFacade createModuleForTests(TicketRepository ticketRepository, LocalDateTime drawDate){
+        Clock clock = Clock.systemUTC();
         NumbersValidator numbersValidator = new NumbersValidator();
         TicketRandomUUID uuid = new TicketRandomUUID();
-        TicketDrawDate ticketDrawDate = new TicketDrawDate(ticketCurrentDateTime);
+        TicketDrawDate ticketDrawDate = new TicketDrawDate(clock);
         TicketGenerator ticketCreated = new TicketGenerator(uuid, ticketDrawDate);
         return new NumberReceiverFacade(numbersValidator, ticketRepository, ticketCreated, drawDate);
     }
