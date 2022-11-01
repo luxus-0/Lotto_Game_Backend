35c80306dc0b001955688a7821073ce02d88e4c8
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-01 20:59:38 +0100

    REFACTOR CODE

--- src/test/java/pl/lotto/numberreceiver/TicketDrawDateTest.java
+++ src/test/java/pl/lotto/numberreceiver/TicketDrawDateTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class TicketDrawDateTest {
@@ -52,8 +54,8 @@ class TicketDrawDateTest {
     }
 
     @Test
-    @DisplayName("return correct draw day time when user give sunday 11 am with clock UTC")
-    public void should_return_failed_draw_day_time_when_user_get_not_saturday_12_am_with_clock_UTC_and_not_today_is_draw_date() {
+    @DisplayName("return failed draw day time when user give sunday 11 am with clock UTC")
+    public void should_return_failed_draw_day_time_when_user_get_sunday_11_am_with_clock_UTC_and_not_today_is_draw_date() {
 
         //given
         LocalDate actualDate = LocalDate.of(2022, Month.NOVEMBER, DayOfWeek.SUNDAY.getValue());
@@ -65,5 +67,39 @@ class TicketDrawDateTest {
 
         //then
         assertNotEquals(resultDrawDate, actualDateTime);
+    }
+
+    @Test
+    @DisplayName("return draw date time is after now when user give saturday 12 december am with clock UTC")
+    public void should_return_draw_date_time_is_after_now_when_user_get_saturday_12_00_am_december_with_clock_UTC() {
+
+        //given
+        LocalDateTime date = LocalDate.of(2022, Month.DECEMBER, 3).atStartOfDay();
+        LocalDateTime dateTime = date.plus(12, ChronoUnit.HOURS).plusMinutes(0);
+        dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+        LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
+
+        //when
+        LocalDateTime dateTimeResult = drawDate.generateDrawDate(now);
+
+        //then
+        assertThat(dateTime).isAfter(dateTimeResult);
+    }
+
+    @Test
+    @DisplayName("return draw date time is after now when user give saturday 12 december am with clock UTC")
+    public void should_return_draw_date_time_is_before_now_when_user_get_saturday_12_00_am_december_with_clock_UTC() {
+
+        //given
+        LocalDateTime date = LocalDate.of(2022, Month.DECEMBER, 3).atStartOfDay();
+        LocalDateTime dateTime = date.plus(12, ChronoUnit.HOURS).plusMinutes(0);
+        dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+        LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
+
+        //when
+        LocalDateTime dateTimeResult = drawDate.generateDrawDate(now);
+
+        //then
+        assertThat(dateTime).isBefore(dateTimeResult);
     }
 }
