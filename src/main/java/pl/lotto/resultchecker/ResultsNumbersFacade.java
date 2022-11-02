fa65c99be5b68bfea64f883406d3d0e7c4030f09
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-02 13:30:05 +0100

    refactor

--- src/main/java/pl/lotto/resultchecker/ResultsNumbersFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsNumbersFacade.java
@@ -1,18 +1,13 @@
 package pl.lotto.resultchecker;
 
-import pl.lotto.numberreceiver.NumberReceiverFacade;
-import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
-
 import java.util.Optional;
 import java.util.Set;
 
 public class ResultsNumbersFacade {
     private final ResultsChecker resultsChecker;
-    private final NumberReceiverFacade receiverFacade;
 
-    public ResultsNumbersFacade(ResultsChecker resultsChecker, NumberReceiverFacade receiverFacade) {
+    public ResultsNumbersFacade(ResultsChecker resultsChecker) {
         this.resultsChecker = resultsChecker;
-        this.receiverFacade = receiverFacade;
     }
 
     String messageResultChecker(Set<Integer>inputNumbers, Set<Integer> lottoNumbers){
