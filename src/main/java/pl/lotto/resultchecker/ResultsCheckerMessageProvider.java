665e52456fdb8c5b3e85c6513c9612b02032c5ce
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-02 13:00:30 +0100

    Refactor

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerMessageProvider.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerMessageProvider.java
@@ -1,6 +1,20 @@
 package pl.lotto.resultchecker;
 
+import java.util.Set;
+
 class ResultsCheckerMessageProvider {
     public final static String WIN = "win";
     public final static String NOT_WIN = "not win";
+    private final ResultsChecker resultsChecker;
+
+    ResultsCheckerMessageProvider(ResultsChecker resultsChecker) {
+        this.resultsChecker = resultsChecker;
+    }
+
+    public String getResultMessage(Set<Integer> inputNumbers, Set<Integer> randomNumbers){
+        if(resultsChecker.checkWinnerNumbers(inputNumbers, randomNumbers)){
+            return ResultsCheckerMessageProvider.WIN;
+        }
+        return ResultsCheckerMessageProvider.NOT_WIN;
+    }
 }
