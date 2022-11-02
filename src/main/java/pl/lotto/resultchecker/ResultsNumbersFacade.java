67dbfff1fa2f3337a6854f3ae1e362f2e27603b1
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-02 13:34:00 +0100

    One error, not finished method: messageResultAll

--- src/main/java/pl/lotto/resultchecker/ResultsNumbersFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsNumbersFacade.java
@@ -1,20 +1,28 @@
 package pl.lotto.resultchecker;
 
+import pl.lotto.numberreceiver.Ticket;
+
 import java.util.Optional;
 import java.util.Set;
 
 public class ResultsNumbersFacade {
     private final ResultsChecker resultsChecker;
+    private final ResultsNumbersGenerator resultsNumbersGenerator;
 
-    public ResultsNumbersFacade(ResultsChecker resultsChecker) {
+    public ResultsNumbersFacade(ResultsChecker resultsChecker, ResultsNumbersGenerator resultsNumbersGenerator) {
         this.resultsChecker = resultsChecker;
+        this.resultsNumbersGenerator = resultsNumbersGenerator;
     }
 
-    String messageResultChecker(Set<Integer>inputNumbers, Set<Integer> lottoNumbers){
+    String messageResultsChecker(Set<Integer>inputNumbers, Set<Integer> lottoNumbers){
         if(resultsChecker.checkWinnerNumbers(inputNumbers, lottoNumbers)){
             ResultsCheckerMessageProvider messageResult = new ResultsCheckerMessageProvider(resultsChecker);
             return messageResult.getResultMessage(inputNumbers, lottoNumbers);
         }
         return Optional.of("NOT WIN NUMBERS").orElseThrow();
+    }
+
+    String messageResultsAll(Ticket ticket, String message){
+
     }
 }
