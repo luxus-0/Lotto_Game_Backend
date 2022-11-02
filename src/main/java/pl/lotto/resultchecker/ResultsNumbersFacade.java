c20c91827a61e024c863acb2bb60557ebeed0b58
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-02 13:29:01 +0100

    Refactor

--- src/main/java/pl/lotto/resultchecker/ResultsNumbersFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsNumbersFacade.java
@@ -1,4 +1,25 @@
 package pl.lotto.resultchecker;
 
+import pl.lotto.numberreceiver.NumberReceiverFacade;
+import pl.lotto.numberreceiver.dto.NumbersResultMessageDto;
+
+import java.util.Optional;
+import java.util.Set;
+
 public class ResultsNumbersFacade {
+    private final ResultsChecker resultsChecker;
+    private final NumberReceiverFacade receiverFacade;
+
+    public ResultsNumbersFacade(ResultsChecker resultsChecker, NumberReceiverFacade receiverFacade) {
+        this.resultsChecker = resultsChecker;
+        this.receiverFacade = receiverFacade;
+    }
+
+    String messageResultChecker(Set<Integer>inputNumbers, Set<Integer> lottoNumbers){
+        if(resultsChecker.checkWinnerNumbers(inputNumbers, lottoNumbers)){
+            ResultsCheckerMessageProvider messageResult = new ResultsCheckerMessageProvider(resultsChecker);
+            return messageResult.getResultMessage(inputNumbers, lottoNumbers);
+        }
+        return Optional.of("NOT WIN NUMBERS").orElseThrow();
+    }
 }
