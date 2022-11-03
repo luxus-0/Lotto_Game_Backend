d4dbbf46ea1fe560ccb54e0040ae4e75f533ea9d
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-03 12:46:02 +0100

    Refactor

--- src/main/java/pl/lotto/resultchecker/ResultsLotto.java
+++ src/main/java/pl/lotto/resultchecker/ResultsLotto.java
@@ -1,10 +1,7 @@
 package pl.lotto.resultchecker;
 
-import lombok.Data;
-
 import java.time.LocalDateTime;
 import java.util.Set;
 
-record ResultsLotto(String uuid, Set<Integer> numbersUser, Set<Integer> winningNumbers, LocalDateTime drawDate,
-                    boolean isWinner, String message) {
+record ResultsLotto(String uuid, Set<Integer> numbersUser, Set<Integer> winningNumbers, LocalDateTime drawDate, String message) {
 }
