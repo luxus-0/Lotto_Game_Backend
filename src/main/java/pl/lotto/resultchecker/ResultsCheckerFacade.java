7fa4d57f2597013353ac17797cbfac359a63f759
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-05 17:39:41 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
@@ -1,5 +1,9 @@
 package pl.lotto.resultchecker;
 
+import pl.lotto.numbersgenerator.InMemoryWinningNumbersRepository;
+import pl.lotto.numbersgenerator.WinningNumbersRepository;
+import pl.lotto.numbersgenerator.dto.WinningNumbersResultDto;
+
 import java.util.Optional;
 import java.util.Set;
 
@@ -7,9 +11,11 @@ import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WINNER_NUMBER
 
 public class ResultsCheckerFacade {
     private final ResultsChecker resultsChecker;
+    private final WinningNumbersResultDto winningNumbersResult;
 
-    public ResultsCheckerFacade(ResultsChecker resultsChecker) {
+    public ResultsCheckerFacade(ResultsChecker resultsChecker, WinningNumbersResultDto winningNumbersResult) {
         this.resultsChecker = resultsChecker;
+        this.winningNumbersResult = winningNumbersResult;
     }
 
     String getNumbersResult(Set<Integer>inputNumbers, Set<Integer> lottoNumbers){
@@ -26,5 +32,15 @@ public class ResultsCheckerFacade {
             return new ResultsLotto(results.uuid(), results.numbersUser(), results.winningNumbers(), results.drawDate(), successResult);
         }
         return Optional.of(results).get();
+    }
+
+    Set<Integer> getWinners(String uuid, ResultsLotto results){
+        Set<Integer> numbersFromUser = results.numbersUser();
+        Set<Integer> winnerNumbers = results.winningNumbers();
+        if(resultsChecker.checkWinnerNumbers(numbersFromUser, winnerNumbers)){
+            WinningNumbersRepository winningNumbersRepository = new InMemoryWinningNumbersRepository();
+            return winningNumbersRepository.findWinningNumbers(uuid);
+        }
+        return Optional.of(results.winningNumbers()).orElseThrow();
     }
 }
