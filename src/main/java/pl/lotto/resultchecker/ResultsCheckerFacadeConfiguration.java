c95bf9c85005a461b17e6bcd7f3aaaad347522d3
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 21:57:56 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacadeConfiguration.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacadeConfiguration.java
@@ -1,4 +1,12 @@
 package pl.lotto.resultchecker;
 
+import pl.lotto.numberreceiver.*;
+
 public class ResultsCheckerFacadeConfiguration {
+
+    ResultsCheckerFacade createModuleForTests(ResultsLottoRepository resultsRepository){
+        ResultsChecker resultsChecker = new ResultsChecker();
+        NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
+        return new ResultsCheckerFacade(resultsChecker);
+    }
 }
