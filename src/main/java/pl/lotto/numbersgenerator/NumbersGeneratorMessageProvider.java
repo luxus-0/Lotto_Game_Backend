93778ff3cddc3022f9089661fed3e1085ea3ea28
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 15:49:18 +0100

    Refactor code

--- src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorMessageProvider.java
+++
@@ -1,8 +0,0 @@
-package pl.lotto.numbersgenerator;
-
-class NumbersGeneratorMessageProvider {
-    public static final Integer SIZE_MIN = 1;
-    public static final Integer SIZE_MAX = 6;
-    public static final Integer MIN_NUMBERS = 1;
-    public static final Integer MAX_NUMBERS = 99;
-}
