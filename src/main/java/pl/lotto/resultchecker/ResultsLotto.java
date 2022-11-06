26181736cc4a1a4f7217c07d44cd5b7beb7e2c02
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 16:23:01 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/ResultsLotto.java
+++ src/main/java/pl/lotto/resultchecker/ResultsLotto.java
@@ -3,5 +3,5 @@ package pl.lotto.resultchecker;
 import java.time.LocalDateTime;
 import java.util.Set;
 
-record ResultsLotto(String uuid, Set<Integer> numbersUser, Set<Integer> winningNumbers, LocalDateTime drawDate, String message) {
+record ResultsLotto(String uuid, Set<Integer> numbersUser, Set<Integer> lottoNumbers, LocalDateTime drawDate, String message) {
 }
