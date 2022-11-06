cd40952080493b1187e14f690a695329321c142d
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 21:57:44 +0100

    Refactor code

---
+++ src/main/java/pl/lotto/resultchecker/exceptions/DateWinnerNotFoundException.java
@@ -0,0 +1,9 @@
+package pl.lotto.resultchecker.exceptions;
+
+import java.time.LocalDateTime;
+
+public class DateWinnerNotFoundException extends RuntimeException {
+    public DateWinnerNotFoundException(LocalDateTime dateTime){
+        System.err.println("In Date: " +dateTime + " numbers winner not found");
+    }
+}
