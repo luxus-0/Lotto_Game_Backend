c331392327b62e654f5bc4ef57e5327448901fa8
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 16:22:56 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
@@ -1,8 +1,8 @@
 package pl.lotto.resultchecker;
 
-import pl.lotto.numbersgenerator.InMemoryWinningNumbersRepository;
-import pl.lotto.numbersgenerator.WinningNumbersRepository;
-import pl.lotto.numbersgenerator.dto.WinningNumbersResultDto;
+import pl.lotto.numberreceiver.InMemoryNumberReceiverRepository;
+import pl.lotto.numberreceiver.NumberReceiverRepository;
+import pl.lotto.numberreceiver.dto.NumbersDateMessageDto;
 
 import java.util.Optional;
 import java.util.Set;
@@ -11,9 +11,9 @@ import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WINNER_NUMBER
 
 public class ResultsCheckerFacade {
     private final ResultsChecker resultsChecker;
-    private final WinningNumbersResultDto winningNumbersResult;
+    private final NumbersDateMessageDto winningNumbersResult;
 
-    public ResultsCheckerFacade(ResultsChecker resultsChecker, WinningNumbersResultDto winningNumbersResult) {
+    public ResultsCheckerFacade(ResultsChecker resultsChecker, NumbersDateMessageDto winningNumbersResult) {
         this.resultsChecker = resultsChecker;
         this.winningNumbersResult = winningNumbersResult;
     }
@@ -26,21 +26,21 @@ public class ResultsCheckerFacade {
         throw new IllegalArgumentException(WINNER_NUMBERS_NOT_FOUND);
     }
 
-    ResultsLotto getAllResults(ResultsLotto results) {
-        if (resultsChecker.checkWinnerNumbers(results.numbersUser(), results.winningNumbers())) {
+    ResultsLotto getWinners(String id, ResultsLotto results) {
+        if (resultsChecker.checkWinnerNumbers(results.numbersUser(), results.lottoNumbers())) {
             String successResult = ResultsCheckerMessageProvider.WIN;
-            return new ResultsLotto(results.uuid(), results.numbersUser(), results.winningNumbers(), results.drawDate(), successResult);
+            return new ResultsLotto(id, results.numbersUser(), results.lottoNumbers(), results.drawDate(), successResult);
         }
         return Optional.of(results).get();
     }
 
-    Set<Integer> getWinners(String uuid, ResultsLotto results) {
+    Set<Integer> getWinnersByUUID(ResultsLotto results) {
         Set<Integer> numbersFromUser = results.numbersUser();
-        Set<Integer> winnerNumbers = results.winningNumbers();
-        if (resultsChecker.checkWinnerNumbers(numbersFromUser, winnerNumbers)) {
-            WinningNumbersRepository winningNumbersRepository = new InMemoryWinningNumbersRepository();
-            return winningNumbersRepository.findWinningNumbers(uuid);
+        Set<Integer> numbersFromLotto = results.lottoNumbers();
+        if (resultsChecker.checkWinnerNumbers(numbersFromUser, numbersFromLotto)) {
+            NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
+            return numberReceiverRepository.findByUUID(results.uuid());
         }
-        return Optional.of(results.winningNumbers()).orElseThrow();
+        throw new IllegalArgumentException();
     }
 }
