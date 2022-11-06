8f70382cea2cbe7f3c5c7bc2fc9bff2fb52ec91c
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 21:58:12 +0100

    Refactor code

---
+++ src/main/java/pl/lotto/resultchecker/ResultsLottoRepository.java
@@ -0,0 +1,11 @@
+package pl.lotto.resultchecker;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+interface ResultsLottoRepository {
+    Set<ResultsLotto> findByDate(LocalDateTime dateTime, boolean isWinner);
+    Set<ResultsLotto> findByUUID(UUID uuid, boolean isWinner);
+    ResultsLotto save(ResultsLotto results);
+}
