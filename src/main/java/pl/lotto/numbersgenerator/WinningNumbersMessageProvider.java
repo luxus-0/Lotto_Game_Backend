31f82e8eb1b65d0dfe424cbaa195213ce29dda39
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-06 15:50:20 +0100

    Refactor code

--- src/main/java/pl/lotto/numbersgenerator/WinningNumbersMessageProvider.java
+++
@@ -1,6 +0,0 @@
-package pl.lotto.numbersgenerator;
-
-public class WinningNumbersMessageProvider {
-    public final static String SUCCESS = "win";
-    public final static String FAILED = "not win";
-}
