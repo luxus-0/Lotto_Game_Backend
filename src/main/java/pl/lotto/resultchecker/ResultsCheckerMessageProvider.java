ee96608b6a204efd94015a8d49499dda82398112
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-07 20:26:33 +0100

    Reformat code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerMessageProvider.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerMessageProvider.java
@@ -4,8 +4,4 @@ class ResultsCheckerMessageProvider {
     public final static String WIN = "win";
     public final static String NOT_WIN = "not win";
     public final static String WINNER_NUMBERS_NOT_FOUND = "results not found";
-
-    ResultsCheckerMessageProvider(ResultsCheckerValidator resultsChecker) {
-        this.resultsChecker = resultsChecker;
-    }
 }
