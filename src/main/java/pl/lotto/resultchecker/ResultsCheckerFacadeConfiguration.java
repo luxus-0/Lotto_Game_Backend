da9dd468b295df8a93531964c7a56c6bb25c79b8
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-07 23:59:08 +0100

    Reformat code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacadeConfiguration.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacadeConfiguration.java
@@ -1,10 +1,13 @@
 package pl.lotto.resultchecker;
 
+import pl.lotto.numberreceiver.NumbersReceiverValidator;
+
 public class ResultsCheckerFacadeConfiguration {
 
     ResultsCheckerFacade createModuleForTests() {
-        ResultsCheckerValidator resultsChecker = new ResultsCheckerValidator();
-        ResultsCheckerRepository resultsLottoRepository = new InMemoryResultsCheckerRepository();
-        return new ResultsCheckerFacade(resultsChecker, resultsLottoRepository);
+        ResultsCheckerValidator resultsCheckerValidator = new ResultsCheckerValidator();
+        NumbersReceiverValidator numbersReceiverValidator = new NumbersReceiverValidator();
+        ResultsCheckerRepository resultsCheckerRepository = new InMemoryResultsCheckerRepository();
+        return new ResultsCheckerFacade(resultsCheckerValidator, numbersReceiverValidator, resultsCheckerRepository);
     }
 }
