330b2324c0b36778f690ba9f81514fa7f6fca081
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-07 23:58:54 +0100

    Reformat code

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverRepository.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverRepository.java
@@ -2,12 +2,12 @@ package pl.lotto.numberreceiver;
 
 import java.time.LocalDateTime;
 import java.util.Set;
-import java.util.UUID;
 
 public interface NumberReceiverRepository {
     Set<Integer> save(NumberReceiver numberReceiver);
 
     Set<Integer> findByDate(LocalDateTime drawDate);
 
-    Set<Integer> findByUUID(UUID uuid);
+
+    Set<Integer> findByUUID(String uuid);
 }
