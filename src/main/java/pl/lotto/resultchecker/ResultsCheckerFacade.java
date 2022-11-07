bd3fb5a7d2a7dc954c0e50f82275256f891364f2
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-07 20:26:20 +0100

    Reformat code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
@@ -11,9 +11,9 @@ import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WINNER_NUMBER
 
 public class ResultsCheckerFacade {
     private final ResultsCheckerValidator validator;
-    private final ResultsLottoRepository resultsLottoRepository;
+    private final ResultsCheckerRepository resultsLottoRepository;
 
-    public ResultsCheckerFacade(ResultsCheckerValidator validator, ResultsLottoRepository resultsLottoRepository) {
+    public ResultsCheckerFacade(ResultsCheckerValidator validator, ResultsCheckerRepository resultsLottoRepository) {
         this.validator = validator;
         this.resultsLottoRepository = resultsLottoRepository;
     }
@@ -30,16 +30,13 @@ public class ResultsCheckerFacade {
         throw new IllegalArgumentException(WINNER_NUMBERS_NOT_FOUND);
     }
 
-    public ResultsLotto getWinners(UUID uuid, ResultsLotto results) {
-    }
-
-    public Set<Integer> getWinnersByUUID(ResultsLotto results) {
+    public Set<Integer> getWinnersByUUID(UUID uuid, ResultsLotto results) {
         Set<Integer> numbersFromUser = results.numbersUser();
         Set<Integer> numbersFromLotto = results.lottoNumbers();
         boolean validation = validator.checkWinnerNumbers(numbersFromUser, numbersFromLotto);
         if (validation) {
             NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
-            return numberReceiverRepository.findByUUID(results.uuid());
+            return numberReceiverRepository.findByUUID(uuid);
         }
         throw new IllegalArgumentException();
     }
@@ -47,7 +44,7 @@ public class ResultsCheckerFacade {
     public Set<Integer> getWinnersByDateDraw(ResultsLotto results) {
         Set<Integer> numbersFromUser = results.numbersUser();
         Set<Integer> numbersFromLotto = results.lottoNumbers();
-        if (resultsChecker.checkWinnerNumbers(numbersFromUser, numbersFromLotto)) {
+        if (validator.checkWinnerNumbers(numbersFromUser, numbersFromLotto)) {
             NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
             return numberReceiverRepository.findByDate(results.drawDate());
         }
