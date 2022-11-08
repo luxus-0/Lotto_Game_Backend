847810a35eafe2dbe1b68a5dcdd7144dc34cf40a
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-08 19:01:27 +0100

    Reformat code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacade.java
@@ -5,9 +5,13 @@ import pl.lotto.numberreceiver.NumberReceiverRepository;
 import pl.lotto.numberreceiver.NumbersReceiverValidator;
 import pl.lotto.resultchecker.exceptions.DateWinnerNotFoundException;
 
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WIN;
 import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WINNER_NUMBERS_NOT_FOUND;
 
 public class ResultsCheckerFacade {
@@ -21,36 +25,31 @@ public class ResultsCheckerFacade {
         this.resultsCheckerRepository = resultsCheckerRepository;
     }
 
-    public ResultsLotto getAllResults(ResultsLotto resultsLotto) {
-        boolean isWinnerNumbers = containWinnerNumbers(resultsLotto);
-        boolean isSizeSixNumbers = isEqualsSixNumbers(resultsLotto);
-        if (isWinnerNumbers && isSizeSixNumbers) {
-            return new ResultsLotto(resultsLotto.uuid(), resultsLotto.numbersUser(), resultsLotto.lottoNumbers(), resultsLotto.drawDate(), ResultsCheckerMessageProvider.WIN);
+    public ResultsLotto getResults(ResultsLotto resultsLotto) {
+        boolean checkWinnerNumbers = resultsValidator.isWinnerNumbers(resultsLotto.numbersUser(), resultsLotto.lottoNumbers());
+        boolean checkSizeSixNumbers = receiverValidator.isEqualsSixNumbers(resultsLotto.numbersUser());
+        if (checkWinnerNumbers && checkSizeSixNumbers) {
+            resultsCheckerRepository.save(resultsLotto);
+            return new ResultsLotto(resultsLotto.uuid(), resultsLotto.numbersUser(), resultsLotto.lottoNumbers(), resultsLotto.drawDate(), WIN);
         }
         throw new IllegalArgumentException(WINNER_NUMBERS_NOT_FOUND);
     }
 
-    private boolean isEqualsSixNumbers(ResultsLotto resultsLotto) {
-        return receiverValidator.isEqualsSixNumbers(resultsLotto.numbersUser());
+    public Set<Integer> getWinnerNumbersByUUID(Set<Integer> numberInput, Set<Integer> numbersLotto, UUID uuid) {
+        boolean checkWinnerNumbers = resultsValidator.isWinnerNumbers(numberInput, numbersLotto);
+        Set<Integer> winNumbers = resultsCheckerRepository.findByUUID(uuid, checkWinnerNumbers);
+        return new HashSet<>(winNumbers);
     }
 
-    private boolean containWinnerNumbers(ResultsLotto resultsLotto) {
-        return resultsValidator.checkWinnerNumbers(resultsLotto.numbersUser(), resultsLotto.lottoNumbers());
+    public Set<Integer> getWinnersNumbersByDateDraw(ResultsLotto results) {
+        NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
+        Set<Integer> winNumbersByDate = numberReceiverRepository.findByDate(results.drawDate());
+        return winNumbersByDate.stream()
+                .filter(checkWin -> isWinningNumbers(results))
+                .collect(Collectors.toSet());
     }
 
-    public Set<Integer> getResultsByUUID(String uuid, ResultsLotto results) {
-        if (containWinnerNumbers(results) && isEqualsSixNumbers(results)) {
-            NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
-            return numberReceiverRepository.findByUUID(uuid);
-        }
-        throw new IllegalArgumentException();
-    }
-
-    public Set<Integer> getWinnersByDateDraw(ResultsLotto results) {
-        if (containWinnerNumbers(results) && isEqualsSixNumbers(results)) {
-            NumberReceiverRepository numberReceiverRepository = new InMemoryNumberReceiverRepository();
-            return numberReceiverRepository.findByDate(results.drawDate());
-        }
-        throw new DateWinnerNotFoundException(results.drawDate());
+    private boolean isWinningNumbers(ResultsLotto results) {
+        return resultsValidator.isWinnerNumbers(results.numbersUser(), results.lottoNumbers());
     }
 }
