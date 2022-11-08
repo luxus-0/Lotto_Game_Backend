1390405b3de684d3a8888a8255b4ccc11859a722
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-08 23:33:30 +0100

    Change method FROM Set<ResultsLotto> findByDate(LocalDateTime dateTime, boolean isWinner) TO Set<Integer> findWinnerNumbersByDate(LocalDateTime dateTime, Set<Integer> userNumbers, Set<Integer> lottoNumbers)
    Change in filter with return method
    
    FROM
    Set<Integer> findByUUID(UUID uuid, boolean isWinner)
    TO
    Set<Integer> findWinnerNumbersByUUID(UUID uuid, Set<Integer> userNumbers, Set<Integer> lottoNumbers)
    
    FROM
    save(ResultsLotto resultsLotto)
    TO
    Set<Integer> save(UUID uuid, Set<Integer> winningNumbers)

--- src/main/java/pl/lotto/resultchecker/InMemoryResultsCheckerRepository.java
+++ src/main/java/pl/lotto/resultchecker/InMemoryResultsCheckerRepository.java
@@ -1,37 +1,40 @@
 package pl.lotto.resultchecker;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 public class InMemoryResultsCheckerRepository implements ResultsCheckerRepository {
-    private final Map<UUID, ResultsLotto> databaseInMemory = new ConcurrentHashMap<>();
+    private final Map<UUID, Set<Integer>> databaseInMemory = new ConcurrentHashMap<>();
+    private final ResultsCheckerValidator validator;
 
-    private static boolean isWinnerMessage(ResultsLotto resultsLotto) {
-        return resultsLotto.message().equals(ResultsCheckerMessageProvider.WIN);
+    public InMemoryResultsCheckerRepository(ResultsCheckerValidator validator) {
+        this.validator = validator;
     }
 
     @Override
-    public Set<ResultsLotto> findByDate(LocalDateTime dateTime, boolean isWinner) {
+    public Set<Integer> findWinnerNumbersByDate(LocalDateTime dateTime, Set<Integer> userNumbers, Set<Integer> lottoNumbers) {
         return databaseInMemory.values()
                 .stream()
-                .filter(resultsLotto -> resultsLotto.drawDate().equals(dateTime))
-                .collect(Collectors.toSet());
+                .filter(winner -> validator.isWinnerNumbers(userNumbers, lottoNumbers))
+                .findAny()
+                .orElseThrow();
     }
 
     @Override
-    public Set<Integer> findByUUID(UUID uuid, boolean isWinner) {
+    public Set<Integer> findWinnerNumbersByUUID(UUID uuid, Set<Integer> userNumbers, Set<Integer> lottoNumbers) {
         return databaseInMemory.values()
                 .stream()
-                .filter(InMemoryResultsCheckerRepository::isWinnerMessage)
-                .collect(Collectors.toSet());
+                .filter(win -> validator.isWinnerNumbers(userNumbers, lottoNumbers))
+                .findAny()
+                .orElseGet(LinkedHashSet::new);
     }
 
     @Override
-    public void save(ResultsLotto resultsLotto) {
-        databaseInMemory.put(resultsLotto.uuid(), resultsLotto);
+    public Set<Integer> save(UUID uuid, Set<Integer> winningNumbers) {
+        return databaseInMemory.put(uuid, winningNumbers);
     }
 }
