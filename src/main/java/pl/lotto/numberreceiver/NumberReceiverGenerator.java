c074e04b5929257f0558854ab4e30509d0c3d157
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-09 00:25:07 +0100

    class generate Ticket with:  uuid, numbersFromUser, dateTime

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverGenerator.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverGenerator.java
@@ -1,13 +1,8 @@
 package pl.lotto.numberreceiver;
 
 import java.time.LocalDateTime;
-import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toSet;
-import static pl.lotto.numberreceiver.NumbersReceiverMessageProvider.*;
 
 class NumberReceiverGenerator {
 
@@ -17,16 +12,5 @@ class NumberReceiverGenerator {
                 .numbersFromUser(numbersUser)
                 .drawDate(drawDate)
                 .build();
-    }
-
-    public Set<Integer> generateLottoNumbers() {
-        return IntStream.rangeClosed(SIZE_MIN, SIZE_MAX)
-                .map(mapper -> readNumbers())
-                .boxed()
-                .collect(toSet());
-    }
-
-    private int readNumbers() {
-        return new Random().nextInt(RANGE_FROM_NUMBER, RANGE_TO_NUMBER);
     }
 }
