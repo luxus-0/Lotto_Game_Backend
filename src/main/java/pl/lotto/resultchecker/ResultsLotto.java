2911c1c8d9ec8490d856c739a79b93a75147e68a
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-09 00:15:15 +0100

    Delete class Because I don't use this

--- src/main/java/pl/lotto/resultchecker/ResultsLotto.java
+++
@@ -1,9 +0,0 @@
-package pl.lotto.resultchecker;
-
-import java.time.LocalDateTime;
-import java.util.Set;
-import java.util.UUID;
-
-record ResultsLotto(UUID uuid, Set<Integer> numbersUser, Set<Integer> lottoNumbers, LocalDateTime drawDate,
-                    String message) {
-}
