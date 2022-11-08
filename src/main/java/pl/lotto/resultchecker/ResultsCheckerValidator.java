3ff4f893a5ba2b1dc7024d8fa89dd0955435aeb4
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-08 19:01:44 +0100

    Reformat code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerValidator.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerValidator.java
@@ -6,7 +6,7 @@ import static pl.lotto.numberreceiver.NumbersReceiverMessageProvider.SIZE_MAX;
 
 class ResultsCheckerValidator {
 
-    public boolean checkWinnerNumbers(Set<Integer> inputNumbers, Set<Integer> randomNumbers) {
+    public boolean isWinnerNumbers(Set<Integer> inputNumbers, Set<Integer> randomNumbers) {
         return inputNumbers.stream().filter(checkNumbers -> inputNumbers.size() <= SIZE_MAX).anyMatch(randomNumbers::contains);
     }
 }
