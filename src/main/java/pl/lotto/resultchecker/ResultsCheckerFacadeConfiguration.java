6df3147ee3039d95d0370e1405c78db7649a8696
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-08 23:51:12 +0100

    Changing in creating class TO: ResultsCheckerRepository resultsCheckerRepository = new InMemoryResultsCheckerRepository(resultsCheckerValidator);

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerFacadeConfiguration.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerFacadeConfiguration.java
@@ -7,7 +7,7 @@ public class ResultsCheckerFacadeConfiguration {
     ResultsCheckerFacade createModuleForTests() {
         ResultsCheckerValidator resultsCheckerValidator = new ResultsCheckerValidator();
         NumbersReceiverValidator numbersReceiverValidator = new NumbersReceiverValidator();
-        ResultsCheckerRepository resultsCheckerRepository = new InMemoryResultsCheckerRepository();
+        ResultsCheckerRepository resultsCheckerRepository = new InMemoryResultsCheckerRepository(resultsCheckerValidator);
         return new ResultsCheckerFacade(resultsCheckerValidator, numbersReceiverValidator, resultsCheckerRepository);
     }
 }
