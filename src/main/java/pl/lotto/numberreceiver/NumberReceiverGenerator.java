f00d0d2e1761decf2c40ba229362f2051266e017
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-08 23:44:28 +0100

    Change name methods: FROM generateUserTicket(numbersUser,date) TO generateTicket(uuid, numbersUser, date)
    FROM
    generateLottoTicket()
    TO
    generateLottoNumbers();
    
    REFACTOR ALL CODE

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverGenerator.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverGenerator.java
@@ -4,19 +4,14 @@ import java.time.LocalDateTime;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.toSet;
 import static pl.lotto.numberreceiver.NumbersReceiverMessageProvider.*;
 
 class NumberReceiverGenerator {
 
-    private static int readNumbers() {
-        return new Random().nextInt(RANGE_FROM_NUMBER, RANGE_TO_NUMBER);
-    }
-
-    public NumberReceiver generateUserTicket(Set<Integer> numbersUser, LocalDateTime drawDate) {
-        String uuid = UUID.randomUUID().toString();
+    public NumberReceiver generateTicket(UUID uuid, Set<Integer> numbersUser, LocalDateTime drawDate) {
         return NumberReceiver.builder()
                 .uuid(uuid)
                 .numbersFromUser(numbersUser)
@@ -24,10 +19,14 @@ class NumberReceiverGenerator {
                 .build();
     }
 
-    public Set<Integer> generateLottoTicket() {
+    public Set<Integer> generateLottoNumbers() {
         return IntStream.rangeClosed(SIZE_MIN, SIZE_MAX)
                 .map(mapper -> readNumbers())
                 .boxed()
-                .collect(Collectors.toSet());
+                .collect(toSet());
+    }
+
+    private int readNumbers() {
+        return new Random().nextInt(RANGE_FROM_NUMBER, RANGE_TO_NUMBER);
     }
 }
