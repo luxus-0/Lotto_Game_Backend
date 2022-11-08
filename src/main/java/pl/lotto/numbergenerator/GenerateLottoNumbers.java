b78780d7e90a7e85f7896c89bfb8b001afc1b2f0
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-09 00:23:12 +0100

    Add class with method generate() - random numbers

---
+++ src/main/java/pl/lotto/numbergenerator/GenerateLottoNumbers.java
@@ -0,0 +1,22 @@
+package pl.lotto.numbergenerator;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toSet;
+import static pl.lotto.numberreceiver.NumbersReceiverMessageProvider.*;
+import static pl.lotto.numberreceiver.NumbersReceiverMessageProvider.RANGE_TO_NUMBER;
+
+class GenerateLottoNumbers {
+    public Set<Integer> generate() {
+        return IntStream.rangeClosed(SIZE_MIN, SIZE_MAX)
+                .map(mapToRandom -> readNumbers())
+                .boxed()
+                .collect(toSet());
+    }
+
+    private int readNumbers() {
+        return new Random().nextInt(RANGE_FROM_NUMBER, RANGE_TO_NUMBER);
+    }
+}
