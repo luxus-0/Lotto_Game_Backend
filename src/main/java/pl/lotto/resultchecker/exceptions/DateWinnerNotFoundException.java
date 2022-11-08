c047d2b915bd813c4aa6e2dfa71555a3bdef2f0c
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-08 20:14:47 +0100

    Delete not used class

--- src/main/java/pl/lotto/resultchecker/exceptions/DateWinnerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pl.lotto.resultchecker.exceptions;
-
-import java.time.LocalDateTime;
-
-public class DateWinnerNotFoundException extends RuntimeException {
-    public DateWinnerNotFoundException(LocalDateTime dateTime) {
-        System.err.println("In Date: " + dateTime + " numbers winner not found");
-    }
-}
