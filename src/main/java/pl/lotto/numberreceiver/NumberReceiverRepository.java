513980ed1468eb27eed56e75ff6d943878dd1744
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-10 00:14:37 +0100

    Refactor code

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverRepository.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverRepository.java
@@ -2,12 +2,11 @@ package pl.lotto.numberreceiver;
 
 import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.UUID;
 
 public interface NumberReceiverRepository {
-    Set<Integer> save(NumberReceiver numberReceiver);
-
-    Set<Integer> findByDate(LocalDateTime drawDate);
-
-
-    Set<Integer> findByUUID(String uuid);
+    Set<Integer> save(UUID uuid, Set<Integer> numbersFromUser);
+    Set<Integer> save(LocalDateTime dateTime, Set<Integer> numbersFromUser);
+    Set<Integer> findByDate(String dateTime);
+    Set<Integer> findByUUID(UUID uuid);
 }
