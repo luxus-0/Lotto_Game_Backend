cfc84f65c4abc4c8d569aecc712ee1adbdebd8f5
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-10 00:14:59 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerRepository.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerRepository.java
@@ -5,7 +5,7 @@ import java.util.Set;
 import java.util.UUID;
 
 interface ResultsCheckerRepository {
-    Set<Integer> findWinnerNumbersByDate(LocalDateTime dateTime, Set<Integer> userNumbers, Set<Integer> lottoNumbers);
-    Set<Integer> findWinnerNumbersByUUID(UUID uuid, Set<Integer> userNumbers, Set<Integer> lottoNumbers);
+    Set<Integer> findWinnerNumbersByDate(LocalDateTime dateTime, Set<Integer> userNumbers);
+    Set<Integer> findWinnerNumbersByUUID(UUID uuid, Set<Integer> userNumbers);
     Set<Integer> save(UUID uuid, Set<Integer> winningNumbers);
 }
