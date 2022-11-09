d5bb4679b75eff18da6a9829479dc9778d9c2461
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-10 00:15:04 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/ResultsCheckerValidator.java
+++ src/main/java/pl/lotto/resultchecker/ResultsCheckerValidator.java
@@ -1,12 +1,16 @@
 package pl.lotto.resultchecker;
 
+import pl.lotto.numbergenerator.GenerateLottoNumbers;
+
 import java.util.Set;
 
 import static pl.lotto.numberreceiver.NumbersReceiverMessageProvider.SIZE_MAX;
 
 class ResultsCheckerValidator {
 
-    public boolean isWinnerNumbers(Set<Integer> inputNumbers, Set<Integer> randomNumbers) {
-        return inputNumbers.stream().filter(checkNumbers -> inputNumbers.size() <= SIZE_MAX).anyMatch(randomNumbers::contains);
+    public boolean isWinnerNumbers(Set<Integer> inputNumbers) {
+        GenerateLottoNumbers generateLottoNumbers = new GenerateLottoNumbers();
+        Set<Integer> lottoNumbers = generateLottoNumbers.generate();
+        return inputNumbers.stream().filter(checkNumbers -> inputNumbers.size() <= SIZE_MAX).anyMatch(lottoNumbers::contains);
     }
 }
