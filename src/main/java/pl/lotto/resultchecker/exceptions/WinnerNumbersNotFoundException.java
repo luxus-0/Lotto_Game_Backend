946563b7ee881e7204678cbc0e4042b630654a73
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-10 01:46:45 +0100

    Refactor code

--- src/main/java/pl/lotto/resultchecker/exceptions/WinnerNumbersNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pl.lotto.resultchecker.exceptions;
-
-import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WINNER_NUMBERS_NOT_FOUND;
-
-public class WinnerNumbersNotFoundException extends RuntimeException{
-    public WinnerNumbersNotFoundException(){
-        System.err.println(WINNER_NUMBERS_NOT_FOUND);
-    }
-}
