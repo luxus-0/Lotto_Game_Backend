b1653caaa7225cdc58d6612f7760ef39d97d5fcd
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-11 01:06:30 +0100

    Change record parameter: FROM
    public record NumbersMessageDto(Set<Integer> inputNumber, List<String> messages) {
    }
    
    TO
    
    public record NumbersMessageDto(Set<Integer> inputNumber, String message) {
    }

--- src/main/java/pl/lotto/numberreceiver/dto/NumbersMessageDto.java
+++ src/main/java/pl/lotto/numberreceiver/dto/NumbersMessageDto.java
@@ -1,7 +1,6 @@
 package pl.lotto.numberreceiver.dto;
 
-import java.util.List;
 import java.util.Set;
 
-public record NumbersMessageDto(Set<Integer> inputNumber, List<String> messages) {
+public record NumbersMessageDto(Set<Integer> inputNumber, String message) {
 }
