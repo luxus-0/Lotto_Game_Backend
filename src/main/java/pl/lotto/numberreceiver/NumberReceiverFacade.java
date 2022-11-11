07ca32c5bfc30974a37f8d3b34212ef526c54950
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-11 02:16:28 +0100

    Add in method:  userInput(numbersInput)
     NumberReceiver numberReceiver = numberReceiverGenerator.generateTicket(uuid,numbersFromUser, drawDate);
                 NumberReceiver savedReceiver = numberReceiverRepository.save(numberReceiver);
                 return new NumbersMessageDto(savedReceiver.numbersFromUser(), messageValidation);

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -11,13 +11,16 @@ public class NumberReceiverFacade {
 
     private final NumbersReceiverValidator numberValidator;
     private final NumberReceiverRepository numberReceiverRepository;
+    private final NumberReceiverGenerator numberReceiverGenerator;
     private final LocalDateTime drawDateTime;
 
-    public NumberReceiverFacade(NumbersReceiverValidator numberValidator, NumberReceiverRepository numberReceiverRepository, LocalDateTime drawDateTime, NumberReceiverGenerator numberReceiverGenerator) {
+    public NumberReceiverFacade(NumbersReceiverValidator numberValidator, NumberReceiverRepository numberReceiverRepository,LocalDateTime drawDateTime, NumberReceiverGenerator numberReceiverGenerator) {
         this.numberValidator = numberValidator;
         this.numberReceiverRepository = numberReceiverRepository;
+        this.numberReceiverGenerator = numberReceiverGenerator;
         this.drawDateTime = drawDateTime;
     }
+
 
     public NumbersMessageDto inputNumbers(Set<Integer> numbersFromUser) {
         boolean validate = numberValidator.validate(numbersFromUser);
@@ -27,9 +30,9 @@ public class NumberReceiverFacade {
             DateTimeReceiver ticketDrawDate = new DateTimeReceiver(clock);
             UUID uuid = UUID.randomUUID();
             LocalDateTime drawDate = ticketDrawDate.generateDrawDate(drawDateTime);
-            Set<Integer> savedUUIDNumbers = numberReceiverRepository.save(uuid, numbersFromUser);
-            numberReceiverRepository.save(drawDate, savedUUIDNumbers);
-                return new NumbersMessageDto(numbersFromUser, messageValidation);
+            NumberReceiver numberReceiver = numberReceiverGenerator.generateTicket(uuid,numbersFromUser, drawDate);
+            NumberReceiver savedReceiver = numberReceiverRepository.save(numberReceiver);
+            return new NumbersMessageDto(savedReceiver.numbersFromUser(), messageValidation);
         }
         return new NumbersMessageDto(numbersFromUser, messageValidation);
     }
