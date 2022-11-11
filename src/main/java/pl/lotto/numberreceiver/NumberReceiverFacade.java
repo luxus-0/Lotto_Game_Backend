e156fe07efa2832033bcac693c36ccfcb9c7526d
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-11 01:02:19 +0100

    Change in method inputNumbers(numbersFromUser): Add messageValidation with return String Return method new NumbersMessageDto(Set numbers, String message);

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -4,8 +4,6 @@ import pl.lotto.numberreceiver.dto.NumbersMessageDto;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -14,30 +12,25 @@ public class NumberReceiverFacade {
     private final NumbersReceiverValidator numberValidator;
     private final NumberReceiverRepository numberReceiverRepository;
     private final LocalDateTime drawDateTime;
-    private final NumberReceiverGenerator numberReceiverGenerator;
 
     public NumberReceiverFacade(NumbersReceiverValidator numberValidator, NumberReceiverRepository numberReceiverRepository, LocalDateTime drawDateTime, NumberReceiverGenerator numberReceiverGenerator) {
         this.numberValidator = numberValidator;
         this.numberReceiverRepository = numberReceiverRepository;
         this.drawDateTime = drawDateTime;
-        this.numberReceiverGenerator = numberReceiverGenerator;
     }
 
     public NumbersMessageDto inputNumbers(Set<Integer> numbersFromUser) {
         boolean validate = numberValidator.validate(numbersFromUser);
+        String messageValidation = numberValidator.messages.stream().findAny().orElseThrow();
         if (validate) {
             Clock clock = Clock.systemUTC();
             DateTimeReceiver ticketDrawDate = new DateTimeReceiver(clock);
             UUID uuid = UUID.randomUUID();
             LocalDateTime drawDate = ticketDrawDate.generateDrawDate(drawDateTime);
             Set<Integer> savedUUIDNumbers = numberReceiverRepository.save(uuid, numbersFromUser);
-            Set<Integer> savedTicket = numberReceiverRepository.save(drawDate, savedUUIDNumbers);
-            return new NumbersMessageDto(savedTicket, numberValidator.messages);
+            numberReceiverRepository.save(drawDate, savedUUIDNumbers);
+                return new NumbersMessageDto(numbersFromUser, messageValidation);
         }
-        return throwNumbersNotFoundException();
-    }
-
-    private NumbersMessageDto throwNumbersNotFoundException() {
-        return Optional.of(new NumbersMessageDto(null, numberValidator.messages)).orElseThrow();
+        return new NumbersMessageDto(numbersFromUser, messageValidation);
     }
 }
