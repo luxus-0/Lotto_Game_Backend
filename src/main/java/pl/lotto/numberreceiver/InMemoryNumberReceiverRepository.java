822326ea4b0f62e6ea12f55714d1148eaf6d9264
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-11 02:14:59 +0100

    Change in methods:  save(UUID, numbers)  save(LocalDateTime, numbers)
     TO
    
     save(NumberReceiver)

--- src/main/java/pl/lotto/numberreceiver/InMemoryNumberReceiverRepository.java
+++ src/main/java/pl/lotto/numberreceiver/InMemoryNumberReceiverRepository.java
@@ -8,28 +8,25 @@ import java.util.Set;
 import java.util.UUID;
 
 public class InMemoryNumberReceiverRepository implements NumberReceiverRepository {
-    private final Map<UUID, Set<Integer>> uniqueNumbersMap = new HashMap<>();
-    private final Map<LocalDateTime, Set<Integer>> dateTimeNumbersMap = new HashMap<>();
+    private final Map<UUID, Set<Integer>> databaseInMemory = new HashMap<>();
+    private final Map<LocalDateTime, Set<Integer>> dateTimeInMemory = new HashMap<>();
 
     @Override
-    public Set<Integer> save(UUID uuid, Set<Integer> numbersFromUser) {
-        return uniqueNumbersMap.put(uuid, numbersFromUser);
-    }
-
-    @Override
-    public Set<Integer> save(LocalDateTime dateTime, Set<Integer> numbersFromUser) {
-        return dateTimeNumbersMap.put(dateTime, numbersFromUser);
+    public NumberReceiver save(NumberReceiver numberReceiver) {
+        databaseInMemory.put(numberReceiver.uuid(), numberReceiver.numbersFromUser());
+        dateTimeInMemory.put(numberReceiver.drawDate(), numberReceiver.numbersFromUser());
+        return new NumberReceiver(numberReceiver.uuid(), numberReceiver.numbersFromUser(), numberReceiver.drawDate());
     }
 
     @Override
     public Set<Integer> findByDate(String dateTime) {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
         LocalDateTime drawDateTime = LocalDateTime.parse(dateTime, dateFormat);
-        return dateTimeNumbersMap.get(drawDateTime);
+        return dateTimeInMemory.get(drawDateTime);
     }
 
     @Override
     public Set<Integer> findByUUID(UUID uuid) {
-        return uniqueNumbersMap.get(uuid);
+        return databaseInMemory.get(uuid);
     }
 }
