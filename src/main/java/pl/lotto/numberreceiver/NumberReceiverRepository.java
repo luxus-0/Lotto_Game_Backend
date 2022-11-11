78b532e8dd04b01e74be012868f63fbcb8ab56ce
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-11 02:16:59 +0100

    Change in repository to:
    NumberReceiver save(NumberReceiver numberReceiver);

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverRepository.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverRepository.java
@@ -1,12 +1,10 @@
 package pl.lotto.numberreceiver;
 
-import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
 public interface NumberReceiverRepository {
-    Set<Integer> save(UUID uuid, Set<Integer> numbersFromUser);
-    Set<Integer> save(LocalDateTime dateTime, Set<Integer> numbersFromUser);
+    NumberReceiver save(NumberReceiver numberReceiver);
     Set<Integer> findByDate(String dateTime);
     Set<Integer> findByUUID(UUID uuid);
 }
