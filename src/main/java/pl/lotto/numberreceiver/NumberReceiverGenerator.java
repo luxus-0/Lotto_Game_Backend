e85e3967d47a553bb4e13a7e5c0bb5aeec929f35
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-12 17:17:29 +0100

    Refactor code

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverGenerator.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverGenerator.java
@@ -1,15 +1,16 @@
 package pl.lotto.numberreceiver;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
 class NumberReceiverGenerator {
 
-    public NumberReceiver generateTicket(UUID uuid, Set<Integer> numbersUser, boolean isCorrectDateTimeDraw) {
+    public NumberReceiver generateTicket(UUID uuid, Set<Integer> numbersUser, LocalDateTime drawDate) {
         return NumberReceiver.builder()
                 .uuid(uuid)
                 .numbersFromUser(numbersUser)
-                .isCorrectDateTimeDraw(isCorrectDateTimeDraw)
+                .drawDate(drawDate)
                 .build();
     }
 }
