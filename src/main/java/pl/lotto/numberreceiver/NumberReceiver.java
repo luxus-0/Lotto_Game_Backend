d8e758cb02de6448fd2bf36e7b31e4e5d4fd5efc
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-14 00:59:39 +0100

    Change name field to LocalDateTime dateTimeDraw

--- src/main/java/pl/lotto/numberreceiver/NumberReceiver.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiver.java
@@ -2,10 +2,9 @@ package pl.lotto.numberreceiver;
 
 import lombok.Builder;
 
-import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
 @Builder
-public record NumberReceiver(UUID uuid, Set<Integer> numbersFromUser, LocalDateTime drawDate) {
+public record NumberReceiver(UUID uuid, Set<Integer> numbersFromUser, DateTimeDraw dateTimeDraw) {
 }
