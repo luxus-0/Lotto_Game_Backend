01065d50b07c0c0839356dd06c0c92f47cc39b29
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-14 00:55:12 +0100

    Add messages:  success draw date time  and  failed draw date time

---
+++ src/main/java/pl/lotto/numberreceiver/DateTimeDrawMessageProvider.java
@@ -0,0 +1,6 @@
+package pl.lotto.numberreceiver;
+
+class DateTimeDrawMessageProvider {
+    public static final String SUCCESS_DRAW_DATE_TIME = "SUCCESS";
+    public static final String FAILED_DRAW__DATE_TIME = "FAILED";
+}
