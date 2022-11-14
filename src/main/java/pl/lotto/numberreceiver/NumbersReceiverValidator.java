8e37e1692c275ed2ea4e47be470e0647eba28c9c
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-14 01:49:49 +0100

    Refactor code

--- src/main/java/pl/lotto/numberreceiver/NumbersReceiverValidator.java
+++ src/main/java/pl/lotto/numberreceiver/NumbersReceiverValidator.java
@@ -12,7 +12,7 @@ public class NumbersReceiverValidator {
     List<String> messages = new LinkedList<>();
 
     public boolean validate(Set<Integer> inputNumbers) {
-        if (isEqualsSixNumbers(inputNumbers)) {
+        if (isEqualsSixNumbers(inputNumbers) && isPositiveNumbers(inputNumbers)) {
             messages.add(EQUALS_SIX_NUMBERS);
         } else if (isLessThanSixNumbers(inputNumbers)) {
             messages.add(LESS_THAN_SIX_NUMBERS);
@@ -20,10 +20,22 @@ public class NumbersReceiverValidator {
             messages.add(MORE_THAN_SIX_NUMBERS);
         } else if (isNotInRangeNumbers(inputNumbers)) {
             messages.add(NOT_IN_RANGE_NUMBERS);
-        } else {
+        } else if(isEmptyNumbers(inputNumbers)){
             messages.add(NUMBERS_IS_EMPTY);
         }
         return false;
+    }
+
+    public boolean isPositiveNumbers(Set<Integer> inputNumbers) {
+        Integer positiveNumber = inputNumbers.stream().findAny().orElse(0);
+        if(positiveNumber > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEmptyNumbers(Set<Integer> inputNumbers) {
+        return inputNumbers.isEmpty();
     }
 
     public boolean isLessThanSixNumbers(Collection<Integer> inputNumbers) {
