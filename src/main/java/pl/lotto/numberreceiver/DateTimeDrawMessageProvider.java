c849857f7b40f8b063da7a4c189c03838a0bcb0c
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-14 01:49:14 +0100

    Refactor code

--- src/main/java/pl/lotto/numberreceiver/DateTimeDrawMessageProvider.java
+++ src/main/java/pl/lotto/numberreceiver/DateTimeDrawMessageProvider.java
@@ -2,5 +2,5 @@ package pl.lotto.numberreceiver;
 
 class DateTimeDrawMessageProvider {
     public static final String SUCCESS_DRAW_DATE_TIME = "SUCCESS";
-    public static final String FAILED_DRAW__DATE_TIME = "FAILED";
+    public static final String FAILED_DRAW_DATE_TIME = "FAILED";
 }
