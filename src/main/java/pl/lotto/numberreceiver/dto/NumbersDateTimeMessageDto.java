49a6490b1d306d39470f989091fee4632bae495d
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-14 01:08:35 +0100

    Delete unused class

--- src/main/java/pl/lotto/numberreceiver/dto/NumbersDateTimeMessageDto.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto.numberreceiver.dto;
-
-import java.time.LocalDateTime;
-import java.util.Set;
-
-public record NumbersDateTimeMessageDto(Set<Integer> numbers, String numbersMessage ,LocalDateTime dateTimeDraw, boolean isDrawDate) {
-}
--- src/main/java/pl/lotto/numberreceiver/dto/NumbersMessageDto.java
+++
@@ -1,6 +0,0 @@
-package pl.lotto.numberreceiver.dto;
-
-import java.util.Set;
-
-public record NumbersMessageDto(Set<Integer> inputNumber, String message) {
-}
