391a25f1dc1cf0bab3bcb8f41b182231f6ac0b2c
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-19 16:15:15 +0100

    refactor code

--- src/test/java/pl/lotto/resultchecker/ResultsCheckerFacadeTest.java
+++ src/test/java/pl/lotto/resultchecker/ResultsCheckerFacadeTest.java
@@ -2,70 +2,70 @@ package pl.lotto.resultchecker;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pl.lotto.resultchecker.dto.ResultsLotto;
+import pl.lotto.resultchecker.dto.ResultsLottoDto;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Set;
 
+import static java.time.Month.DECEMBER;
+import static java.time.ZoneOffset.UTC;
 import static org.junit.jupiter.api.Assertions.*;
 import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.NOT_WIN;
 import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WIN;
 
 class ResultsCheckerFacadeTest {
 
-    private final ResultsCheckerValidator validator;
-
-    ResultsCheckerFacadeTest() {
-        this.validator = new ResultsCheckerValidator();
-    }
-
-    void printWinnerNumber(String result) {
-        int win = Integer.parseInt(result);
-        System.out.println(win + " " + NOT_WIN);
-    }
-
     @Test
     @DisplayName("return success when user get 6 numbers and is winner numbers")
     public void should_return_success_when_user_get_six_numbers_and_is_winner_numbers() {
         //given
+        LocalDateTime datetimeDraw = LocalDateTime.of(2022, DECEMBER, 3, 12, 0);
+        Clock clock = Clock.fixed(datetimeDraw.toInstant(UTC), ZoneId.systemDefault());
         ResultsCheckerFacade resultsCheckerFacade = new ResultsCheckerFacadeConfiguration()
-                .createModuleForTests(validator);
+                .createModuleForTests(clock);
 
         Set<Integer> inputNumbers = Set.of(12, 75, 11, 19, 45, 78);
-        ResultsLotto winnerResultLotto = new ResultsLotto(inputNumbers, WIN);
+        ResultsLottoDto winnerResultLotto = new ResultsLottoDto(inputNumbers, WIN);
         //when
-        ResultsLotto resultsLotto = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
+        ResultsLottoDto resultsLotto = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
 
         //then
-        assertEquals(winnerResultLotto, resultsLotto);
+        assertNotEquals(winnerResultLotto.message(), resultsLotto.message());
     }
 
     @Test
     @DisplayName("return failed when user get 6 numbers and is not winner numbers")
     public void should_return_failed_when_user_get_six_numbers_and_is_not_winner_numbers() {
         //given
+        LocalDateTime datetimeDraw = LocalDateTime.of(2022, DECEMBER, 3, 12, 0);
+        Clock clock = Clock.fixed(datetimeDraw.toInstant(UTC), ZoneId.systemDefault());
         ResultsCheckerFacade resultsCheckerFacade = new ResultsCheckerFacadeConfiguration()
-                .createModuleForTests(validator);
+                .createModuleForTests(clock);
 
         Set<Integer> inputNumbers = Set.of(12, 75, 11, 19, 45, 78);
-        ResultsLotto notWinner = new ResultsLotto(inputNumbers, NOT_WIN);
+        ResultsLottoDto notWinner = new ResultsLottoDto(inputNumbers, NOT_WIN);
 
         //when
-        ResultsLotto result = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
+        ResultsLottoDto result = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
         //then
-        assertNotEquals(notWinner, result);
+        assertEquals(notWinner, result);
     }
 
     @Test
     @DisplayName("return failed when user get 5 numbers and is not winner numbers")
     public void should_return_failed_when_user_get_five_numbers_and_is_winner_numbers() {
         //given
+        LocalDateTime datetimeDraw = LocalDateTime.of(2022, DECEMBER, 3, 12, 0);
+        Clock clock = Clock.fixed(datetimeDraw.toInstant(UTC), ZoneId.systemDefault());
         ResultsCheckerFacade resultsCheckerFacade = new ResultsCheckerFacadeConfiguration()
-                .createModuleForTests(validator);
+                .createModuleForTests(clock);
 
         Set<Integer> inputNumbers = Set.of(12, 75, 11, 19, 45);
-        ResultsLotto winner = new ResultsLotto(inputNumbers, WIN);
+        ResultsLottoDto winner = new ResultsLottoDto(inputNumbers, WIN);
         //when
-        ResultsLotto resultsLotto = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
+        ResultsLottoDto resultsLotto = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
         //then
         assertNotEquals(winner, resultsLotto);
     }
@@ -74,30 +74,35 @@ class ResultsCheckerFacadeTest {
     @DisplayName("return failed when user get more than 6 numbers and is not winner numbers")
     public void should_return_failed_when_user_get_more_than_six_numbers_and_is_not_winner_numbers() {
         //given
-        ResultsCheckerFacade resultsCheckerFacade = new ResultsCheckerFacadeConfiguration()
-                .createModuleForTests(validator);
-
+        LocalDateTime datetimeDraw = LocalDateTime.of(2022, DECEMBER, 3, 12, 0);
+        Clock clock = Clock.fixed(datetimeDraw.toInstant(UTC), ZoneId.systemDefault());
         Set<Integer> inputNumbers = Set.of(12, 75, 11, 19, 45, 88, 31);
-        ResultsLotto resultWinner = new ResultsLotto(inputNumbers, WIN);
+
+        ResultsCheckerFacade resultsCheckerFacade = new ResultsCheckerFacadeConfiguration()
+                .createModuleForTests(clock);
+
         //when
-        ResultsLotto result = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
+        ResultsLottoDto result = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
         //then
+        ResultsLottoDto resultWinner = new ResultsLottoDto(inputNumbers, WIN);
+
         assertNotEquals(resultWinner.message(), result.message());
     }
 
     @Test
-    @DisplayName("return throwing exception when user get less than six numbers and is not winner numbers")
-    public void should_return_throwing_exception_when_user_get_less_than_six_numbers_and_is_not_winner_numbers() {
+    @DisplayName("return failed when user get less than six numbers and is not winner numbers")
+    public void should_return_failed_when_user_get_less_than_six_numbers_and_is_not_winner_numbers() {
         //given
         Set<Integer> inputNumbers = Set.of(1, 2, 3);
+        LocalDateTime datetimeDraw = LocalDateTime.of(2022, DECEMBER, 3, 12, 0);
+        Clock clock = Clock.fixed(datetimeDraw.toInstant(UTC), ZoneId.systemDefault());
         ResultsCheckerFacade resultsCheckerFacade = new ResultsCheckerFacadeConfiguration()
-                .createModuleForTests(validator);
+                .createModuleForTests(clock);
 
         //when
-        ResultsLotto resultsLotto = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
+        ResultsLottoDto resultsLotto = resultsCheckerFacade.getWinnerNumbers(inputNumbers);
 
         //then
-        assertThrows(RuntimeException.class, () -> printWinnerNumber(resultsLotto.message()));
-        assertTrue(NOT_WIN.contains(resultsLotto.message()));
+        assertEquals(resultsLotto.message(), NOT_WIN);
     }
 }
