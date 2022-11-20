e053922f61dda83e90085ec1fca93662e06f4e0d
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-20 20:51:08 +0100

    Refactor code

--- src/main/java/pl/lotto/resultannouncer/ResultAnnouncerFacade.java
+++ src/main/java/pl/lotto/resultannouncer/ResultAnnouncerFacade.java
@@ -1,10 +1,14 @@
 package pl.lotto.resultannouncer;
 
+import pl.lotto.resultannouncer.dto.ResultAnnouncerDto;
 import pl.lotto.resultchecker.ResultsCheckerFacade;
 import pl.lotto.resultchecker.dto.ResultsLottoDto;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
+
+import static pl.lotto.resultchecker.ResultsCheckerMessageProvider.WIN;
 
 public class ResultAnnouncerFacade {
     private final ResultsCheckerFacade resultsCheckerFacade;
@@ -13,8 +17,13 @@ public class ResultAnnouncerFacade {
         this.resultsCheckerFacade = resultsCheckerFacade;
     }
 
-    String getResultAnnouncerByUUID(UUID uuid, Set<Integer> winningNumbers){
+    ResultAnnouncerDto getResultAnnouncerByUUID(UUID uuid, Set<Integer> winningNumbers){
         ResultsLottoDto resultsLottoDto = resultsCheckerFacade.getWinnerNumbersByUUID(uuid, winningNumbers);
-        return resultsLottoDto.message();
+        LocalDateTime dateTimeResult = resultsLottoDto.dateTimeDraw();
+        boolean checkWinnerNumbers = resultsLottoDto.winnerNumbers().size() > 0;
+        if(resultsLottoDto.message().equals(WIN)) {
+            return new ResultAnnouncerDto(uuid, winningNumbers, dateTimeResult, checkWinnerNumbers);
+        }
+        return new ResultAnnouncerDto(null, null, null, false);
     }
 }
