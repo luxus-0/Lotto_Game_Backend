039fd30186501ea10c88802a1c297dfbbfceef84
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-28 22:45:11 +0100

    Empty file

--- src/main/java/pl/lotto/infrastructure/controllers/resultannouncer/ResultAnnouncerApi.java
+++ src/main/java/pl/lotto/infrastructure/controllers/resultannouncer/ResultAnnouncerApi.java
@@ -1,4 +1,7 @@
 package pl.lotto.infrastructure.controllers.resultannouncer;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ResultAnnouncerApi {
 }
