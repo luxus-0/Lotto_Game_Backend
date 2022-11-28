6b9c213e8163f193c3853c9dd5556eb236284234
Łukasz Nowogórski <luxus0@gmail.com>
2022-11-28 21:13:19 +0100

    Refactor code

---
+++ src/main/java/pl/lotto/infrastructure/controllers/NumberReceiverApi.java
@@ -0,0 +1,38 @@
+package pl.lotto.infrastructure.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.lotto.numberreceiver.NumberReceiverFacade;
+import pl.lotto.numberreceiver.dto.AllUsersNumbersDto;
+import pl.lotto.numberreceiver.dto.NumberReceiverDto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@RestController
+public class NumberReceiverApi {
+
+    private final NumberReceiverFacade numberReceiverFacade;
+
+    public NumberReceiverApi(NumberReceiverFacade numberReceiverFacade) {
+        this.numberReceiverFacade = numberReceiverFacade;
+    }
+
+    @GetMapping("/numbers")
+    public ResponseEntity<NumberReceiverDto> inputNumbers(@RequestBody NumberReceiverDto numberReceiverDto){
+        Set<Integer> responseNumbers = numberReceiverDto.numbersFromUser();
+        NumberReceiverDto numberReceiverResponse = numberReceiverFacade.inputNumbers(responseNumbers);
+        return new ResponseEntity<>(numberReceiverResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/user_numbers")
+    public ResponseEntity<AllUsersNumbersDto> userNumbers(@RequestBody NumberReceiverDto numberReceiverDto){
+        Set<Integer> responseNumbers = numberReceiverDto.numbersFromUser();
+        LocalDateTime responseDateTime = numberReceiverDto.dateTimeDraw();
+        AllUsersNumbersDto allUsersNumbersDto = numberReceiverFacade.usersNumbers(responseNumbers, responseDateTime);
+        return new ResponseEntity<>(allUsersNumbersDto, HttpStatus.OK);
+    }
+}
