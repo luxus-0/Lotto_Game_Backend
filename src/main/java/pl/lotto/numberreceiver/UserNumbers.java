5484daa9d711f7ac2cf236cda61b9d4201cbe726
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-02 21:07:39 +0100

    Remove annotation @Builder

--- src/main/java/pl/lotto/numberreceiver/UserNumbers.java
+++ src/main/java/pl/lotto/numberreceiver/UserNumbers.java
@@ -1,11 +1,8 @@
 package pl.lotto.numberreceiver;
 
-import lombok.Builder;
-
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
-@Builder
 record UserNumbers(UUID uuid, Set<Integer> numbersFromUser, LocalDateTime dateTimeDraw) {
 }
