9f83535425640ed85a74f799f616c4f05bf84541
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-03 00:08:55 +0100

    Add annotation @Document

--- src/main/java/pl/lotto/numbersgenerator/NumbersGenerator.java
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGenerator.java
@@ -1,11 +1,13 @@
 package pl.lotto.numbersgenerator;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
 import java.util.UUID;
 
 @Data
+@Document(collection = "numbersgenerator")
 class NumbersGenerator {
     private UUID uuid;
     private Set<Integer> lottoNumbers;
