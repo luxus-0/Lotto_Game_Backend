9f9802eb4231f409c0f56c97cb797ce23165c632
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-02 23:49:59 +0100

    Add MAPPER for numbers lotto random generator

---
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorMapper.java
@@ -0,0 +1,9 @@
+package pl.lotto.numbersgenerator;
+
+import pl.lotto.numbersgenerator.dto.LottoNumbersDto;
+
+class NumbersGeneratorMapper {
+    static LottoNumbersDto toDto(NumbersGenerator numbersGenerator){
+        return new LottoNumbersDto(numbersGenerator.getLottoNumbers());
+    }
+}
