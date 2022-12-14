16134b18820d9338347237479aec14264ce1cf8c
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-14 22:31:03 +0100

    Add function findNumbersGeneratorByUUID(UUID), findNumbersGenerator() createNumbersGenerator(NumbersGenerator)

--- src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorFacade.java
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorFacade.java
@@ -1,10 +1,12 @@
 package pl.lotto.numbersgenerator;
 
 import org.springframework.stereotype.Service;
+import pl.lotto.numbersgenerator.dto.LottoNumbersDto;
 import pl.lotto.numbersgenerator.dto.WinningNumbersDto;
 
 import java.util.Random;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,9 +19,11 @@ public class NumbersGeneratorFacade {
     private static final Integer MAX_NUMBER = 99;
 
     private final NumbersGeneratorValidator numbersGeneratorValidator;
+    private final NumbersGeneratorRepository numbersGeneratorRepository;
 
-    public NumbersGeneratorFacade(NumbersGeneratorValidator numbersGeneratorValidator) {
+    public NumbersGeneratorFacade(NumbersGeneratorValidator numbersGeneratorValidator, NumbersGeneratorRepository numbersGeneratorRepository) {
         this.numbersGeneratorValidator = numbersGeneratorValidator;
+        this.numbersGeneratorRepository = numbersGeneratorRepository;
     }
 
     public Set<Integer> generateLottoNumbers() {
@@ -38,5 +42,23 @@ public class NumbersGeneratorFacade {
         }
         winningNumbersNotFound();
         return new WinningNumbersDto(Set.of(0));
+    }
+
+    public LottoNumbersDto findNumbersGeneratorById(UUID uuid) {
+        return numbersGeneratorRepository.findById(uuid)
+                .map(NumbersGeneratorMapper::toDto)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public LottoNumbersDto findNumbersGenerator() {
+        return numbersGeneratorRepository.findAll()
+                .stream()
+                .map(NumbersGeneratorMapper::toDto)
+                .findAny()
+                .orElse(null);
+    }
+
+    public NumbersGenerator createNumbersGenerator(NumbersGenerator numbersGenerator) {
+        return numbersGeneratorRepository.save(numbersGenerator);
     }
 }
