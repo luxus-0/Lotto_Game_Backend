fad5c7623ee9aaa990c2cdf3173f6c9be44ada2d
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-21 21:09:25 +0100

    Remove annotation

--- src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorFacadeConfiguration.java
+++ src/main/java/pl/lotto/numbersgenerator/NumbersGeneratorFacadeConfiguration.java
@@ -1,13 +1,11 @@
 package pl.lotto.numbersgenerator;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 class NumbersGeneratorFacadeConfiguration {
     NumbersGeneratorFacade createModuleForTests(NumbersGeneratorRepository numbersGeneratorRepository) {
         NumbersGeneratorValidator numbersGeneratorValidator = new NumbersGeneratorValidator();
-        return new NumbersGeneratorFacade(numbersGeneratorRepository, numbersGeneratorValidator);
+        return new NumbersGeneratorFacade(numbersGeneratorValidator);
     }
 }
