a6dd370c451a43a596751c543830c76cbd44170c
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-23 21:20:22 +0100

    Add new functionality

--- src/main/java/pl/lotto/emailsender/EmailSender.java
+++ src/main/java/pl/lotto/emailsender/EmailSender.java
@@ -1,34 +1,27 @@
 package pl.lotto.emailsender;
 
-import org.simplejavamail.api.email.Email;
-import org.simplejavamail.config.ConfigLoader;
-import org.simplejavamail.email.EmailBuilder;
-import org.simplejavamail.mailer.MailerBuilder;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import static pl.lotto.emailsender.EmailConfigMessage.SMTP_HOST;
-import static pl.lotto.emailsender.EmailConfigMessage.SMTP_PORT;
 import static pl.lotto.emailsender.EmailSenderMessage.*;
 
 @Service
 class EmailSender {
 
+    private final JavaMailSender mailSender;
+
+    EmailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     void send() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(FROM_EMAIL);
+        message.setTo(TO_EMAIL);
+        message.setSubject(SUBJECT);
+        message.setText(TEXT);
+        mailSender.send(message);
 
-        ConfigLoader.loadProperties("application.properties", true);
-        
-        Email email = EmailBuilder.startingBlank()
-                .from(FROM_EMAIL_DESCRIPTION, FROM_EMAIL)
-                .to(TO_EMAIL_DESCRIPTION, TO_EMAIL)
-                .withReplyTo(TO_EMAIL)
-                .withSubject(SUBJECT)
-                .withHTMLText(HTML)
-                .buildEmail();
-
-        MailerBuilder
-                .withSMTPServer(SMTP_HOST, SMTP_PORT, TO_EMAIL, PASSWORD)
-                .async()
-                .buildMailer()
-                .sendMail(email);
     }
 }
--- src/main/java/pl/lotto/emailsender/EmailSenderMessage.java
+++ src/main/java/pl/lotto/emailsender/EmailSenderMessage.java
@@ -6,7 +6,7 @@ class EmailSenderMessage {
     public static final String TO_EMAIL = "nowogorski.lukasz0@gmail.com";
     public static final String TO_EMAIL_DESCRIPTION = "Łukasz Nowogórski";
     public static final String SUBJECT = "Winner lotto game";
-    public static final String PASSWORD = "lukasz123564$&";
+    public static final String TEXT = "CONGRATULATION!!!YOU WIN IN LOTTO GAME :D";
 
     public static final String HTML = "<img src='https://media.tenor.com/E6lFjorkDRAAAAAM/winner.gif'>" +
             "<b>YOU WIN!!</b>" +
