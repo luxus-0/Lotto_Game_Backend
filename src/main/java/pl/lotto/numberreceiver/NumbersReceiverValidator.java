e576ad9237439de9b9d7840393f9e2b66750138f
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-19 19:18:23 +0100

    Change module number receiver

--- src/main/java/pl/lotto/numberreceiver/NumbersReceiverValidator.java
+++ src/main/java/pl/lotto/numberreceiver/NumbersReceiverValidator.java
@@ -21,11 +21,9 @@ class NumbersReceiverValidator {
             messages.add(MORE_THAN_SIX_NUMBERS);
         } else if (isEmptyNumbers(inputNumbers)) {
             messages.add(NUMBERS_IS_EMPTY);
-        } else if (isNotInRangeNumbers(inputNumbers)) {
-            messages.add(NOT_IN_RANGE_NUMBERS);
         } else {
             messages.add(EQUALS_SIX_NUMBERS);
-            return isEqualsSixNumbers(inputNumbers);
+            return isEqualsSixNumberFrom1To99(inputNumbers);
         }
         return false;
     }
@@ -39,19 +37,16 @@ class NumbersReceiverValidator {
         return inputNumbers.size() < SIZE_MAX;
     }
 
-    boolean isEqualsSixNumbers(Collection<Integer> inputNumbers) {
-        return inputNumbers.size() == SIZE_MAX && isPositiveNumber(inputNumbers);
-    }
-
-    private boolean isPositiveNumber(Collection<Integer> inputNumbers) {
-        return inputNumbers.stream().allMatch(number -> number > 0);
-    }
-
-    boolean isNotInRangeNumbers(Collection<Integer> inputNumbers) {
-        return inputNumbers.stream().anyMatch(number -> number > RANGE_TO_NUMBER || number < RANGE_FROM_NUMBER);
-    }
-
     boolean isMoreThanSixNumbers(Collection<Integer> inputNumbers) {
         return inputNumbers.size() > SIZE_MAX;
+    }
+
+    boolean isEqualsSixNumberFrom1To99(Collection<Integer> inputNumbers) {
+        List<Integer> rangeNumbers = inputNumbers.stream()
+                .filter(number -> number >= FROM_NUMBER)
+                .filter(number -> number <= TO_NUMBER)
+                .toList();
+
+        return rangeNumbers.stream().anyMatch(number -> number == SIZE_MAX);
     }
 }
