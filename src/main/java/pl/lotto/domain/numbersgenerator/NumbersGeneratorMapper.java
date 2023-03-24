85f3288233c849ee4f70ee2e2184f3fc3bba1a77
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-24 23:53:56 +0100

    removed class

--- src/main/java/pl/lotto/domain/numbersgenerator/NumbersGeneratorMapper.java
+++
@@ -1,9 +0,0 @@
-package pl.lotto.domain.numbersgenerator;
-
-import pl.lotto.domain.numbersgenerator.dto.LottoNumbersDto;
-
-class NumbersGeneratorMapper {
-    static LottoNumbersDto toDto(NumbersGenerator numbersGenerator) {
-        return new LottoNumbersDto(numbersGenerator.uuid(), numbersGenerator.lottoNumbers());
-    }
-}
--- src/main/java/pl/lotto/domain/numbersgenerator/NumbersGeneratorRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.lotto.domain.numbersgenerator;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface NumbersGeneratorRepository extends MongoRepository<NumbersGenerator, UUID> {
-}
