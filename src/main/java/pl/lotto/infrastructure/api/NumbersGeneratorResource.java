ce5636d8009b49e5a7389a4f1cbfb65420621cd6
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-24 23:56:07 +0100

    comment all controller

--- src/main/java/pl/lotto/infrastructure/api/NumbersGeneratorResource.java
+++ src/main/java/pl/lotto/infrastructure/api/NumbersGeneratorResource.java
@@ -1,45 +1,41 @@
 package pl.lotto.infrastructure.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.lotto.domain.numbersgenerator.NumbersGenerator;
-import pl.lotto.domain.numbersgenerator.NumbersGeneratorFacade;
-import pl.lotto.domain.numbersgenerator.dto.LottoNumbersDto;
+import pl.lotto.domain.numbersgenerator.WinningNumbersGeneratorFacade;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
 
 import java.util.Set;
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 class NumbersGeneratorResource {
 
-    private final NumbersGeneratorFacade numbersGeneratorFacade;
+    //private final WinningNumbersGeneratorFacade winningNumbersGeneratorFacade;
 
-    NumbersGeneratorResource(NumbersGeneratorFacade numbersGeneratorFacade) {
-        this.numbersGeneratorFacade = numbersGeneratorFacade;
-    }
-
-    @GetMapping("/lotto_numbers")
+    /*@GetMapping("/lotto_numbers")
     ResponseEntity<LottoNumbersDto> readLottoNumbers() {
         UUID uuid = UUID.randomUUID();
-        Set<Integer> randomNumbersLotto = numbersGeneratorFacade.generateLottoNumbers();
+        Set<Integer> randomNumbersLotto = winningNumbersGeneratorFacade.generateLottoNumbers();
         return ResponseEntity.ok(new LottoNumbersDto(uuid, randomNumbersLotto));
     }
 
     @GetMapping("/winner_numbers")
     ResponseEntity<WinningNumbersDto> printWinnerNumbers() {
-        return ResponseEntity.ok(numbersGeneratorFacade.showWinnerNumbers());
+        return ResponseEntity.ok(winningNumbersGeneratorFacade.showWinnerNumbers());
     }
 
     @PostMapping("/user/lotto_numbers")
     ResponseEntity<LottoNumbersDto> pickLottoNumbers(NumbersGenerator numbersGenerator) {
-        return ResponseEntity.ok(numbersGeneratorFacade.selectLottoNumbers(numbersGenerator));
+        return ResponseEntity.ok(winningNumbersGeneratorFacade.selectLottoNumbers(numbersGenerator));
     }
 
     @PostMapping("/user/lotto_numbers/{uuid}")
     ResponseEntity<LottoNumbersDto> seekLottoNumbers(UUID uuid) {
-        return ResponseEntity.ok(numbersGeneratorFacade.findLottoNumbers(uuid));
-    }
+        return ResponseEntity.ok(winningNumbersGeneratorFacade.findLottoNumbers(uuid));
+    }*/
 }
