aab30b2e751cec5cdd312cc8a53534d14ad179cf
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-25 23:29:34 +0100

    Add checking function if day is Saturday and before noon(12pm), If day is Saturday and after noon(12pm)

--- src/main/java/pl/lotto/domain/drawdate/DrawDateGenerator.java
+++ src/main/java/pl/lotto/domain/drawdate/DrawDateGenerator.java
@@ -1,19 +1,43 @@
 package pl.lotto.domain.drawdate;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static java.time.DayOfWeek.SATURDAY;
 import static java.time.temporal.TemporalAdjusters.next;
 
 @Service
 @AllArgsConstructor
+@Log4j2
 public class DrawDateGenerator {
     private final AdjustableClock clock;
+    private final LocalTime DRAW_TIME = LocalTime.of(12,0,0,0);
 
     LocalDateTime generateNextDrawDate() {
+       LocalDateTime currentDateTime = LocalDateTime.now(clock);
+        if(isSaturdayAndBeforeNoon(currentDateTime)) {
+            log.error("Draw date is saturday and before 12 pm");
+            return LocalDateTime.of(currentDateTime.toLocalDate(), DRAW_TIME);
+        } else if(isSaturdayAndAfterNoon(currentDateTime)){
+            log.error("Draw date is saturday and after 12 pm");
+            return LocalDateTime.of(currentDateTime.toLocalDate(), DRAW_TIME);
+        }
+        return readNextDrawDate();
+    }
+
+    private boolean isSaturdayAndAfterNoon(LocalDateTime currentDateTime) {
+        return currentDateTime.getDayOfWeek().equals(SATURDAY) && currentDateTime.toLocalTime().isAfter(DRAW_TIME);
+    }
+
+    private boolean isSaturdayAndBeforeNoon(LocalDateTime currentDateTime) {
+        return currentDateTime.getDayOfWeek().equals(SATURDAY) && currentDateTime.toLocalTime().isBefore(LocalTime.of(12, 0, 0, 0));
+    }
+
+    private LocalDateTime readNextDrawDate() {
         return LocalDateTime.now(clock)
                 .with(next(SATURDAY))
                 .withHour(12)
