719cd89f20c3747e0a32b76fea7d747f2792a1cc
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-25 13:49:43 +0100

    Add validation for winning numbers:
    If numbers less than 1 and more than 99
    If numbers quantity is not less or equal 6 numbers, throw IllegalStateException

--- src/main/java/pl/lotto/domain/numbersgenerator/WinningNumberValidator.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/WinningNumberValidator.java
@@ -1,8 +1,32 @@
 package pl.lotto.domain.numbersgenerator;
 
+import org.springframework.beans.factory.annotation.Value;
+import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
+
 import java.util.Set;
 
 public class WinningNumberValidator {
-    public void validate(Set<Integer> winningNumbersDto) {
+    @Value("${range.from.number}")
+    private int RANGE_FROM_NUMBER;
+    @Value("${range.to.number}")
+    private int RANGE_TO_NUMBER;
+    @Value("${quantity.numbers}")
+    private int QUANTITY_NUMBERS;
+
+    public WinningNumbersDto validate(Set<Integer> winningNumbers) {
+        boolean correctQuantityNumbers = isCorrectQuantityNumbers(winningNumbers);
+        return winningNumbers.stream()
+                .filter(numbers -> numbers < RANGE_FROM_NUMBER)
+                .filter(numbers -> numbers > RANGE_TO_NUMBER)
+                .filter(numbers -> !correctQuantityNumbers)
+                .map(number -> WinningNumbersDto.builder()
+                        .winningNumbers(winningNumbers)
+                        .build())
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("Number out of range"));
+    }
+
+    private boolean isCorrectQuantityNumbers(Set<Integer> winningNumbers) {
+        return winningNumbers.size() <= QUANTITY_NUMBERS;
     }
 }
