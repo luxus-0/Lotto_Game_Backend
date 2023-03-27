247c0506dcef92507b7150f49961b1d2103e14ea
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-27 13:30:45 +0200

    Add mapper json for Client

--- src/main/java/pl/lotto/infrastructure/numbergenerator/client/NumberGeneratorClient.java
+++ src/main/java/pl/lotto/infrastructure/numbergenerator/client/NumberGeneratorClient.java
@@ -1,44 +1,45 @@
 package pl.lotto.infrastructure.numbergenerator.client;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.*;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.lotto.domain.numbersgenerator.dto.RandomNumberDto;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Service
 public class NumberGeneratorClient {
-    @Value("${random.numbers.api}")
-    private String RANDOM_NUMBERS_API;
-    @Value("${range.from.number}")
-    private int RANGE_FROM_NUMBER;
-    @Value("${range.to.number}")
-    private int RANGE_TO_NUMBER;
-    @Value("${quantity.numbers}")
-    private int QUANTITY_NUMBERS;
-    private final RestTemplate restTemplate;
-    public ResponseEntity<RandomNumberDto> generateSixRandomNumbers(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        String url = getRandomNumbersUrl();
+    private static final String URL = "https://www.random.org/integers/?num=6&min=1&max=99&col=1&base=10&format=plain&rnd=new";
 
-        return restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                RandomNumberDto.class
-                );
+    public RandomNumberDto generateSixRandomNumbers() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.GET, entity, String.class);
+
+        String responseBody = response.getBody();
+        Objects.requireNonNull(responseBody);
+        Set<Integer> randomNumbers = getRandomNumbers(responseBody);
+
+        return RandomNumberDto.builder()
+                .randomNumbers(randomNumbers)
+                .build();
     }
 
-    private String getRandomNumbersUrl() {
-        return RANDOM_NUMBERS_API +
-                "?num=" + QUANTITY_NUMBERS +
-                "&min=" + RANGE_FROM_NUMBER +
-                "&max=" + RANGE_TO_NUMBER +
-                "&format=plain" +
-                "&rnd=new";
+    @NotNull
+    private static Set<Integer> getRandomNumbers(String responseBody) {
+        return Arrays.stream(responseBody.split("\n"))
+                .map(Integer::valueOf)
+                .collect(Collectors.toSet());
     }
 }
