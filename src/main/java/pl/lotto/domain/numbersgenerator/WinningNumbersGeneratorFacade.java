f940e7c5176453a640053032dcd9af2dc60421a2
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-27 13:29:48 +0200

    Change in test for Winning Number generator facade

--- src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersGeneratorFacade.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersGeneratorFacade.java
@@ -1,15 +1,14 @@
 package pl.lotto.domain.numbersgenerator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import pl.lotto.domain.drawdate.DrawDateFacade;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
 import pl.lotto.domain.numbersgenerator.exception.WinningNumbersNotFoundException;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @AllArgsConstructor
 public class WinningNumbersGeneratorFacade {
@@ -18,7 +17,7 @@ public class WinningNumbersGeneratorFacade {
     private final WinningNumberValidator winningNumberValidator;
     private final WinningNumbersRepository winningNumbersRepository;
 
-    WinningNumbersDto generateWinningNumbers() {
+    WinningNumbersDto generateWinningNumbers() throws JSONException, JsonProcessingException {
         LocalDateTime drawDate = drawDateFacade.retrieveNextDrawDate();
         Set<Integer> winningNumbers = randomNumberGeneratorFacade.generateSixRandomNumbers().winningNumbers();
         winningNumberValidator.validate(winningNumbers);
@@ -26,16 +25,16 @@ public class WinningNumbersGeneratorFacade {
                 .winningNumbers(winningNumbers)
                 .date(drawDate)
                 .build();
+
         Set<Integer> winningNumbersSaved = winningNumbersRepository.save(winningNumbersCreator).winningNumbers();
+
         return WinningNumbersDto.builder()
                 .winningNumbers(winningNumbersSaved)
                 .build();
     }
     WinningNumbersDto retrieveWinningNumbersByDate(LocalDateTime date) {
-        WinningNumbers winningNumbers = winningNumbersRepository.findWinningNumbersByDate(date);
-        List<WinningNumbers> winningNumberList = new ArrayList<>();
-        winningNumberList.add(winningNumbers);
-        return winningNumberList.stream()
+        Optional<WinningNumbers> winningNumbers = winningNumbersRepository.findWinningNumbersByDate(date);
+        return winningNumbers.stream()
                 .filter(Objects::nonNull)
                 .map(WinningNumbersMapper::toDto)
                 .findAny()
