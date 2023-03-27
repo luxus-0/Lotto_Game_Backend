63305084f4d637b760111d3fb5534bf3fcf21f04
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-27 14:20:01 +0200

    Add Test method: Check correct range numbers between 1 to 99

--- src/test/java/pl/lotto/domain/numbersgenerator/WinningNumbersGeneratorFacadeTest.java
+++ src/test/java/pl/lotto/domain/numbersgenerator/WinningNumbersGeneratorFacadeTest.java
@@ -1,14 +1,12 @@
 package pl.lotto.domain.numbersgenerator;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import pl.lotto.domain.drawdate.DrawDateFacade;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -19,19 +17,33 @@ class WinningNumbersGeneratorFacadeTest {
 
     WinningNumbersRepository winningNumbersRepository = new WinningNumbersRepositoryTestImpl();
     DrawDateFacade drawDateFacade = mock(DrawDateFacade.class);
-
     RandomNumberGeneratorFacade randomNumberGeneratorFacade = new RandomNumberGeneratorConfiguration().createModuleForTests(winningNumbersRepository);
+    WinningNumbersGeneratorFacade winningNumbersGeneratorFacade = new WinningNumbersGeneratorFacadeConfiguration().createModuleForTest(randomNumberGeneratorFacade, drawDateFacade, winningNumbersRepository);
 
     @Test
     public void should_return_set_of_required_size() {
         //given
         when(drawDateFacade.retrieveNextDrawDate()).thenReturn(LocalDateTime.now());
-
-        WinningNumbersGeneratorFacade winningNumbersGeneratorFacade = new WinningNumbersGeneratorFacadeConfiguration().createModuleForTest(randomNumberGeneratorFacade, drawDateFacade, winningNumbersRepository);
         //when
         WinningNumbersDto generateNumbers = winningNumbersGeneratorFacade.generateWinningNumbers();
         //then
         assertThat(generateNumbers.winningNumbers().size()).isEqualTo(6);
+    }
+
+    @Test
+    public void should_return_set_of_required_size_within_required_range(){
+        //given
+        int upperBand = 99;
+        int lowerBand = 1;
+
+        when(drawDateFacade.retrieveNextDrawDate()).thenReturn(LocalDateTime.now());
+        //when
+        WinningNumbersDto result = winningNumbersGeneratorFacade.generateWinningNumbers();
+        //then
+        Set<Integer> winningNumbers = result.winningNumbers();
+        boolean numbersInRange = winningNumbers.stream().anyMatch(numbers -> numbers >= lowerBand && numbers <= upperBand);
+
+        assertThat(numbersInRange).isTrue();
     }
 
 }
