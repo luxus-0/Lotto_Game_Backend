421b33ba767392f13ab661760493ab33802dc695
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-30 16:41:00 +0200

    Remove unused exception class

--- src/main/java/pl/lotto/domain/numbersgenerator/exception/IncorrectSizeNumbersException.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto.domain.numbersgenerator.exception;
-
-public class IncorrectSizeNumbersException extends RuntimeException {
-    public IncorrectSizeNumbersException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pl/lotto/domain/numbersgenerator/exception/OutOfRangeNumbersException.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto.domain.numbersgenerator.exception;
-
-public class OutOfRangeNumbersException extends RuntimeException {
-    public OutOfRangeNumbersException(String message){
-        super(message);
-    }
-}
