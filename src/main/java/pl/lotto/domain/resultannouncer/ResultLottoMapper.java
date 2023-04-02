852936e89a21d3e19f5b02a7bf739835187cc4a3
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-02 16:57:03 +0200

    Remove unused function from Mapper

--- src/main/java/pl/lotto/domain/resultannouncer/ResultLottoMapper.java
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultLottoMapper.java
@@ -5,29 +5,15 @@ import pl.lotto.domain.resultchecker.dto.ResultDto;
 
 public class ResultLottoMapper {
 
-    static ResultResponseDto mapToResultResponseDto(ResultDto resultLottoDto) {
+    static ResultResponseDto mapToResultResponseDto(ResultLotto resultLotto) {
         return ResultResponseDto.builder()
-                .resultDto(resultLottoDto)
-                .build();
-    }
-
-    static ResultLotto mapToResultLotto(ResultResponseDto resultLottoSaved) {
-        return ResultLotto.builder()
-                .hash(resultLottoSaved.resultDto().hash())
-                .numbers(resultLottoSaved.resultDto().numbers())
-                .hitNumbers(resultLottoSaved.resultDto().hitNumbers())
-                .drawDate(resultLottoSaved.resultDto().drawDate())
-                .isWinner(resultLottoSaved.resultDto().isWinner())
-                .build();
-    }
-
-   static ResultDto mapToResultDto(ResultLotto resultLotto) {
-        return ResultDto.builder()
-                .hash(resultLotto.hash())
-                .numbers(resultLotto.numbers())
-                .hitNumbers(resultLotto.hitNumbers())
-                .drawDate(resultLotto.drawDate())
-                .isWinner(resultLotto.isWinner())
+                .resultDto(ResultDto.builder()
+                        .hash(resultLotto.hash())
+                        .numbers(resultLotto.numbers())
+                        .hitNumbers(resultLotto.hitNumbers())
+                        .drawDate(resultLotto.drawDate())
+                        .isWinner(resultLotto.isWinner())
+                        .build())
                 .build();
     }
 }
