b64bf526f583309f39ac16da1a740cc7a914d766
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-04 00:30:07 +0200

    Add exception for validation not correct size numbers

---
+++ src/main/java/pl/lotto/domain/resultchecker/NotCorrectSizeNumbersException.java
@@ -0,0 +1,7 @@
+package pl.lotto.domain.resultchecker;
+
+class NotCorrectSizeNumbersException extends RuntimeException {
+    NotCorrectSizeNumbersException(String message) {
+        super(message);
+    }
+}
