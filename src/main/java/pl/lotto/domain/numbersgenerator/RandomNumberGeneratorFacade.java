8518af58f5152df0dbfad43d87f163f9e137ec7d
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-04 19:25:50 +0200

    Change in random number generator facade

--- src/main/java/pl/lotto/domain/numbersgenerator/RandomNumberGeneratorFacade.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/RandomNumberGeneratorFacade.java
@@ -3,29 +3,28 @@ package pl.lotto.domain.numbersgenerator;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import pl.lotto.domain.numbersgenerator.dto.RandomNumbersDto;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
-import pl.lotto.infrastructure.numbergenerator.client.NumberGeneratorClient;
-
-import java.util.Set;
+import pl.lotto.infrastructure.numbergenerator.client.RandomNumberGeneratorClient;
 
 @AllArgsConstructor
 @Log4j2
 @Service
 public class RandomNumberGeneratorFacade {
 
-    private final NumberGeneratorClient numberGeneratorClient;
+    private final RandomNumberGeneratorClient randomNumberGeneratorClient;
     private final WinningNumbersRepository winningNumbersRepository;
 
-    public WinningNumbersDto generateSixRandomNumbers() {
-        WinningNumbersDto response = numberGeneratorClient.generateRandomNumbers();
+    public RandomNumbersDto generateSixRandomNumbers() {
+        RandomNumbersDto response = randomNumberGeneratorClient.generateRandomNumbers();
             WinningNumbers winningNumbers = WinningNumbers.builder()
-                    .winningNumbers(response.winningNumbers())
+                    .winningNumbers(response.randomNumbers())
                     .build();
 
             WinningNumbers winningNumbersSaved = winningNumbersRepository.save(winningNumbers);
 
-            return WinningNumbersDto.builder()
-                    .winningNumbers(winningNumbersSaved.winningNumbers())
+            return RandomNumbersDto.builder()
+                    .randomNumbers(winningNumbersSaved.winningNumbers())
                     .build();
     }
 }
