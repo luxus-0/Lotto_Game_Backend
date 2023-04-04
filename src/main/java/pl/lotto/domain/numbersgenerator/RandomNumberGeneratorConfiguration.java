cade22d02132356cd4518e56ac8423293b59e43f
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-04 19:24:49 +0200

    Change configuration

--- src/main/java/pl/lotto/domain/numbersgenerator/RandomNumberGeneratorConfiguration.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/RandomNumberGeneratorConfiguration.java
@@ -4,19 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
-import pl.lotto.infrastructure.numbergenerator.client.NumberGeneratorClient;
+import pl.lotto.infrastructure.numbergenerator.client.RandomNumberGeneratorClient;
 
 @Configuration
 @Profile("test")
 public class RandomNumberGeneratorConfiguration {
-
     @Bean
-    public RestTemplate restTemplate(){
+    RestTemplate restTemplate(){
         return new RestTemplate();
     }
     @Bean
     public RandomNumberGeneratorFacade createModuleForTests(WinningNumbersRepository winningNumbersRepository) {
-        NumberGeneratorClient numberGeneratorClient = new NumberGeneratorClient();
-        return new RandomNumberGeneratorFacade(numberGeneratorClient, winningNumbersRepository);
+        RandomNumberGeneratorClient randomNumberGeneratorClient = new RandomNumberGeneratorClient();
+        return new RandomNumberGeneratorFacade(randomNumberGeneratorClient, winningNumbersRepository);
     }
 }
