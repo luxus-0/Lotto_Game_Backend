aa412d70481af99a21c672428e39881d4c96ab70
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-06 17:58:44 +0200

    Change in method generateNextDrawDate() If is Saturday and before 12 pm If is Saturday and after 12 pm throw new NextDrawDateNotFound();

--- src/main/java/pl/lotto/domain/drawdate/DrawDateGenerator.java
+++ src/main/java/pl/lotto/domain/drawdate/DrawDateGenerator.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import static java.time.DayOfWeek.SATURDAY;
+import static java.time.LocalTime.NOON;
 import static java.time.ZoneOffset.UTC;
 import static java.time.temporal.TemporalAdjusters.next;
 
@@ -15,33 +16,29 @@ import static java.time.temporal.TemporalAdjusters.next;
 @AllArgsConstructor
 @Log4j2
 public class DrawDateGenerator {
-    public static final LocalTime DRAW_TIME = LocalTime.of(12, 0, 0, 0);
     private final AdjustableClock clock;
-
     LocalDateTime generateNextDrawDate() {
         LocalDateTime currentDateTime = LocalDateTime.now(clock.withZone(UTC));
         if (isSaturdayAndBeforeNoon(currentDateTime)) {
-            throw new NextDrawDateNotFoundException("Date: \n" +currentDateTime.toLocalDate() + " " + currentDateTime.toLocalTime() + "Next draw date: " +readNextDrawDate());
+            throw new NextDrawDateNotFoundException(currentDateTime.toLocalDate() + " " + currentDateTime.toLocalTime() + "\nNext draw date: " +readNextDrawDate());
         } else if (isSaturdayAndAfterNoon(currentDateTime)) {
-            throw new NextDrawDateNotFoundException("Date: \n" +currentDateTime.toLocalDate() + " " + currentDateTime.toLocalTime() + "Next draw date: " +readNextDrawDate());
+            throw new NextDrawDateNotFoundException(currentDateTime.toLocalDate() + " " + currentDateTime.toLocalTime() + "\nNext draw date: " +readNextDrawDate());
         } else {
             return readNextDrawDate();
         }
     }
 
     private boolean isSaturdayAndAfterNoon(LocalDateTime currentDateTime) {
-        return currentDateTime.getDayOfWeek().equals(SATURDAY) && currentDateTime.toLocalTime().isAfter(DRAW_TIME);
+        return currentDateTime.getDayOfWeek().equals(SATURDAY) && currentDateTime.toLocalTime().isAfter(NOON);
     }
 
     private boolean isSaturdayAndBeforeNoon(LocalDateTime currentDateTime) {
-        return currentDateTime.getDayOfWeek().equals(SATURDAY) && currentDateTime.toLocalTime().isBefore(DRAW_TIME);
+        return currentDateTime.getDayOfWeek().equals(SATURDAY) && currentDateTime.toLocalTime().isBefore(NOON);
     }
 
     private LocalDateTime readNextDrawDate() {
         return LocalDateTime.now(clock)
                 .with(next(SATURDAY))
-                .withHour(12)
-                .withMinute(0)
-                .withSecond(0);
+                .with(NOON);
     }
 }
