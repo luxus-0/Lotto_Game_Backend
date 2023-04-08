1d01bdbb558c6abaa4d6338a0195db276c256a6d
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-08 22:08:38 +0200

    Refactor: Reformat line breaks

--- src/main/java/pl/lotto/domain/resultchecker/Ticket.java
+++ src/main/java/pl/lotto/domain/resultchecker/Ticket.java
@@ -6,6 +6,5 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
-public
-record Ticket(String hash, Set<Integer> numbers, LocalDateTime drawDate) {
+public record Ticket(String hash, Set<Integer> numbers, LocalDateTime drawDate) {
 }
