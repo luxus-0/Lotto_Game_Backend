881dec18e8f302c594b357acc1833682575e433f
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-08 11:01:09 +0200

    Fix: bug remove from @EnableConfigurationProperties(RandomNumberParametersUrl.class)

--- src/main/java/pl/lotto/LottoApplication.java
+++ src/main/java/pl/lotto/LottoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import pl.lotto.domain.numbersgenerator.WinningNumbersFacadeConfigurationProperties;
-import pl.lotto.infrastructure.numbergenerator.client.RandomNumberParametersUrl;
 
 @SpringBootApplication
-@EnableConfigurationProperties({WinningNumbersFacadeConfigurationProperties.class, RandomNumberParametersUrl.class})
+@EnableConfigurationProperties({WinningNumbersFacadeConfigurationProperties.class})
 public class LottoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LottoApplication.class, args);
