7f15b7088dd81cadd2694b41122b846cae69ed79
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-08 22:05:43 +0200

    Change name method class

--- src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerFacade.java
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerFacade.java
@@ -1,7 +1,6 @@
 package pl.lotto.domain.resultannouncer;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import pl.lotto.domain.resultannouncer.dto.ResultResponseDto;
 import pl.lotto.domain.resultchecker.ResultsCheckerFacade;
 import pl.lotto.domain.resultchecker.dto.ResultDto;
@@ -14,24 +13,19 @@ import static pl.lotto.domain.resultannouncer.ResultLottoMapper.*;
 import static pl.lotto.domain.resultannouncer.ResultStatus.*;
 
 @AllArgsConstructor
-@Log4j2
 public class ResultAnnouncerFacade {
     private final ResultsCheckerFacade resultsCheckerFacade;
     private final ResultLottoRepository resultLottoRepository;
     private final Clock clock;
 
-    private static ResultResponseDto getEmptyResultWithMessage() {
-        return ResultResponseDto.builder()
-                .resultDto(null)
-                .message(HASH_NOT_EXIST.message)
-                .build();
-    }
-
     ResultResponseDto findResult(String hash) {
         ResultDto resultDto = resultsCheckerFacade.findByHash(hash);
 
         if (resultDto == null) {
-            return getEmptyResultWithMessage();
+            return ResultResponseDto.builder()
+                    .resultDto(null)
+                    .message(HASH_NOT_EXIST.message)
+                    .build();
         }
 
         Optional<ResultLotto> resultByHash = resultLottoRepository.findById(hash);
