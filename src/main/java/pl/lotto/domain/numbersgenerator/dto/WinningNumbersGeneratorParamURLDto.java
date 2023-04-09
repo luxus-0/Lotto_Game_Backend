894b96453453fcbdc1d0fff7a9fe6abd52804bab
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-09 22:38:07 +0200

    Fix: delete @Value annotation from fields

--- src/main/java/pl/lotto/domain/numbersgenerator/dto/WinningNumbersGeneratorParamURLDto.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/dto/WinningNumbersGeneratorParamURLDto.java
@@ -1,15 +1,14 @@
 package pl.lotto.domain.numbersgenerator.dto;
 
 import lombok.Builder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Builder
 @ConfigurationProperties(value = "parameters-url")
-public record WinningNumbersGeneratorParamURLDto(@Value("${count}") int count,
-                                                 @Value("${lower_band}") int lowerBand,
-                                                 @Value("${upper_band}") int upperBand,
-                                                 @Value("${format}") String format,
-                                                 @Value("${base}") int base,
-                                                 @Value("${number_column}") int numberColumn) {
+public record WinningNumbersGeneratorParamURLDto(int count,
+                                                 int lowerBand,
+                                                 int upperBand,
+                                                 String format,
+                                                 int base,
+                                                 int numberColumn) {
 }
