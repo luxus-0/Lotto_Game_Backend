07e41a1e425c1d8b189a77ecd3260fda25f6a8cc
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-09 21:02:55 +0200

    Fix: add annotation @EnableConfigurationProperties for WinningNumbersGeneratorParamURLDto and TimeConnectionClientDto

--- src/main/java/pl/lotto/LottoApplication.java
+++ src/main/java/pl/lotto/LottoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.lotto.domain.numbersgenerator.WinningNumbersFacadeConfigurationProperties;
+import pl.lotto.domain.numbersgenerator.dto.WinningNumbersGeneratorParamURLDto;
+import pl.lotto.infrastructure.numbergenerator.client.TimeConnectionClientDto;
 
 @SpringBootApplication
-@EnableConfigurationProperties({WinningNumbersFacadeConfigurationProperties.class})
+@EnableConfigurationProperties({WinningNumbersFacadeConfigurationProperties.class, WinningNumbersGeneratorParamURLDto.class, TimeConnectionClientDto.class})
 @EnableScheduling
 public class LottoApplication {
     public static void main(String[] args) {
