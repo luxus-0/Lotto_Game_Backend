f6169ac5d443bf3eaa32d7f2c31e7aceb67f1dd0
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-12 12:02:36 +0200

    Add parameters to winningNumbersGeneratorFacade()

---
+++ src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersFacadeConfiguration.java
@@ -0,0 +1,35 @@
+package pl.lotto.domain.numbersgenerator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import pl.lotto.domain.drawdate.DrawDateFacade;
+import pl.lotto.infrastructure.numbergenerator.client.RandomNumberClient;
+
+@Configuration
+public class WinningNumbersFacadeConfiguration {
+
+    @Bean
+    RandomNumberClient randomNumberClient(RestTemplate restTemplate, WinningNumbersConfigurationProperties properties) {
+        return new RandomNumberClient(restTemplate, properties);
+    }
+
+    @Bean
+    public WinningNumbersFacade winningNumbersGeneratorFacade(DrawDateFacade drawDateFacade, RandomNumbersGenerable generator, WinningNumbersRepository winningNumbersRepository, WinningNumbersConfigurationProperties properties) {
+        WinningNumberValidator winningNumberValidator = new WinningNumberValidator(properties);
+        return new WinningNumbersFacade(drawDateFacade, generator, winningNumbersRepository, winningNumberValidator);
+    }
+
+    public WinningNumbersFacade winningNumbersGeneratorFacade(DrawDateFacade drawDateFacade, RandomNumbersGenerable generator, WinningNumbersRepository winningNumbersRepository) {
+        WinningNumbersConfigurationProperties properties = WinningNumbersConfigurationProperties.builder()
+                .url("\"https://random.org/integers/")
+                .count(6)
+                .lowerBand(1)
+                .upperBand(99)
+                .format("plain")
+                .column(1)
+                .base(10)
+                .build();
+        return winningNumbersGeneratorFacade(drawDateFacade, generator, winningNumbersRepository, properties);
+    }
+}
