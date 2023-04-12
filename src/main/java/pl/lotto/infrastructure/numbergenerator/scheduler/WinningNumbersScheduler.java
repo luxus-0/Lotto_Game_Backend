99541ceabe9f559a5e14b56fa2ae8e4dd8a8e674
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-12 12:06:02 +0200

    Add Scheduled for WinningNumbersScheduler with cron start every Saturday at 12 pm

--- src/main/java/pl/lotto/infrastructure/numbergenerator/scheduler/WinningNumbersScheduler.java
+++ src/main/java/pl/lotto/infrastructure/numbergenerator/scheduler/WinningNumbersScheduler.java
@@ -2,10 +2,8 @@ package pl.lotto.infrastructure.numbergenerator.scheduler;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import pl.lotto.domain.numbersgenerator.WinningNumbersGeneratorFacade;
+import pl.lotto.domain.numbersgenerator.WinningNumbersFacade;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
 
 /**
@@ -16,11 +14,11 @@ import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
 @Log4j2
 public class WinningNumbersScheduler {
 
-    private final WinningNumbersGeneratorFacade winningNumbersGeneratorFacade;
+    private final WinningNumbersFacade winningNumbersFacade;
 
     //@Scheduled(cron = "${numbers.generator.lottery.run.occurence}")
     public void generateWinningNumbers() {
-        WinningNumbersDto winningNumbersDto = winningNumbersGeneratorFacade.generateWinningNumbers();
+        WinningNumbersDto winningNumbersDto = winningNumbersFacade.generateWinningNumbers();
         log.info(winningNumbersDto.winningNumbers());
         log.info(winningNumbersDto.drawDate());
     }
