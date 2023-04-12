2151b09433f9b0491f57da6898dced8f0c3ebf68
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-12 11:52:37 +0200

    Refactor: name method generateSixRandomNumbers();

--- src/main/java/pl/lotto/domain/numbersgenerator/RandomNumbersGenerable.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/RandomNumbersGenerable.java
@@ -3,5 +3,5 @@ package pl.lotto.domain.numbersgenerator;
 import pl.lotto.domain.numbersgenerator.dto.RandomNumbersDto;
 
 public interface RandomNumbersGenerable {
-    RandomNumbersDto generateRandomNumbers(int count, int lowerBand, int upperBand);
+    RandomNumbersDto generateSixRandomNumbers();
 }
