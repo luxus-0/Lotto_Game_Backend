a954d9cae331582a21f28c2b77a5fc0b9267c704
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-12 11:47:57 +0200

    Remove Dto class from @EnableConfigurationProperties

--- src/main/java/pl/lotto/LottoApplication.java
+++ src/main/java/pl/lotto/LottoApplication.java
@@ -3,14 +3,15 @@ package pl.lotto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import pl.lotto.domain.numbersgenerator.WinningNumbersFacadeConfigurationProperties;
-import pl.lotto.domain.numbersgenerator.dto.WinningNumbersGeneratorParamURLDto;
+import pl.lotto.domain.numbersgenerator.WinningNumbersConfigurationProperties;
 import pl.lotto.infrastructure.numbergenerator.client.TimeConnectionClientDto;
 
 @SpringBootApplication
-@EnableConfigurationProperties({WinningNumbersFacadeConfigurationProperties.class, WinningNumbersGeneratorParamURLDto.class, TimeConnectionClientDto.class})
+@EnableConfigurationProperties({WinningNumbersConfigurationProperties.class, TimeConnectionClientDto.class})
 @EnableScheduling
+@EnableMongoRepositories
 public class LottoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LottoApplication.class, args);
