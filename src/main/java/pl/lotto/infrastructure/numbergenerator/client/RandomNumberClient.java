9c693fc759c03fc0e0801840b38ec9c2df1eb815
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-12 11:51:39 +0200

    Refactor: Method generateSixRandomNumbers adding HttpHeader,HttpEntity
    Implement: private method extractNumbersRandom

--- src/main/java/pl/lotto/infrastructure/numbergenerator/client/RandomNumberClient.java
+++ src/main/java/pl/lotto/infrastructure/numbergenerator/client/RandomNumberClient.java
@@ -2,11 +2,13 @@ package pl.lotto.infrastructure.numbergenerator.client;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import pl.lotto.domain.numbersgenerator.RandomNumbersGenerable;
-import pl.lotto.domain.numbersgenerator.WinningNumbersFacadeConfigurationProperties;
+import pl.lotto.domain.numbersgenerator.WinningNumbersConfigurationProperties;
 import pl.lotto.domain.numbersgenerator.dto.RandomNumbersDto;
 
 import java.util.Arrays;
@@ -17,41 +19,41 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class RandomNumberClient implements RandomNumbersGenerable {
     private final RestTemplate restTemplate;
-    private final WinningNumbersFacadeConfigurationProperties properties;
+    private final WinningNumbersConfigurationProperties properties;
 
     @Override
-    public RandomNumbersDto generateRandomNumbers(int count, int lowerBand, int upperBand) {
+    public RandomNumbersDto generateSixRandomNumbers() {
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
         final String url = UriComponentsBuilder.fromHttpUrl(properties.url())
                 .queryParam("num", properties.count())
                 .queryParam("min", properties.lowerBand())
                 .queryParam("max", properties.upperBand())
-                .queryParam("format", "plain")
-                .queryParam("col", 1)
-                .queryParam("base", 10)
+                .queryParam("format", properties.format())
+                .queryParam("col", properties.column())
+                .queryParam("base", properties.base())
                 .toUriString();
 
-        String response = restTemplate.exchange(
-                        url,
-                        HttpMethod.GET,
-                        HttpEntity.EMPTY,
-                        String.class
-
-                )
-                .getBody();
-
-        Objects.requireNonNull(response);
-        Set<Integer> randomNumbers = parseResponse(response);
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+        String body = response.getBody();
+        Set<Integer> randomNumbers = extractNumbersFromString(body);
 
         return RandomNumbersDto.builder()
                 .randomNumbers(randomNumbers)
                 .build();
     }
 
-    private Set<Integer> parseResponse(String response){
-        return Arrays.stream(response.split("\\s+"))
+    private Set<Integer> extractNumbersFromString(String body) {
+        return Arrays.stream(Objects.requireNonNull(body)
+                        .split("\\s+"))
                 .map(String::trim)
-                .filter(s -> s.matches("\\d"))
                 .map(Integer::valueOf)
                 .collect(Collectors.toSet());
     }
