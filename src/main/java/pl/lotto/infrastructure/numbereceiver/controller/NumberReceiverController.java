acc2640a9987e7024d9e3cb52a7f5bb377616ad5
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-14 14:24:33 +0200

    refactor import

--- src/main/java/pl/lotto/infrastructure/numbereceiver/controller/NumberReceiverController.java
+++ src/main/java/pl/lotto/infrastructure/numbereceiver/controller/NumberReceiverController.java
@@ -1,14 +1,11 @@
 package pl.lotto.infrastructure.numbereceiver.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.lotto.domain.numberreceiver.NumberReceiverFacade;
-import pl.lotto.domain.numberreceiver.Ticket;
 import pl.lotto.domain.numberreceiver.dto.NumberReceiverDto;
 import pl.lotto.domain.numberreceiver.dto.TicketResultDto;
 
--- src/test/java/integration/LottoIntegrationTest.java
+++ src/test/java/integration/LottoIntegrationTest.java
@@ -1,7 +1,6 @@
 package integration;
 
 import com.github.tomakehurst.wiremock.matching.StringValuePattern;
-import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
