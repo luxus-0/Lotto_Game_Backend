1d8b2d4b09b484c70ce11dc612b48d67830ec6e2
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-14 14:22:24 +0200

    add test method

--- src/test/java/integration/LottoIntegrationTest.java
+++ src/test/java/integration/LottoIntegrationTest.java
@@ -5,8 +5,12 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.client.RestTemplate;
+import pl.lotto.domain.numberreceiver.dto.TicketResultDto;
 import pl.lotto.domain.numbersgenerator.WinningNumbersFacade;
 import pl.lotto.domain.numbersgenerator.WinningNumbersNotFoundException;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
@@ -18,10 +22,10 @@ import java.util.Objects;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@Log4j2
+
 public class LottoIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
@@ -31,6 +35,7 @@ public class LottoIntegrationTest extends BaseIntegrationTest {
 
     @Test
     public void should_user_win_and_generate_winners() {
+        //given
         wireMockServer.stubFor(get("random.org/integers/?num=6&min=1&max=99&format=plain&col=1&base=10")
                 .willReturn(aResponse()
                         .withStatus(200)
@@ -38,8 +43,9 @@ public class LottoIntegrationTest extends BaseIntegrationTest {
                         .withBody("""
                                 [1 2 3 4 5 6 23 56 78 89 90 43 65 87 40 11]
                                 """.trim())));
-
+        //when
         LocalDateTime drawDate = LocalDateTime.of(2022, 11, 19, 12, 0, 0);
+        //then
         await()
                 .atMost(Duration.ofSeconds(10))
                 .pollInterval(Duration.ofSeconds(1))
@@ -55,6 +61,7 @@ public class LottoIntegrationTest extends BaseIntegrationTest {
 
     @Test
     public void should_return_generate_six_random_numbers_with_correct_range() {
+        //given
         StringValuePattern countPattern = equalTo("6");
         StringValuePattern rangeFromPattern = equalTo("1");
         StringValuePattern rangeToPattern = equalTo("99");
@@ -66,39 +73,43 @@ public class LottoIntegrationTest extends BaseIntegrationTest {
                 .withQueryParam("num", countPattern)
                 .withQueryParam("min", rangeFromPattern)
                 .withQueryParam("max", rangeToPattern));
-
+        //when
         ResponseEntity<String> response = restTemplate.getForEntity("https://random.org/integers/?num=6&min=1&max=99&format=plain&col=1&base=10", String.class);
-
+        //then
         assertThat(response.getBody()).isNotEmpty();
         assertEquals(200, response.getStatusCodeValue());
     }
 
     @Test
     public void ShouldThrowingExceptionWhenUrlIsIncorrect() {
+        //given
         wireMockServer.stubFor(get(urlEqualTo("/integers.*"))
                 .willReturn(aResponse()
                         .withHeader("Media-Type", "application/json")
                         .withBody("")
                         .withStatus(500)));
-
+        //when
+        //then
         assertThrows(IllegalArgumentException.class,
                 () -> Objects.requireNonNull(
-                        restTemplate.exchange("/integers?num=6&min=1&max=99",
-                                        HttpMethod.GET,
-                                        null,
-                                        WinningNumbersDto.class)
-                                .getBody())
+                                restTemplate.exchange("/integers?num=6&min=1&max=99",
+                                                HttpMethod.GET,
+                                                null,
+                                                WinningNumbersDto.class)
+                                        .getBody())
                         .winningNumbers());
     }
 
     @Test
     public void ShouldThrowingExceptionWhenUrlIsEmpty() {
+        //given
         wireMockServer.stubFor(get(urlEqualTo("/integers.*"))
                 .willReturn(aResponse()
                         .withHeader("Media-Type", "application/json")
                         .withBody("")
                         .withStatus(500)));
-
+        //when
+        //then
         assertThrows(IllegalArgumentException.class,
                 () -> Objects.requireNonNull(
                                 restTemplate.exchange("",
@@ -107,5 +118,50 @@ public class LottoIntegrationTest extends BaseIntegrationTest {
                                                 WinningNumbersDto.class)
                                         .getBody())
                         .winningNumbers());
+    }
+
+    @Test
+    public void should_return_saved_six_input_numbers() throws Exception {
+        //given
+        LocalDateTime drawDate = LocalDateTime.of(2022, 11, 19, 12, 0, 0);
+
+        ResultActions perform = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                        "inputNumbers": [1, 2, 3, 4, 5, 6]
+                        }
+                        """.trim()
+                ).contentType(MediaType.APPLICATION_JSON)
+        );
+        //when
+        MvcResult mvcResult = perform.andExpect(status -> status(200)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        TicketResultDto ticketResultDto = objectMapper.readValue(json, TicketResultDto.class);
+        //then
+       assertThat(ticketResultDto.ticketDto().drawDate()).isEqualTo(drawDate);
+       assertThat(ticketResultDto.message()).isEqualTo("equals six numbers");
+    }
+
+    @Test
+    public void should_return_empty_input_numbers() throws Exception {
+        //given
+
+        ResultActions perform = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                        "inputNumbers": [100, 2, 3, 4, 5, 200]
+                        }
+                        """.trim()
+                ).contentType(MediaType.APPLICATION_JSON)
+        );
+        //when
+        MvcResult mvcResult = perform.andExpect(status -> status(200)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        TicketResultDto ticketResultDto = objectMapper.readValue(json, TicketResultDto.class);
+        //then
+        assertAll(
+                () -> assertThat(ticketResultDto.ticketDto()).isNull(),
+                () -> assertThat(ticketResultDto.message()).isEqualTo("out of range numbers")
+        );
     }
 }
