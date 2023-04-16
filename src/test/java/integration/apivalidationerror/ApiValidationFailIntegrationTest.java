67aa7a571f0e6faae90343da06059ad84142e8c5
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-16 21:06:23 +0200

    Add integration test:
    return 400 bad request with validation message when input numbers are empty
    
    return 400 bad request and validation message when does not have input numbers

--- src/test/java/integration/apivalidationerror/ApiValidationFailIntegrationTest.java
+++ src/test/java/integration/apivalidationerror/ApiValidationFailIntegrationTest.java
@@ -2,11 +2,11 @@ package integration.apivalidationerror;
 
 import integration.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.ResponseEntity.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -14,7 +14,7 @@ public class ApiValidationFailIntegrationTest extends BaseIntegrationTest {
 
     @Test
     public void should_return_400_bad_request_and_validation_message_when_input_numbers_are_empty() throws Exception {
-        //when
+        //given && when
         ResultActions emptyInputNumbers = mockMvc.perform(post("/inputNumbers")
                 .content("""
                         {
@@ -23,10 +23,29 @@ public class ApiValidationFailIntegrationTest extends BaseIntegrationTest {
                         """.trim()
                 ).contentType(MediaType.APPLICATION_JSON)
         );
-        //when
-        MvcResult mvcResult = emptyInputNumbers.andExpect(result -> status(404)).andReturn();
+        //then
+        MvcResult mvcResult = emptyInputNumbers.andExpect(result -> status(400)).andReturn();
         String json = mvcResult.getResponse().getContentAsString();
-        ApiValidationErrorResponseDto responseError = objectMapper.readValue(json, ApiValidationErrorResponseDto.class);
-        ApiValidationErrorResponseDto validationError = new ApiValidationErrorResponseDto("", HttpStatus.NOT_FOUND);
+        ApiValidationErrorDto response = objectMapper.readValue(json, ApiValidationErrorDto.class);
+        assertThat(response.messages()).containsExactlyInAnyOrder(
+                "Input numbers must not be empty");
+    }
+    @Test
+    public void should_return_400_bad_request_and_validation_message_when_does_not_have_input_numbers() throws Exception {
+        //given && when
+        ResultActions notHaveInputNumbers = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                        }
+                        """.trim()
+                ).contentType(MediaType.APPLICATION_JSON)
+        );
+        //then
+        MvcResult mvcResult = notHaveInputNumbers.andExpect(result -> status(400)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        ApiValidationErrorDto response = objectMapper.readValue(json, ApiValidationErrorDto.class);
+        assertThat(response.messages()).containsExactlyInAnyOrder(
+                "Input numbers must not be null",
+                "Input numbers must not be empty");
     }
 }
