faeb0db5ec2656188081247beb75215519b9ab42
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-16 21:00:45 +0200

    Add dto for api validation error used in handleExceptions

---
+++ src/test/java/integration/apivalidationerror/ApiValidationErrorDto.java
@@ -0,0 +1,8 @@
+package integration.apivalidationerror;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ApiValidationErrorDto(List<String> messages, HttpStatus status) {
+}
--- src/test/java/integration/apivalidationerror/ApiValidationErrorResponseDto.java
+++
@@ -1,6 +0,0 @@
-package integration.apivalidationerror;
-
-import org.springframework.http.HttpStatus;
-
-public record ApiValidationErrorResponseDto(String message, HttpStatus status) {
-}
