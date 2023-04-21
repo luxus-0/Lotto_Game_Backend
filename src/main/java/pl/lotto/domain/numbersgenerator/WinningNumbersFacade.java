192d0d0a3d4ca8a88730c64e01226928d0597022
Łukasz Nowogórski <luxus0@gmail.com>
2023-04-21 19:59:04 +0200

    Change

--- src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersFacade.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersFacade.java
@@ -2,6 +2,8 @@ package pl.lotto.domain.numbersgenerator;
 
 import lombok.AllArgsConstructor;
 import pl.lotto.domain.drawdate.DrawDateFacade;
+import pl.lotto.domain.numberreceiver.TicketIdGenerator;
+import pl.lotto.domain.numberreceiver.TicketIdGeneratorImpl;
 import pl.lotto.domain.numbersgenerator.dto.RandomNumbersDto;
 import pl.lotto.domain.numbersgenerator.dto.WinningNumbersDto;
 
@@ -21,21 +23,31 @@ public class WinningNumbersFacade {
         LocalDateTime nextDrawDate = drawDateFacade.retrieveNextDrawDate();
         RandomNumbersDto randomNumbers = randomNumbersGenerable.generateSixRandomNumbers();
         Set<Integer> winningNumbers = randomNumbers.randomNumbers();
+        TicketIdGenerator ticketIdGenerator = new TicketIdGeneratorImpl();
         winningNumberValidator.validate(winningNumbers);
         WinningNumbers winningNumbersDocument = WinningNumbers.builder()
+                .ticketId(ticketIdGenerator.generateTicketId())
                 .winningNumbers(winningNumbers)
                 .drawDate(nextDrawDate)
                 .build();
         WinningNumbers saved = winningNumbersRepository.save(winningNumbersDocument);
         return WinningNumbersDto.builder()
+                .ticketId(saved.ticketId())
                 .winningNumbers(saved.winningNumbers())
                 .drawDate(saved.drawDate())
                 .build();
     }
 
     public WinningNumbersDto retrieveWinningNumbersByDate(LocalDateTime drawDate) {
-        WinningNumbers numbersByDate = winningNumbersRepository.findWinningNumbersByDrawDate(drawDate).orElseThrow(() -> new WinningNumbersNotFoundException(WINNING_NUMBERS_MESSAGE));
+        WinningNumbersDto winningNumbersDto = generateWinningNumbers();
+        WinningNumbers winningNumbers = WinningNumbers.builder()
+                .ticketId(winningNumbersDto.ticketId())
+                .winningNumbers(winningNumbersDto.winningNumbers())
+                .drawDate(drawDate)
+                .build();
+        WinningNumbers numbersByDate = winningNumbersRepository.findWinningNumbersByDrawDate(winningNumbers.drawDate()).orElseThrow(() -> new WinningNumbersNotFoundException(WINNING_NUMBERS_MESSAGE));
         return WinningNumbersDto.builder()
+                .ticketId(numbersByDate.ticketId())
                 .winningNumbers(numbersByDate.winningNumbers())
                 .drawDate(numbersByDate.drawDate())
                 .build();
