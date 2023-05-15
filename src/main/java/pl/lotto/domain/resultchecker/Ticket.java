541d7353f346ef4e393bf48dad665b10e0c3fc6e
Łukasz Nowogórski <luxus0@gmail.com>
2023-05-15 08:29:21 +0200

    Add annotation @Id

--- src/main/java/pl/lotto/domain/resultchecker/Ticket.java
+++ src/main/java/pl/lotto/domain/resultchecker/Ticket.java
@@ -1,12 +1,12 @@
 package pl.lotto.domain.resultchecker;
 
 import lombok.Builder;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
 @Document
-public record Ticket(String ticketId, Set<Integer> numbers, LocalDateTime drawDate) {
+public record Ticket(@Id String ticketId, Set<Integer> numbers, LocalDateTime drawDate) {
 }
