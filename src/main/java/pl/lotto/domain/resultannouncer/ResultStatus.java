04aee2fcf03b5bdc74051beee6bd0a606a84dc62
Łukasz Nowogórski <luxus0@gmail.com>
2023-05-23 21:12:25 +0200

    Change

--- src/main/java/pl/lotto/domain/resultannouncer/ResultStatus.java
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultStatus.java
@@ -1,11 +1,10 @@
 package pl.lotto.domain.resultannouncer;
 
 enum ResultStatus {
-    HASH_NOT_EXIST("Ticket doesn't exist"),
+    TICKET_ID_IS_EMPTY("Ticket doesn't exist"),
     WAIT("Results are being calculated, please come back later"),
     WIN("Congratulation! You won"),
     LOSE("You lost, try again"),
-    RESULT_MESSAGE_EXCEPTION("Result lotto not found"),
     ALREADY_CHECKED("You have already checked your ticket, please come back later");
 
     final String message;
