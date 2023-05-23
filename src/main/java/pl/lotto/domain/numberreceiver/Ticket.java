7e8a7e83f9c67f6c4e1da084955f15d584083a83
Łukasz Nowogórski <luxus0@gmail.com>
2023-05-23 22:00:02 +0200

    Change

--- src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
+++ src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
@@ -1,12 +1,13 @@
 package pl.lotto.domain.numberreceiver;
 
+import io.swagger.models.auth.In;
 import lombok.Builder;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Document
 @Builder
 public record Ticket(String ticketId, Set<Integer> numbers, LocalDateTime drawDate) {
 }
