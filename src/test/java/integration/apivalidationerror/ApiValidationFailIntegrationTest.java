f8d92ea952914bebda1500d7929bb907a7f114b4
Łukasz <luxus0@gmail.com>
2023-12-01 13:34:59 +0100

    Change

--- src/test/java/integration/apivalidationerror/ApiValidationFailIntegrationTest.java
+++ src/test/java/integration/apivalidationerror/ApiValidationFailIntegrationTest.java
@@ -4,7 +4,9 @@ import integration.BaseIntegrationTest;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,18 +17,22 @@ public class ApiValidationFailIntegrationTest extends BaseIntegrationTest {
     public void should_return_400_bad_request_and_validation_message_when_request_has_empty_input_numbers() throws Exception {
         //given && when
         try {
-            mockMvc.perform(post("/inputNumbers")
+            MvcResult perform = mockMvc.perform(post("/inputNumbers")
                             .content("""
                                     {
                                     "inputNumbers" : []
                                     }
                                     """.trim()
-                            ).contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isBadRequest())
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
-        } catch (Exception e) {
+                            ).contentType(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isBadRequest())
+                    .andReturn();
+
+            //then
+            String json = perform.getResponse().getContentAsString();
+            ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+            assertThat(result.messages()).containsExactlyInAnyOrder(
+                    "inputNumbers must not be empty");
+        }catch (Exception e){
             log.error(e.getMessage());
         }
     }
@@ -35,17 +41,23 @@ public class ApiValidationFailIntegrationTest extends BaseIntegrationTest {
     public void should_return_400_bad_request_and_validation_message_when_does_not_have_input_numbers() throws Exception {
         //given && when
         try {
-            mockMvc.perform(post("/inputNumbers")
+            MvcResult perform = mockMvc.perform(post("/inputNumbers")
                             .content("""
-                                    {
+                                   {
+                                   
                                     }
                                     """.trim()
                             ).contentType(MediaType.APPLICATION_JSON)
                     ).andExpect(status().isBadRequest())
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
-        } catch (Exception e) {
+                    .andReturn();
+
+            //then
+            String json = perform.getResponse().getContentAsString();
+            ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+            assertThat(result.messages()).containsExactlyInAnyOrder(
+                    "inputNumbers must not be empty",
+                    "inputNumbers must not be null");
+        }catch (Exception e){
             log.error(e.getMessage());
         }
     }
