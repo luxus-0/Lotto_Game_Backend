2959937663bd69194005fd75a3c0a1360b964b13
Łukasz <luxus0@gmail.com>
2023-12-01 03:08:27 +0100

    Change

--- src/main/java/pl/lotto/domain/numberreceiver/TicketValidationResult.java
+++ src/main/java/pl/lotto/domain/numberreceiver/TicketValidationResult.java
@@ -7,7 +7,6 @@ public enum TicketValidationResult {
     LESS_THAN_SIX_NUMBERS("less than six numbers"),
     MORE_THAN_SIX_NUMBERS("more than six numbers"),
     OUT_OF_RANGE_NUMBERS("out of range numbers"),
-    EMPTY_NUMBERS("empty numbers"),
     EQUALS_SIX_NUMBERS("equals six numbers");
     private final String info;
 
