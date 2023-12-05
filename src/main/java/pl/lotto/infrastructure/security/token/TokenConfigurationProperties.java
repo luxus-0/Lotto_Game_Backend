747cbda2d59b5c8a553d3f9a342dab0ee42c8934
Łukasz <luxus0@gmail.com>
2023-12-05 23:53:40 +0100

    Add Configuration properties for Token JWT

---
+++ src/main/java/pl/lotto/infrastructure/security/token/TokenConfigurationProperties.java
@@ -0,0 +1,9 @@
+package pl.lotto.infrastructure.security.token;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("auth.jwt")
+public record TokenConfigurationProperties(String secret,
+                                           long expirationMs,
+                                           String issuer) {
+}
