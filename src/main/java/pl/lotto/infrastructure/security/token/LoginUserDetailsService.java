08b93d480e310d7aab6e52c4aa54022d05f45a6a
Łukasz <luxus0@gmail.com>
2023-12-05 22:49:19 +0100

    Add login user details service with method: UserDetails loadUserByUsername(Username)

---
+++ src/main/java/pl/lotto/infrastructure/security/token/LoginUserDetailsService.java
@@ -0,0 +1,27 @@
+package pl.lotto.infrastructure.security.token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import pl.lotto.domain.login.LoginAndRegisterFacade;
+import pl.lotto.domain.login.dto.UserDto;
+
+@AllArgsConstructor
+public class LoginUserDetailsService implements UserDetailsService {
+
+    private final LoginAndRegisterFacade loginAndRegisterFacade;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws BadCredentialsException {
+        UserDto userDto = loginAndRegisterFacade.findByUsername(username);
+        return getUser(userDto);
+    }
+
+    private static UserDetails getUser(UserDto user) {
+        return User.builder()
+                .username(user.username())
+                .password(user.password())
+                .build();
+    }
+}
