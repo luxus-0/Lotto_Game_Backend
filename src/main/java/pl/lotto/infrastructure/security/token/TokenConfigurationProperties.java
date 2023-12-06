0b9401604c01687299dbedcbecf998fc5045ecb3
Łukasz <luxus0@gmail.com>
2023-12-06 20:28:17 +0100

    Change TokenConfigurationProperties from time expirationMs to expirationDays. Token time usage 3 days

--- src/main/java/pl/lotto/infrastructure/security/token/TokenConfigurationProperties.java
+++ src/main/java/pl/lotto/infrastructure/security/token/TokenConfigurationProperties.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("auth.jwt")
 public record TokenConfigurationProperties(String secret,
-                                           long expirationMs,
+                                           long expirationDays,
                                            String issuer) {
 }
