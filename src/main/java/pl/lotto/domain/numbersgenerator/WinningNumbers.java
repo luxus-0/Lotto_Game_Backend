020e8f3481ba2f17f83be0caaa881ee0751071cc
Łukasz <luxus0@gmail.com>
2023-12-06 22:49:57 +0100

    Add validation to class

--- src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbers.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbers.java
@@ -1,13 +1,21 @@
 package pl.lotto.domain.numbersgenerator;
 
 import lombok.Builder;
-import org.springframework.data.annotation.Id;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
 @Document
-public record WinningNumbers(@Id String ticketId, Set<Integer> winningNumbers, LocalDateTime drawDate) {
+public record WinningNumbers(@UUID String ticketId,
+                             @NotNull(message = "winning numbers not null")
+                             @NotEmpty(message = "winning numbers not empty")
+                             Set<Integer> winningNumbers,
+                             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                             LocalDateTime drawDate) {
 }
