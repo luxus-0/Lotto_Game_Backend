58234252e3b3ab3a1984c3a839e30b8a79e1753d
Łukasz <luxus0@gmail.com>
2023-12-06 22:57:33 +0100

    Add Login API

---
+++ src/main/java/pl/lotto/domain/login/controller/LoginController.java
@@ -0,0 +1,24 @@
+package pl.lotto.domain.login.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.lotto.infrastructure.security.token.JwtAuthenticatorFacade;
+import pl.lotto.infrastructure.security.token.dto.TokenRequestDto;
+import pl.lotto.infrastructure.security.token.dto.TokenResponseDto;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@AllArgsConstructor
+public class LoginController {
+
+    private final JwtAuthenticatorFacade jwtAuthenticatorFacade;
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login(@RequestBody TokenRequestDto tokenRequest) {
+        TokenResponseDto jwtResponse = jwtAuthenticatorFacade.authenticateAndGenerateToken(tokenRequest);
+        return ResponseEntity.status(CREATED).body(jwtResponse);
+    }
+}
