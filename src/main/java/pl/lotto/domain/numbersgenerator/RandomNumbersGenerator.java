82f29a3618164b0379224c43df1d60e3136e07be
Łukasz <luxus0@gmail.com>
2023-12-07 20:58:37 +0100

    Refactor method name String generateTicketUUID()

--- src/main/java/pl/lotto/domain/numbersgenerator/RandomNumbersGenerator.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/RandomNumbersGenerator.java
@@ -2,8 +2,8 @@ package pl.lotto.domain.numbersgenerator;
 
 import pl.lotto.domain.numbersgenerator.dto.RandomNumbersResponseDto;
 
-public interface RandomNumbersGenerable {
-    String generateUniqueTicketId();
+public interface RandomNumbersGenerator {
+    String generateTicketUUID();
 
     RandomNumbersResponseDto generateRandomNumbers(int count, int lowerBand, int upperBand);
 }
