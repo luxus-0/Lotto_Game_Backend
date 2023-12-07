60b16d16338047c66942a1fa33ff200937ba68c1
Łukasz <luxus0@gmail.com>
2023-12-07 21:02:43 +0100

    Change TicketIdGenerator and TicketIdGeneratorImpl To TicketUUIDGenerator and TicketUUIDGeneratorImpl

--- src/main/java/pl/lotto/domain/numberreceiver/TicketIdGenerator.java
+++
@@ -1,5 +0,0 @@
-package pl.lotto.domain.numberreceiver;
-
-public interface TicketIdGenerator {
-    String generateTicketId();
-}
--- src/main/java/pl/lotto/domain/numberreceiver/TicketIdGeneratorImpl.java
+++
@@ -1,8 +0,0 @@
-package pl.lotto.domain.numberreceiver;
-
-public class TicketIdGeneratorImpl implements TicketIdGenerator {
-    @Override
-    public String generateTicketId() {
-        return "1234567";
-    }
-}
