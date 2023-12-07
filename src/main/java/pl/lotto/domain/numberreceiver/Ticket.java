2652154385fe8c836a1e6a42b332f0128b16190e
Łukasz <luxus0@gmail.com>
2023-12-07 21:01:30 +0100

    Add Validation to Ticket: @UUID String ticketId,
    @NotNull(message = "ticket numbers not null")
    @NotEmpty(message = "ticket numbers not empty")
    Set<Integer> numbers,
    
    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
    LocalDateTime drawDate,
    
    @Enumerated(EnumType.STRING)
    NumberReceiverValidationResult NumberReceiverValidation

--- src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
+++ src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
@@ -1,13 +1,26 @@
 package pl.lotto.domain.numberreceiver;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Builder;
-import org.springframework.data.annotation.Id;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
 @Document
-public record Ticket(@Id String ticketId, Set<Integer> numbers, LocalDateTime drawDate, String message) {
+public record Ticket(@UUID
+                     String ticketId,
+                     @NotNull(message = "ticket numbers not null")
+                     @NotEmpty(message = "ticket numbers not empty")
+                     Set<Integer> numbers,
+                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                     LocalDateTime drawDate,
+                     @Enumerated(EnumType.STRING)
+                     NumberReceiverValidationResult numberReceiverValidation) {
 }
