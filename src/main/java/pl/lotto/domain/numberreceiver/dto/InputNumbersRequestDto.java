0e83e915c3d6c908012759c4fade426cfd7f44aa
Łukasz <luxus0@gmail.com>
2023-12-13 14:04:12 +0100

    Change

--- src/main/java/pl/lotto/domain/numberreceiver/dto/InputNumbersRequestDto.java
+++ src/main/java/pl/lotto/domain/numberreceiver/dto/InputNumbersRequestDto.java
@@ -1,9 +1,12 @@
 package pl.lotto.domain.numberreceiver.dto;
 
+import lombok.Builder;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
+@Builder
 public record InputNumbersRequestDto(
         @NotNull(message = "{inputNumbers.not.null")
         @NotEmpty(message = "{inputNumbers.not.empty")
--- src/main/java/pl/lotto/infrastructure/numbereceiver/controller/NumberReceiverController.java
+++ src/main/java/pl/lotto/infrastructure/numbereceiver/controller/NumberReceiverController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.lotto.domain.numberreceiver.NumberReceiverFacade;
 import pl.lotto.domain.numberreceiver.dto.InputNumbersRequestDto;
-import pl.lotto.domain.numberreceiver.dto.InputNumbersResponseDto;
+import pl.lotto.domain.numberreceiver.dto.TicketResponseDto;
 
 import javax.validation.Valid;
-import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +17,8 @@ class NumberReceiverController {
     private final NumberReceiverFacade numberReceiverFacade;
 
     @PostMapping("/inputNumbers")
-    ResponseEntity<InputNumbersResponseDto> inputNumbers(@RequestBody @Valid InputNumbersRequestDto inputNumbersRequest) {
-        InputNumbersResponseDto ticketResponse = numberReceiverFacade.inputNumbers(inputNumbersRequest);
+    ResponseEntity<TicketResponseDto> inputNumbers(@RequestBody @Valid InputNumbersRequestDto inputNumbersRequest) {
+        TicketResponseDto ticketResponse = numberReceiverFacade.inputNumbers(inputNumbersRequest);
         return ResponseEntity.ok(ticketResponse);
     }
 }
