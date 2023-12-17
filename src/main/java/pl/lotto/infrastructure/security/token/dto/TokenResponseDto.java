e8c777addd42be0c89819e5aecd7544c09c00ab7
Łukasz <luxus0@gmail.com>
2023-12-17 23:02:16 +0100

    Change

--- src/main/java/pl/lotto/infrastructure/security/token/dto/TokenResponseDto.java
+++ src/main/java/pl/lotto/infrastructure/security/token/dto/TokenResponseDto.java
@@ -2,7 +2,15 @@ package pl.lotto.infrastructure.security.token.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Builder
-public record TokenResponseDto(String username,
-                               String token) {
+public record TokenResponseDto(
+        @NotNull(message = "Username not null")
+        @NotEmpty(message = "Username not empty")
+        String username,
+        @NotNull(message = "Token not null")
+        @NotEmpty(message = "Token not empty")
+        String token) {
 }
