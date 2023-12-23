ec84aa29f1ddb1e19d7920b98b5f60493199d76b
Łukasz <luxus0@gmail.com>
2023-12-23 23:20:36 +0100

    Refactor api validation error DTO

--- src/test/java/integration/dto/ApiValidationErrorDto.java
+++ src/test/java/integration/dto/ApiValidationErrorDto.java
@@ -1,11 +1,8 @@
 package integration.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record ApiValidationErrorDto(@JsonProperty("messages")List<String> messages,@JsonProperty("status") HttpStatus status) {
+public record ApiValidationErrorDto(List<String> messages, HttpStatus status) {
 }
