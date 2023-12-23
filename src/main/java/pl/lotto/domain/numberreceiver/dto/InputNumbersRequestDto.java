e4f2e5ddc3282c87060a25c3eda222918fb0dba4
Łukasz <luxus0@gmail.com>
2023-12-23 21:26:57 +0100

    Add Validation @Size(min = 1, max = 6) Set<Integer> inputNumbers;

--- src/main/java/pl/lotto/domain/numberreceiver/dto/InputNumbersRequestDto.java
+++ src/main/java/pl/lotto/domain/numberreceiver/dto/InputNumbersRequestDto.java
@@ -4,11 +4,13 @@ import lombok.Builder;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Builder
 public record InputNumbersRequestDto(
         @NotNull(message = "{inputNumbers.not.null")
         @NotEmpty(message = "{inputNumbers.not.empty")
+        @Size(min = 1, max = 6, message = "Size must be 6!")
         Set<Integer> inputNumbers) {
 }
