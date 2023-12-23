9987217bc41867ec19322abfa7e4e7c153cbb437
Łukasz <luxus0@gmail.com>
2023-12-23 21:28:47 +0100

    Add @JsonInclude and @JsonProperty for ApiValidationErrorDto

--- src/test/java/integration/dto/ApiValidationErrorDto.java
+++ src/test/java/integration/dto/ApiValidationErrorDto.java
@@ -1,8 +1,11 @@
 package integration.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
-public record ApiValidationErrorDto(List<String> messages, HttpStatus status) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiValidationErrorDto(@JsonProperty("messages")List<String> messages,@JsonProperty("status") HttpStatus status) {
 }
