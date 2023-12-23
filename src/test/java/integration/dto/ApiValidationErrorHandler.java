77d5a167b84b9557e9fc1c4b9538fec55d2e66e2
Łukasz <luxus0@gmail.com>
2023-12-23 21:29:47 +0100

    Add Api validation error handler bad request

---
+++ src/test/java/integration/dto/ApiValidationErrorHandler.java
@@ -0,0 +1,30 @@
+package integration.dto;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@ControllerAdvice
+public class ApiValidationErrorHandler {
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        @ResponseBody
+        ApiValidationErrorDto handleValidationExceptions(MethodArgumentNotValidException e) {
+            final List<String> errors = getErrorsFromException(e);
+            return new ApiValidationErrorDto(errors, HttpStatus.BAD_REQUEST);
+        }
+
+        private List<String> getErrorsFromException(MethodArgumentNotValidException exception) {
+            return exception.getBindingResult()
+                    .getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+        }
+    }
