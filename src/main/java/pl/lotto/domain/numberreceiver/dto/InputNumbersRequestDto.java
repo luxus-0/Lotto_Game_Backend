00d6e04f0adad8b54f3c1c6a84177bb0ed17481b
Łukasz <luxus0@gmail.com>
2023-12-23 23:20:11 +0100

    Add @JsonInclude and @JsonProperty for using json in dto

--- src/main/java/pl/lotto/domain/numberreceiver/dto/InputNumbersRequestDto.java
+++ src/main/java/pl/lotto/domain/numberreceiver/dto/InputNumbersRequestDto.java
@@ -1,16 +1,18 @@
 package pl.lotto.domain.numberreceiver.dto;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
-@Builder
+@JsonInclude
 public record InputNumbersRequestDto(
         @NotNull(message = "{inputNumbers.not.null")
         @NotEmpty(message = "{inputNumbers.not.empty")
         @Size(min = 1, max = 6, message = "Size must be 6!")
+        @JsonProperty("inputNumbers")
         Set<Integer> inputNumbers) {
 }
