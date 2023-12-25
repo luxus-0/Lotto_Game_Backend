dbcdfde1530649c2c36797fda74c7fe698e14cbc
Łukasz <luxus0@gmail.com>
2023-12-25 23:44:57 +0100

    Add Validation @NotNull and @NotEmpty with message

--- src/main/java/pl/lotto/domain/login/dto/RegisterUserDto.java
+++ src/main/java/pl/lotto/domain/login/dto/RegisterUserDto.java
@@ -1,6 +1,12 @@
 package pl.lotto.domain.login.dto;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
-public record RegisterUserDto(@NotBlank String username, @NotBlank String password) {
+public record RegisterUserDto(@NotNull(message = "username not null")
+                              @NotEmpty(message = "username not empty")
+                              String username,
+                              @NotNull(message = "password not null")
+                              @NotEmpty(message = "password not empty")
+                              String password) {
 }
