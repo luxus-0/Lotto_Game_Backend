9355a399b10792d8f129b508834a8de39b9f55b9
Łukasz <luxus0@gmail.com>
2023-12-25 23:47:12 +0100

    Add Validation Registration Dto: @UUID String uuid @NotNull @NotEmpty String username @NotBlank boolean created

--- src/main/java/pl/lotto/domain/login/dto/RegistrationResultDto.java
+++ src/main/java/pl/lotto/domain/login/dto/RegistrationResultDto.java
@@ -3,8 +3,13 @@ package pl.lotto.domain.login.dto;
 import org.hibernate.validator.constraints.UUID;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
-public record RegistrationResultDto(@UUID String id,
-                                    @NotBlank String username,
-                                    @NotBlank boolean created) {
+public record RegistrationResultDto(@UUID String uuid,
+                                   @NotNull(message = "username not null")
+                                   @NotEmpty(message = "username not empty")
+                                   String username,
+                                    @NotBlank
+                                    boolean created) {
 }
