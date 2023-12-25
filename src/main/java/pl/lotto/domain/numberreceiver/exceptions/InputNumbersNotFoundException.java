92cc7f4b8477d762963354e62f2005d763e9a1c6
Łukasz <luxus0@gmail.com>
2023-12-25 23:37:16 +0100

    Removed InputNumbersNotFoundException();

--- src/main/java/pl/lotto/domain/numberreceiver/exceptions/InputNumbersNotFoundException.java
+++ src/main/java/pl/lotto/domain/numberreceiver/exceptions/InputNumbersNotFoundException.java
@@ -3,13 +3,8 @@ package pl.lotto.domain.numberreceiver.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import static pl.lotto.domain.numberreceiver.NumberReceiverValidationResult.INPUT_NUMBERS_NOT_FOUND;
-
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InputNumbersNotFoundException extends RuntimeException {
-    public InputNumbersNotFoundException() {
-        super(INPUT_NUMBERS_NOT_FOUND.getInfo());
-    }
 
     public InputNumbersNotFoundException(String message){
         super(message);
