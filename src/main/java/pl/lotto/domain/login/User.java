14b2d22c25304b4e413527de082ec509ccc444c6
Łukasz <luxus0@gmail.com>
2023-12-25 23:59:03 +0100

    Add User validation
    @NotNull(message = "username not null")
    @NotEmpty(message = "username not empty")
    String username,
    
    @NotNull(message = "password not null")
    @NotEmpty(message = "password not empty")
    String password

--- src/main/java/pl/lotto/domain/login/User.java
+++ src/main/java/pl/lotto/domain/login/User.java
@@ -1,19 +1,25 @@
 package pl.lotto.domain.login;
 
 import lombok.Builder;
-import org.springframework.data.annotation.Id;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @Builder
 @Document
-public record User(@Id String id,
+public record User(@UUID String uuid,
             @Indexed(unique = true)
+            @NotNull(message = "username not null")
+            @NotEmpty(message = "username not empty")
             String username,
+            @NotNull(message = "password not null")
+            @NotEmpty(message = "password not empty")
             String password) implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
