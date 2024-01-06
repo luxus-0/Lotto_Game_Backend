0b2a5c0cae3f5a8622d28c7f10cbcdfd582a657a
Łukasz <luxus0@gmail.com>
2024-01-06 16:00:16 +0100

    Repair constant TOKEN REGEX remove //

--- src/test/java/integration/token/TokenIntegrationTestConstants.java
+++ src/test/java/integration/token/TokenIntegrationTestConstants.java
@@ -1,5 +1,5 @@
 package integration.token;
 
 public class TokenIntegrationTestConstants {
-    public static String TOKEN_REGEX = "^([A-Za-z0-9-_=]+\\\\.)+([A-Za-z0-9-_=])+\\\\.?$";
+    public static String TOKEN_REGEX = "^([A-Za-z0-9-_=]+\\.)+([A-Za-z0-9-_=])+\\.?$";
 }
