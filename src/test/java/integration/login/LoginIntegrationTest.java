d486966d498591ba75ebe1262bfa4b1759aa35b6
Łukasz <luxus0@gmail.com>
2024-01-14 17:15:40 +0100

    Change

---
+++ src/test/java/integration/login/LoginIntegrationTest.java
@@ -0,0 +1,100 @@
+package integration.login;
+
+import integration.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import pl.lotto.domain.login.LoginAndRegisterFacade;
+import pl.lotto.domain.login.LoginRepository;
+import pl.lotto.domain.login.User;
+import pl.lotto.domain.login.dto.UserDto;
+import pl.lotto.infrastructure.security.token.dto.TokenResponseDto;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pl.lotto.domain.login.LoginAndRegisterFacade.USER_NOT_FOUND;
+
+@WithMockUser
+public class LoginIntegrationTest extends BaseIntegrationTest {
+    @Mock
+    private LoginRepository loginRepository;
+
+    @InjectMocks
+    private LoginAndRegisterFacade loginAndRegisterFacade;
+
+    @Test
+    public void should_return_correct_login_credentials() {
+        User testUser = new User("12345", "testUsername", "testPassword");
+        when(loginRepository.findByUsername("testUsername")).thenReturn(Optional.of(testUser));
+
+        UserDto actualUser = loginAndRegisterFacade.findByUsername("testUsername");
+
+        assertEquals("12345", actualUser.uuid());
+        assertEquals("testUsername", actualUser.username());
+        assertEquals("testPassword", actualUser.password());
+    }
+
+    @Test
+    public void should_throw_bad_credentials_when_username_not_found() {
+        when(loginRepository.findByUsername("testUsername"))
+                .thenReturn(Optional.of(new User("12345", "testUsername", "testPassword")));
+
+        when(loginRepository.findByUsername("errorUsername"))
+                .thenThrow(new BadCredentialsException(USER_NOT_FOUND));
+
+        assertThrows(BadCredentialsException.class, () -> loginAndRegisterFacade.findByUsername("errorUsername"));
+    }
+
+    @Test
+    public void should_throw_illegal_argument_exception_when_username_is_null() {
+        when(loginRepository.findByUsername(null))
+                .thenReturn(Optional.of(new User(null, null, null)));
+
+        when(loginRepository.findByUsername(null))
+                .thenThrow(new IllegalArgumentException());
+
+        assertThrows(IllegalArgumentException.class, () -> loginAndRegisterFacade.findByUsername(null));
+    }
+
+    @Test
+    public void should_return_correct_login_credentials_with_token() throws Exception {
+       mockMvc.perform(post("/token")
+                .content("""
+                        {
+                            "username": "someUser",
+                            "password": "somePassword"
+                        }
+                        """.trim())
+                .contentType(APPLICATION_JSON_VALUE)
+                .header(AUTHORIZATION + " Bearer", properties.secret())).andReturn();
+
+        ResultActions login = mockMvc.perform(post("/login")
+                .content("""
+                            {
+                                "username": "someUser",
+                                "password": "somePassword"
+                            }
+                            """.trim())
+                .contentType(APPLICATION_JSON_VALUE)
+        );
+
+        MvcResult loginResult = login.andExpect(status().isCreated()).andReturn();
+        String loginJson = loginResult.getResponse().getContentAsString();
+        TokenResponseDto loginWithToken = objectMapper.readValue(loginJson, TokenResponseDto.class);
+
+        assertAll(
+                () -> assertThat(loginWithToken.username()).isEqualTo("someUser"),
+                () -> assertThat(loginWithToken.token()).isEqualTo(properties.secret()));
+    }
+}
--- src/test/java/integration/numberreceiver/NumberReceiverIntegrationTest.java
+++ src/test/java/integration/numberreceiver/NumberReceiverIntegrationTest.java
@@ -1,6 +1,9 @@
 package integration.numberreceiver;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import integration.BaseIntegrationTest;
+import integration.dto.ApiValidationErrorDto;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
@@ -8,41 +11,198 @@ import org.springframework.test.web.servlet.ResultActions;
 import pl.lotto.domain.numberreceiver.dto.TicketResponseDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.status;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static pl.lotto.domain.numberreceiver.NumberReceiverValidationResult.EQUALS_SIX_NUMBERS;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pl.lotto.domain.numberreceiver.NumberReceiverValidationResult.*;
 
+@Log4j2
 public class NumberReceiverIntegrationTest extends BaseIntegrationTest {
+
+
     @Test
     public void should_post_input_six_numbers_with_draw_date() throws Exception {
-            //given
-           LocalDateTime drawDate = LocalDateTime.of(2023, 12, 16, 12, 0, 0);
+        //given
+        LocalDateTime drawDate = LocalDateTime.of(2023, 12, 16, 12, 0, 0);
 
-            //when
-            ResultActions postInputNumbers = mockMvc.perform(post("/inputNumbers")
-                    .content("""
+        //when
+        ResultActions postInputNumbers = mockMvc.perform(post("/inputNumbers")
+                .content("""
                             {
                                 "inputNumbers" : [93, 10, 32, 45, 11, 75]
                             }
                             """.trim()
-                    ).contentType(MediaType.APPLICATION_JSON)
-                    .header(CONTENT_TYPE, APPLICATION_JSON_VALUE));
+                ).contentType(MediaType.APPLICATION_JSON)
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE));
 
-            MvcResult mvcResult = postInputNumbers.andExpect(httpStatus -> status(200)).andReturn();
-            String json = mvcResult.getResponse().getContentAsString();
-            TicketResponseDto ticketResponseDto = objectMapper.readValue(json, TicketResponseDto.class);
-            String ticketId = ticketResponseDto.ticketUUID();
+        MvcResult mvcResult = postInputNumbers.andExpect(httpStatus -> WireMock.status(200)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        TicketResponseDto ticketResponseDto = objectMapper.readValue(json, TicketResponseDto.class);
+        String ticketId = ticketResponseDto.ticketUUID();
 
-            //then
-            assertAll(
-                    () -> assertThat(ticketId).isNotNull(),
-                    () -> assertThat(ticketResponseDto.drawDate()).isEqualTo(drawDate),
-                    () -> assertThat(ticketResponseDto.message()).isEqualTo(EQUALS_SIX_NUMBERS.getInfo())
-            );
+        //then
+        assertAll(
+                () -> assertThat(ticketId).isNotNull(),
+                () -> assertThat(ticketResponseDto.drawDate()).isEqualTo(drawDate),
+                () -> assertThat(ticketResponseDto.message()).isEqualTo(EQUALS_SIX_NUMBERS.getInfo())
+        );
+    }
+    @Test
+    public void should_return_status_404_not_found_and_validation_message_when_body_has_empty_input_numbers() throws Exception {
+        //give
+        MvcResult perform = mockMvc.perform(post("/inputNumbers")
+                        .content("""
+                                {
+                                "inputNumbers" : []
+                                }
+                                """.trim()
+                        ).contentType(APPLICATION_JSON)
+                ).andExpect(status().isNotFound())
+                .andReturn();
+
+        //when
+        String json = perform.getResponse().getContentAsString();
+
+        //then
+        assertThrows(Exception.class, () -> {
+            ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+            result.messages().containsAll(List.of(NOT_FOUND.name(), "404"));
+        });
+    }
+
+    @Test
+    public void should_return_status_404_not_found_and_validation_message_when_body_is_empty() throws Exception {
+        //given
+        MvcResult perform = mockMvc.perform(post("/inputNumbers")
+                        .content("""
+                                {
+                                                                   
+                                 }
+                                 """.trim()
+                        ).contentType(APPLICATION_JSON)
+                ).andExpect(status().isNotFound())
+                .andReturn();
+
+        //when
+        String json = perform.getResponse().getContentAsString();
+
+        //then
+        assertThrows(Exception.class, () -> {
+            ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+            assertThat(result.messages()).containsExactlyInAnyOrder(
+                    "inputNumbers must not be empty",
+                    "inputNumbers must not be null");
+        });
+    }
+
+    @Test
+    public void should_return_status_403_forbidden_when_request_has_incorrect_url_input_numbers() throws Exception {
+        //given && when
+        MvcResult getInputNumbersWithNoExistingId = mockMvc.perform(post("/inputNumbers/444")
+                        .content("""
+                                {
+                                "inputNumbers" : []
+                                }
+                                """.trim()
+                        ).contentType(APPLICATION_JSON))
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+        //then
+        String json = getInputNumbersWithNoExistingId.getResponse().getContentAsString();
+        ApiValidationErrorDto result = new ApiValidationErrorDto(List.of("403"), FORBIDDEN);
+
+        assertAll(
+                () -> assertThrows(Exception.class,
+                        () -> objectMapper.readValue(json, ApiValidationErrorDto.class)),
+                () -> assertThat(result.messages()).isEqualTo(List.of("403"))
+        );
+    }
+
+    @Test
+    public void should_return_status_200_when_input_numbers_has_size_less_than_six() throws Exception {
+        //given && when
+        LocalDateTime drawDate = LocalDateTime.of(2023, 12, 16, 12, 0, 0);
+
+        ResultActions postInputNumbersWithIncorrectSize = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                            "inputNumbers" : [1, 2, 3, 4, 5]
+                        }
+                        """.trim()).contentType(APPLICATION_JSON)
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE));
+
+        MvcResult mvcResult = postInputNumbersWithIncorrectSize.andExpect(httpStatus -> WireMock.status(200)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        TicketResponseDto ticketResponseDto = objectMapper.readValue(json, TicketResponseDto.class);
+        String ticketId = ticketResponseDto.ticketUUID();
+
+        //then
+        assertAll(
+                () -> assertThat(ticketId).isNotNull(),
+                () -> assertThat(ticketResponseDto.drawDate()).isEqualTo(drawDate),
+                () -> assertThat(ticketResponseDto.message()).isEqualTo(LESS_THAN_SIX_NUMBERS.getInfo())
+        );
+    }
+
+    @Test
+    public void should_return_status_200_when_input_numbers_has_size_more_than_six() throws Exception {
+        //given && when
+        LocalDateTime drawDate = LocalDateTime.of(2023, 12, 16, 12, 0, 0);
+
+        ResultActions postInputNumbersWithIncorrectSize = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                            "inputNumbers" : [1, 2, 3, 4, 5, 6, 7, 8]
+                        }
+                        """.trim()).contentType(APPLICATION_JSON)
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE));
+
+        MvcResult mvcResult = postInputNumbersWithIncorrectSize.andExpect(httpStatus -> WireMock.status(200)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        TicketResponseDto ticketResponseDto = objectMapper.readValue(json, TicketResponseDto.class);
+        String ticketId = ticketResponseDto.ticketUUID();
+
+        //then
+        assertAll(
+                () -> assertThat(ticketId).isNotNull(),
+                () -> assertThat(ticketResponseDto.drawDate()).isEqualTo(drawDate),
+                () -> assertThat(ticketResponseDto.message()).isEqualTo(MORE_THAN_SIX_NUMBERS.getInfo())
+        );
+    }
+
+    @Test
+    public void should_return_status_200_when_input_numbers_is_out_of_range() throws Exception {
+        //given && when
+        LocalDateTime drawDate = LocalDateTime.of(2023, 12, 16, 12, 0, 0);
+
+        ResultActions postInputNumbersWithIncorrectSize = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                            "inputNumbers" : [0, 0, 0]
+                        }
+                        """.trim())
+                .contentType(APPLICATION_JSON)
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE));
+
+        MvcResult mvcResult = postInputNumbersWithIncorrectSize.andExpect(httpStatus -> WireMock.status(200)).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        TicketResponseDto ticketResponseDto = objectMapper.readValue(json, TicketResponseDto.class);
+        String ticketId = ticketResponseDto.ticketUUID();
+
+        //then
+        assertAll(
+                () -> assertThat(ticketId).isNotNull(),
+                () -> assertThat(ticketResponseDto.drawDate()).isEqualTo(drawDate),
+                () -> assertThat(ticketResponseDto.message()).isEqualTo(OUT_OF_RANGE_NUMBERS.getInfo())
+        );
     }
 }
