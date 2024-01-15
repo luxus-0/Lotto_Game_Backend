cf71204a37f851e5113fc9e6f6981caf202f3f6f
Łukasz <luxus0@gmail.com>
2024-01-15 22:25:20 +0100

    change

--- src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerFacadeConfiguration.java
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerFacadeConfiguration.java
@@ -2,24 +2,12 @@ package pl.lotto.domain.resultannouncer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import pl.lotto.domain.drawdate.AdjustableClock;
 import pl.lotto.domain.resultchecker.ResultsCheckerFacade;
 
 import java.time.Clock;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-
-import static java.time.ZoneOffset.UTC;
 
 @Configuration
 public class ResultAnnouncerFacadeConfiguration {
-
-    @Bean
-    @Primary
-    AdjustableClock clock() {
-        return new AdjustableClock(LocalDateTime.of(2022, 11, 19, 12, 0, 0).toInstant(UTC), ZoneId.systemDefault());
-    }
 
     @Bean
     public ResultAnnouncerFacade resultAnnouncerFacade(ResultsCheckerFacade resultsCheckerFacade, ResultAnnouncerRepository resultAnnouncerRepository, Clock clock) {
--- src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerRepository.java
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerRepository.java
@@ -1,9 +1,11 @@
 package pl.lotto.domain.resultannouncer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ResultAnnouncerRepository extends MongoRepository<ResultAnnouncerResponse, String> {
     Optional<ResultAnnouncerResponse> findAllByTicketUUID(String ticketUUID);
 }
