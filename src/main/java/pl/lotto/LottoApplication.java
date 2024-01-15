54c81a7a47c77a8e2c0c4b59f014f3b4d9999113
Łukasz <luxus0@gmail.com>
2024-01-15 23:23:04 +0100

    change

--- src/main/java/pl/lotto/LottoApplication.java
+++ src/main/java/pl/lotto/LottoApplication.java
@@ -3,9 +3,7 @@ package pl.lotto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.lotto.domain.numbersgenerator.WinningNumbersConfigurationProperties;
 import pl.lotto.infrastructure.numbergenerator.client.TimeConnectionClient;
@@ -15,8 +13,6 @@ import pl.lotto.infrastructure.security.token.JwtConfigurationProperties;
 @EnableConfigurationProperties({WinningNumbersConfigurationProperties.class, TimeConnectionClient.class, JwtConfigurationProperties.class})
 @EnableScheduling
 @EnableMongoRepositories
-@EnableRedisRepositories
-@ComponentScan
 public class LottoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LottoApplication.class, args);
