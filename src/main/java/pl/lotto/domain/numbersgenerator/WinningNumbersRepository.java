6cab4d374c85d1dd9304e09d95b927555eb6d530
Łukasz <luxus0@gmail.com>
2024-01-15 22:25:28 +0100

    change

--- src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersRepository.java
+++ src/main/java/pl/lotto/domain/numbersgenerator/WinningNumbersRepository.java
@@ -1,10 +1,12 @@
 package pl.lotto.domain.numbersgenerator;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface WinningNumbersRepository extends MongoRepository<WinningNumbers, String> {
     Optional<WinningNumbers> findWinningNumbersByDrawDate(LocalDateTime drawDate);
 
