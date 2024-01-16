86d30dd32f8970668a68bfda1a7470a2ef3bf403
Łukasz <luxus0@gmail.com>
2024-01-16 20:15:56 +0100

    Add class WinningTicketResponse with static method createWinningTicketResponse()

---
+++ src/test/java/integration/cache/redis/constants/WinningTicketResponse.java
@@ -0,0 +1,23 @@
+package integration.cache.redis.constants;
+
+import pl.lotto.domain.numbersgenerator.dto.WinningTicketResponseDto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+public class WinningTicketResponse {
+    public static WinningTicketResponseDto createWinningTicketResponse() {
+        {
+            Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5,6);
+            LocalDateTime drawDate = LocalDateTime.now();
+            String ticketUUID = UUID.randomUUID().toString();
+
+            return WinningTicketResponseDto.builder()
+                    .winningNumbers(winningNumbers)
+                    .drawDate(drawDate)
+                    .ticketUUID(ticketUUID)
+                    .build();
+        }
+    }
+}
