d00f4e9f4961fea3c6572f7d3f6bfbd891043cc1
Łukasz <luxus0@gmail.com>
2024-01-16 21:45:41 +0100

    Rename class from RedisNumberReceiverConstant to RedisNumberReceiver

--- src/test/java/integration/cache/redis/constants/RedisNumberReceiver.java
+++ src/test/java/integration/cache/redis/constants/RedisNumberReceiver.java
@@ -1,8 +1,8 @@
-package integration.cache.redis;
+package integration.cache.redis.constants;
 
 import java.time.LocalDateTime;
 
-class RedisNumberReceiverConstant {
+public class RedisNumberReceiver {
     public static String TICKET_UUID_REGEX = "[a-zA-Z0-9]";
     public static LocalDateTime DRAW_DATE = LocalDateTime.of(2023, 12, 16, 12, 0);
 }
