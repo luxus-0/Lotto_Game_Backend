e4e9a0eafb727f75602503e6b65c18b5a04ec323
Łukasz <luxus0@gmail.com>
2024-01-18 23:07:43 +0100

    Refactor

--- src/main/java/pl/lotto/domain/resultchecker/exceptions/ResultCheckerNotFoundException.java
+++ src/main/java/pl/lotto/domain/resultchecker/exceptions/ResultCheckerNotFoundException.java
@@ -4,8 +4,4 @@ public class ResultCheckerNotFoundException extends Exception {
     public ResultCheckerNotFoundException(String ticketUUID) {
         super("Winning ticket UUID: " + ticketUUID + " not found");
     }
-
-    public ResultCheckerNotFoundException(){
-        super("Winning ticket not found");
-    }
 }
