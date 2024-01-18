e14ffc1e3bc3ea6fd1ab8cf3105731fcab405002
Łukasz <luxus0@gmail.com>
2024-01-18 22:45:18 +0100

    Refactor

--- src/main/java/pl/lotto/domain/resultchecker/exceptions/NotInRangeNumbersException.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto.domain.resultchecker.exceptions;
-
-public class NotInRangeNumbersException extends RuntimeException {
-    public NotInRangeNumbersException(String message) {
-        super(message);
-    }
-}
