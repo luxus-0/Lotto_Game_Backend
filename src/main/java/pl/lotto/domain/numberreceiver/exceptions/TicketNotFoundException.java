c4efb3bb65923bd68acac12e586838bead660045
Łukasz <luxus0@gmail.com>
2024-01-18 23:08:16 +0100

    Refactor

--- src/main/java/pl/lotto/domain/numberreceiver/exceptions/TicketNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pl.lotto.domain.numberreceiver.exceptions;
-
-public class TicketNotFoundException extends RuntimeException {
-    public TicketNotFoundException(String message) {
-        super("");
-    }
-}
