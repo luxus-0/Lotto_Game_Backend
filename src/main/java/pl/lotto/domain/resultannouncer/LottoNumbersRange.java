29f8cca57de19e682ccd69c6bb3abc053e559961
Łukasz <luxus0@gmail.com>
2024-01-22 13:15:21 +0100

    Add Lotto Numbers Range:
    Constants:
     COUNT_NUMBERS = 6
     LOWER_BAND_NUMBERS = 1;
     UPPER_BAND_NUMBERS = 99;

---
+++ src/main/java/pl/lotto/domain/resultannouncer/LottoNumbersRange.java
@@ -0,0 +1,10 @@
+package pl.lotto.domain.resultannouncer;
+
+import lombok.Getter;
+
+@Getter
+class LottoNumbersRange {
+    public static final int COUNT_NUMBERS = 6;
+    public static final int LOWER_BAND_NUMBERS = 1;
+    public static final  int UPPER_BAND_NUMBERS = 99;
+}
