ce134c16c9692234039010257f5fd4f3cc3aee45
Łukasz <luxus0@gmail.com>
2024-01-22 13:20:40 +0100

    Remove unused Dto record: PlayerResponseDto and PlayerDto

--- src/main/java/pl/lotto/domain/resultannouncer/dto/PlayerResponseDto.java
+++
@@ -1,8 +0,0 @@
-package pl.lotto.domain.resultannouncer.dto;
-
-import lombok.Builder;
-import pl.lotto.domain.numberreceiver.dto.TicketDto;
-
-@Builder
-public record PlayerResponseDto(PlayerDto player, TicketDto ticket) {
-}
