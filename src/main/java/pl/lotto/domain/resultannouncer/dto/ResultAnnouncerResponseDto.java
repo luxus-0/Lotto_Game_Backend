86f927e083c91523a586c9ecaf958676421e2a92
Łukasz <luxus0@gmail.com>
2024-01-22 13:39:11 +0100

    Add validation @NotNull @NotEmpty Set<Integer> hitNumbers

--- src/main/java/pl/lotto/domain/resultannouncer/dto/ResultAnnouncerResponseDto.java
+++ src/main/java/pl/lotto/domain/resultannouncer/dto/ResultAnnouncerResponseDto.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import org.hibernate.validator.constraints.UUID;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -14,15 +13,12 @@ import java.util.Set;
 @Builder
 public record ResultAnnouncerResponseDto(@UUID
                                          String ticketUUID,
-                                         @NotNull(message = "numbers not null")
-                                         @NotEmpty(message = "numbers not empty")
                                          Set<Integer> inputNumbers,
-                                         @NotBlank
+                                         @NotNull
+                                         @NotEmpty
                                          Set<Integer> hitNumbers,
                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                          LocalDateTime drawDate,
                                          boolean isWinner,
-                                         @NotNull
-                                         @NotEmpty
                                          String message) implements Serializable {
 }
