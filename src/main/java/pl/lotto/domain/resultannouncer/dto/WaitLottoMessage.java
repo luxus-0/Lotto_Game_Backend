2adda3d266f4da7a37024d0a287dfd7295678f0d
Łukasz <luxus0@gmail.com>
2024-01-22 13:44:42 +0100

    Removed unused WaitLottoMessage and WinLottoMessage
     Change to ResultLottoDto

--- src/main/java/pl/lotto/domain/resultannouncer/dto/WaitLottoMessage.java
+++
@@ -1,4 +0,0 @@
-package pl.lotto.domain.resultannouncer.dto;
-
-public record WaitLottoMessage(ResultAnnouncerResponseDto toResultAnnouncerResponseSavedDto, String message) {
-}
--- src/main/java/pl/lotto/domain/resultannouncer/dto/WinLottoMessage.java
+++
@@ -1,4 +0,0 @@
-package pl.lotto.domain.resultannouncer.dto;
-
-public record WinLottoMessage(ResultAnnouncerResponseDto toResultAnnouncerResponseSavedDto, String message) {
-}
