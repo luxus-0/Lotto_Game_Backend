325d1087b5b94aedfe4cc5cbd1d4150ca4913cee
Łukasz <luxus0@gmail.com>
2024-01-23 11:41:08 +0100

    Refactor module resultannouncer

---
+++ src/main/java/pl/lotto/domain/resultannouncer/NumbersFactor.java
@@ -0,0 +1,5 @@
+package pl.lotto.domain.resultannouncer;
+
+public class NumbersFactor {
+    public static final int NUMBERS_FACTOR = 3;
+}
--- src/main/java/pl/lotto/domain/resultannouncer/NumbersInRange.java
+++ src/main/java/pl/lotto/domain/resultannouncer/NumbersInRange.java
@@ -3,7 +3,7 @@ package pl.lotto.domain.resultannouncer;
 import lombok.Getter;
 
 @Getter
-class LottoNumbersRange {
+class NumbersInRange {
     public static final int COUNT_NUMBERS = 6;
     public static final int LOWER_BAND_NUMBERS = 1;
     public static final  int UPPER_BAND_NUMBERS = 99;
